1cc648fc0ed30886b950d773c82e6890c2f559fd
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-10-06 09:42:07 +0530

    Time: 1 ms (55.48%) | Memory: 41.4 MB (92.97%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,35 @@
+class Solution {
+    // Piyush //
+    // public int minAddToMakeValid(String s) {
+    //     Stack<Character> st=new Stack<>();
+    //     int count=0;
+    //     for(int i=0; i<s.length(); i++){
+    //         char ch=s.charAt(i);
+    //         if(ch=='('){
+    //             st.push(ch);
+    //         }else if(st.size()>0 && st.peek()=='('){
+    //             st.pop();
+    //         }else{
+    //             count++;
+    //         }
+    //     }
+    //     return count+st.size();
+    // }
+
+    // piyush //
+    public int minAddToMakeValid(String s){
+        Stack<Character> st=new Stack<>();
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                st.push(ch);
+            }else if(st.size()>0 && st.peek()=='('){
+                st.pop();
+            }else{
+                count++;
+            }
+        }
+        return count+st.size();
+    }
+}
