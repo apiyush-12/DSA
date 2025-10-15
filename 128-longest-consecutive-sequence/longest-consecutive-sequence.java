16758ea0d3fcbbdb043b275163c37c52e3025257
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-10-15 14:19:38 +0530

    Time: 16 ms (97.99%) | Memory: 56.8 MB (84.31%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,4 +1,4 @@
-class Solution {
+// class Solution {
     // public int longestConsecutive(int[] nums) {
     //     HashMap<Integer, Boolean> hm = new HashMap<>();
     //     for(int i=0; i<nums.length; i++){
@@ -23,26 +23,49 @@ class Solution {
     // }
 
     // piyush //
-    public int longestConsecutive(int[] nums){
-        HashMap<Integer, Boolean> hm=new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            hm.put(nums[i], false);
+//     public int longestConsecutive(int[] nums){
+//         HashMap<Integer, Boolean> hm=new HashMap<>();
+//         for(int i=0; i<nums.length; i++){
+//             hm.put(nums[i], false);
+//         }
+//         for(int key : hm.keySet()){
+//             if(hm.containsKey(key-1)==false){
+//                 hm.put(key, true);
+//             }
+//         }
+//         int max=0;
+//         for(int key : hm.keySet()){
+//             int k=1;
+//             if(hm.get(key)==true){
+//                 while(hm.containsKey(key+k)==true){
+//                     k++;
+//                 }
+//             }
+//             max=Math.max(max, k);
+//         }
+//         return max;
+//     }
+// }
+
+// 2nd Sol
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){
+            return 0;
         }
-        for(int key : hm.keySet()){
-            if(hm.containsKey(key-1)==false){
-                hm.put(key, true);
+        int count =1;
+        int lng=1;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]+1==nums[i+1]){
+            count ++;
+            }
+            else if( nums[i]!=nums[i+1]){
+              lng=Math.max(lng,count);
+              count=1;
             }
         }
-        int max=0;
-        for(int key : hm.keySet()){
-            int k=1;
-            if(hm.get(key)==true){
-                while(hm.containsKey(key+k)==true){
-                    k++;
-                }
-            }
-            max=Math.max(max, k);
-        }
-        return max;
+    lng=Math.max(lng,count);
+              return lng;
     }
 }
