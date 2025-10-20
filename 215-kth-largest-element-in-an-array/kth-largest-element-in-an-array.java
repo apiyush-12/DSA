749d5e54b41712dfd12711e328bfc7347aeb66f3
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-10-20 17:07:03 +0530

    Time: 2 ms (100.00%) | Memory: 55.3 MB (99.68%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -13,16 +13,31 @@ class Solution {
     // }
 
     // piyush //
-    public int findKthLargest(int[] nums, int k){
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int i=0; i<nums.length; i++){
-            if(pq.size()<k){
-                pq.add(nums[i]);
-            }else if(pq.peek()<nums[i]){
-                pq.remove();
-                pq.add(nums[i]);
+    // public int findKthLargest(int[] nums, int k){
+    //     PriorityQueue<Integer> pq=new PriorityQueue<>();
+    //     for(int i=0; i<nums.length; i++){
+    //         if(pq.size()<k){
+    //             pq.add(nums[i]);
+    //         }else if(pq.peek()<nums[i]){
+    //             pq.remove();
+    //             pq.add(nums[i]);
+    //         }
+    //     }
+    //     return pq.peek();
+    // }
+    
+    public int findKthLargest(int[] nums, int k) {
+        int[] count = new int[20001];
+
+        for (int num : nums) 
+            count[num + 10000]++;
+
+        for (int i = count.length - 1; i >= 0; i--) 
+            if (count[i] > 0) {
+                k -= count[i];
+                if (k <= 0) return i - 10000;
             }
-        }
-        return pq.peek();
+        
+        return -1;
     }
 }
