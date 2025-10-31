d08202080aa8593746c5f9658b144f7d17c74305
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-10-31 22:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (5.43%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,41 +1,41 @@
 class Solution {
-    public List<Integer> targetIndices(int[] nums, int target) {
-        int num=0;
-        int tcount=0;
+//     public List<Integer> targetIndices(int[] nums, int target) {
+//         int num=0;
+//         int tcount=0;
 
-        for(int ele : nums){
-            if(ele==target){
-                tcount++;
-            }else if(ele<target){
-                num++;
-            }
-        }
-        List<Integer> ans=new ArrayList<>();
-        while(tcount>0){
-            ans.add(num);
-            num++;
-            tcount--;
-        }
-        return ans;
-   }
-
-//    public List<Integer> targetIndices(int[] nums, int target){
-//     int num=0;
-//     int tcount=0;
-//     for(int e : nums){
-//         if(e==target){
-//             tcount++;
+//         for(int ele : nums){
+//             if(ele==target){
+//                 tcount++;
+//             }else if(ele<target){
+//                 num++;
+//             }
 //         }
-//         else if(e<target){
+//         List<Integer> ans=new ArrayList<>();
+//         while(tcount>0){
+//             ans.add(num);
 //             num++;
+//             tcount--;
 //         }
-//     }
-//     List<Integer> ans=new ArrayList<>();
-//     while(tcount>0){
-//         ans.add(num);
-//         num++;
-//         tcount--;
-//     }
-//     return ans;
+//         return ans;
 //    }
+
+   public List<Integer> targetIndices(int[] nums, int target){
+    int num=0;
+    int tcount=0;
+    for(int e : nums){
+        if(e==target){
+            tcount++;
+        }
+        else if(e<target){
+            num++;
+        }
+    }
+    List<Integer> ans=new ArrayList<>();
+    while(tcount>0){
+        ans.add(num);
+        num++;
+        tcount--;
+    }
+    return ans;
+   }
 }
