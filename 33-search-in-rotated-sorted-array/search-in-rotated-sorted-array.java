dc094908c6fb4f385acbde32f5ff0ec7c54ca66f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-11-01 22:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (6.47%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,39 +1,54 @@
-// class Solution {
-//     public int search(int[] nums, int target){
-//         int start=0;
-//         int end=nums.length-1;
-//         while(start<=end){
-//             int mid=(start+end)/2;
-//             if(nums[mid]==target){
-//                 return mid;
-//             }else if(nums[start]<=nums[mid]){
-//                 if(nums[start]<=target && nums[mid]>target){
-//                     end=mid-1;
-//                 }else{
-//                     start=mid+1;
-//                 }
-//             }else{
-//                 if(nums[mid]<target && nums[end]>=target){
-//                     start=mid+1;
-//                 }else{
-//                     end=mid-1;
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
 class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(target==nums[i]){
-                return i;
+    // public int search(int[] nums, int target) {
+    //     int start = 0;
+    //     int end=nums.length-1;
+    //     while(start<=end){
+    //         int mid=(start+end)/2;
+    //         if(nums[mid]==target){
+    //             return mid;
+    //         }else if(nums[mid]>=nums[start]){
+    //             if(target>=nums[start] && target<=nums[mid]){
+    //                 end=mid-1;
+    //             }else{
+    //                 start=mid+1;
+    //             }
+    //         }else{
+    //             if(target>nums[mid]&&target<=nums[end]){
+    //                 start=mid+1;
+    //             }else{
+    //                 end=mid-1;
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+// piyush //
+    public int search(int[] nums, int target){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && nums[mid]>target){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(nums[mid]<target && nums[end]>=target){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
         return -1;
     }
 }
+
 
 
 
