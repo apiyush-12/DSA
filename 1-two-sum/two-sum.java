0b8def34c955fac45ecb8fdbbce2de8731c3d771
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-08 17:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (55.14%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -13,7 +13,7 @@ class Solution {
     // throw new IllegalArgumentException("No Match");
     // }
 
-
+    // piyush
     public int[] twoSum(int[]nums, int target){
         for(int i=1; i<nums.length; i++){
             for(int j=0; i+j<nums.length; j++){
