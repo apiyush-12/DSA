44e131f51922fb3f157c5c022171dccafe828116
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-08 17:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (44.37%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,27 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-       Map<Integer, Integer> map=new HashMap<>();
-       for(int i=0; i<nums.length; i++){
-        int complement= target-nums[i];
-        if(map.containsKey(complement)){
-            return new int[] {map.get(complement), i};
+    // public int[] twoSum(int[] nums, int target) {
+    //    Map<Integer, Integer> map=new HashMap<>();
+    //    for(int i=0; i<nums.length; i++){
+    //     int complement= target-nums[i];
+    //     if(map.containsKey(complement)){
+    //         return new int[] {map.get(complement), i};
+    //     }
+    //     else{
+    //         map.put(nums[i], i);
+    //     }
+    // }
+    // throw new IllegalArgumentException("No Match");
+    // }
+
+
+    public int[] twoSum(int[]nums, int target){
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; i+j<nums.length; j++){
+                if(nums[i+j]+nums[j]==target){
+                    return new int[]{j, i+j};
+                }
+            }   
         }
-        else{
-            map.put(nums[i], i);
-        }
-    }
-    throw new IllegalArgumentException("No Match");
+        return new int[] {-1, -1};
     }
 }
