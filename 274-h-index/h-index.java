620e34853b5037d84e797ba11e96f81660477aba
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-16 17:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (49.14%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,9 +1,33 @@
-class Solution {
-    public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        for(int i=0; i<citations.length; i++){
-            if(citations[i]>=citations.length-i){
-                return citations.length-i;
+// class Solution {
+//     // time complexity=O(nlogn)
+//     public int hIndex(int[] citations) {
+//         Arrays.sort(citations);
+//         for(int i=0; i<citations.length; i++){
+//             if(citations[i]>=citations.length-i){
+//                 return citations.length-i;
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+class Solution{
+    public int hIndex(int[] citations){
+        int n=citations.length;
+        int[] arr=new int[n+1];
+        for(int i=0; i<n; i++){
+            int x=citations[i];
+            if(x>=n){
+                arr[n]++;
+            }else{
+                arr[x]++;
+            }
+        }
+        int count=0;
+        for(int j=n; j>=0; j--){
+            count+=arr[j];
+            if(count>=j){
+                return j;
             }
         }
         return 0;
