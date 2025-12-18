4150a6b0977458bc93c1cb1433b6e482d8fb4819
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-18 19:51:50 +0530

    Time: 3 ms (79.26%) | Memory: 44.3 MB (60.31%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -17,10 +17,11 @@ class Solution {
 
     // piyush
     public boolean wordBreak(String s, List<String> wordDict){
-        boolean [] dp= new boolean[s.length()+1];
+        int n=s.length();
+        boolean [] dp= new boolean[n+1];
         dp[0]=true;
         Set<String> wordSet=new HashSet<>(wordDict);
-        for(int i=1; i<=s.length(); i++){
+        for(int i=1; i<=n; i++){
             for(int j=i-1; j>=0; j--){
                 if(dp[j] && wordSet.contains(s.substring(j,i))){
                 dp[i]=true;
@@ -28,6 +29,6 @@ class Solution {
                 }
             }
         }
-        return dp[s.length()];
+        return dp[n];
     }
 }
