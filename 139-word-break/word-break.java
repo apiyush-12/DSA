e9f13c792ef8cf4130354414b7a9b5300c8ff16c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-18 19:34:42 +0530

    Time: 8 ms (44.20%) | Memory: 46.3 MB (11.69%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,14 +1,30 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp=new boolean[s.length()+1];
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     boolean[] dp=new boolean[s.length()+1];
+    //     dp[0]=true;
+    //     Set<String> wordSet=new HashSet<>(wordDict);
+    //     for(int i=1; i<=s.length(); i++){
+    //         for(int j=i-1; j>=0; j--){
+    //             if(dp[j] && wordSet.contains(s.substring(j, i))){
+    //                 dp[i]=true;
+    //                 break;
+
+    //             }
+    //         }
+    //     }
+    //     return dp[s.length()];
+    // }
+
+    // piyush
+    public boolean wordBreak(String s, List<String> wordDict){
+        boolean [] dp= new boolean[s.length()+1];
         dp[0]=true;
         Set<String> wordSet=new HashSet<>(wordDict);
         for(int i=1; i<=s.length(); i++){
-            for(int j=i-1; j>=0; j--){
-                if(dp[j] && wordSet.contains(s.substring(j, i))){
-                    dp[i]=true;
-                    break;
-
+            for(int j=0; j<i; j++){
+                if(dp[j] && wordSet.contains(s.substring(j,i))){
+                dp[i]=true;
+                break;
                 }
             }
         }
