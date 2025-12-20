30a9a11d549156bb8e77cb287615e95de9f60615
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-20 20:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (26.28%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-
-        int max1 = robLinear(nums, 0, nums.length - 2);
-        int max2 = robLinear(nums, 1, nums.length - 1);
-
-        return Math.max(max1, max2); // MAXIMUM ROBBERY
-    }
-
-    private int robLinear(int[] nums, int start, int end) {
-        int rob1 = 0, rob2 = 0;
-
-        for (int i = start; i <= end; i++) {
-            int curr = Math.max(rob1 + nums[i], rob2);
-            rob1 = rob2;
-            rob2 = curr;
+        if(nums.length==1){
+            return nums[0];
         }
-        return rob2;
+        int x=Max(nums, 0, nums.length-1);
+        int y=Max(nums, 1, nums.length);
+        return Math.max(x,y);
+    }
+    public int Max(int[] nums, int start, int end){
+        int rob1=0, rob2=0, max=0;
+        for(int i=start; i<end; i++){
+            max=Math.max(rob1+nums[i], rob2);
+            rob1=rob2;
+            rob2=max;
+        }
+        return max;
     }
 }
