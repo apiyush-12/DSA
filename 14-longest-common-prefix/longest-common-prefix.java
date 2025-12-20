d27d059d6d2e27f09d18c1983af590bd3c75ef78
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-20 18:57:42 +0530

    Time: 1 ms (62.65%) | Memory: 43.4 MB (21.60%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -15,6 +15,21 @@ class Solution {
     // }
 
     // piyush
+    // public String longestCommonPrefix(String[] strs){
+    //     if(strs.length==0 || strs==null){
+    //         return "";
+    //     }
+    //     for(int i=0; i<strs[0].length(); i++){
+    //         char ch=strs[0].charAt(i);
+    //         for(int j=1; j<strs.length; j++){
+    //             if(i==strs[j].length() || strs[j].charAt(i)!=ch){
+    //                 return strs[0].substring(0, i);
+    //             }
+    //         }
+    //     }
+    //     return strs[0];
+    // }
+
     public String longestCommonPrefix(String[] strs){
         if(strs.length==0 || strs==null){
             return "";
@@ -22,7 +37,7 @@ class Solution {
         for(int i=0; i<strs[0].length(); i++){
             char ch=strs[0].charAt(i);
             for(int j=1; j<strs.length; j++){
-                if(i==strs[j].length() || strs[j].charAt(i)!=ch){
+                if(strs[j].length()==i || strs[j].charAt(i)!=ch){
                     return strs[0].substring(0, i);
                 }
             }
