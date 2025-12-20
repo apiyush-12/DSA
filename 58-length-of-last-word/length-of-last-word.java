20e543a0dfd816acdc8d008142283855a16e86cd
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-20 18:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (9.36%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -11,6 +11,33 @@ class Solution {
     //     }
     //     return count;
     // }
+
+    // piyush //
+    // public int lengthOfLastWord(String s){
+    //     int count=0;
+    //     for(int i=s.length()-1; i>=0; i--){
+    //         char ch=s.charAt(i);
+    //         if(ch!=' '){
+    //             count++;
+    //         }else if(count!=0){
+    //             break;
+    //         }
+    //     }
+    //     return count;
+    // }
+    // public int lengthOfLastWord(String s){
+    //     int count=0;
+    //     for(int i=s.length()-1; i>=0; i--){
+    //         char ch=s.charAt(i);
+    //         if(ch!=' '){
+    //             count++;
+    //         }else if(count!=0){
+    //             break;
+    //         }
+    //     }
+    //     return count;
+    // }
+
     public int lengthOfLastWord(String s){
         int count=0;
         for(int i=s.length()-1; i>=0; i--){
