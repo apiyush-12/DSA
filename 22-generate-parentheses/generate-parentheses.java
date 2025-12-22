6253df69af1c869fa48a0e0b0ce8c9115f6c69ce
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-22 21:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (64.42%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,9 +1,35 @@
-class Solution {
-    public List<String> generateParenthesis(int n) {
-        List <String> ans=new ArrayList();
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List <String> ans=new ArrayList();
+//         backTrack(ans, new StringBuilder(), 0, 0, n);
+//         return ans;
+//     }
+//     public void backTrack(List<String> ans, StringBuilder curr, int open, int close, int max){
+//         if(curr.length()==max*2){
+//             ans.add(curr.toString());
+//             return;
+//         }
+//         if(open<max){
+//             curr.append("(");
+//             backTrack(ans, curr, open+1, close, max);
+//             curr.deleteCharAt(curr.length()-1);
+//         }
+//         if(close<open){
+//             curr.append(")");
+//             backTrack(ans, curr, open, close+1, max);
+//             curr.deleteCharAt(curr.length()-1);
+//         }
+
+//     }
+// }
+
+class Solution{
+    public List<String> generateParenthesis(int n){
+        List<String> ans=new ArrayList();
         backTrack(ans, new StringBuilder(), 0, 0, n);
         return ans;
     }
+
     public void backTrack(List<String> ans, StringBuilder curr, int open, int close, int max){
         if(curr.length()==max*2){
             ans.add(curr.toString());
@@ -19,6 +45,5 @@ class Solution {
             backTrack(ans, curr, open, close+1, max);
             curr.deleteCharAt(curr.length()-1);
         }
-
     }
 }
