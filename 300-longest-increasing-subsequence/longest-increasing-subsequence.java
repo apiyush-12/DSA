45f7499d09ee8d8b98513d242b8659c2f9dc2208
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-25 19:58:37 +0530

    Time: 5 ms (90.69%) | Memory: 46.3 MB (29.59%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,17 +1,51 @@
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] list=new int[nums.length];
-        Arrays.fill(list, 1);
-        int max=1;
-        for(int i=1; i<nums.length; i++){
-            for(int j=0; j<i; j++){
-                    if(nums[i]>nums[j]){
-                    list[i]=Math.max(list[i], 1+list[j]);
-                    max=Math.max(max, list[i]);
-                }
-            }
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         // Time Complexity : O(n^2)
+//         int[] list=new int[nums.length];
+//         Arrays.fill(list, 1);
+//         int max=1;
+//         for(int i=1; i<nums.length; i++){
+//             for(int j=0; j<i; j++){
+//                     if(nums[i]>nums[j]){
+//                     list[i]=Math.max(list[i], 1+list[j]);
+//                     max=Math.max(max, list[i]);
+//                 }
+//             }
 
+//         }
+//         return max;
+//     }
+// }
+
+class Solution{
+    public int lengthOfLIS(int[] nums){
+        ArrayList<Integer> sub=new ArrayList<>();
+        sub.add(nums[0]);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]>sub.get(sub.size()-1)){
+                sub.add(nums[i]);
+            }
+            else{
+                int j=binarySearch(sub, nums[i]);
+                sub.set(j, nums[i]);
+            }
         }
-        return max;
+        return sub.size();
+    }
+    private int binarySearch(ArrayList<Integer> sub, int num){
+        int left=0;
+        int right=sub.size()-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(sub.get(mid)==num){
+                return mid;
+            }
+            if(sub.get(mid)<num){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+        }
+        return left;
     }
 }
