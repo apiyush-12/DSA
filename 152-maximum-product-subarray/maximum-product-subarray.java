8f8b4c1b79ec8a5c0469cd689920763ec2831411
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-27 21:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (10.17%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,15 +1,44 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        int minProduct=1;
-        int maxProduct=1;
-        int result=nums[0];
-        for(int i=0; i<nums.length; i++){
-            int temp=maxProduct*nums[i];
-            maxProduct=Math.max(nums[i], Math.max(maxProduct*nums[i], minProduct*nums[i]));
-            minProduct=Math.min(nums[i], Math.min(temp, minProduct*nums[i]));
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         int minProduct=1;
+//         int maxProduct=1;
+//         int result=nums[0];
+//         for(int i=0; i<nums.length; i++){
+//             int temp=maxProduct*nums[i];
+//             maxProduct=Math.max(nums[i], Math.max(maxProduct*nums[i], minProduct*nums[i]));
+//             minProduct=Math.min(nums[i], Math.min(temp, minProduct*nums[i]));
 
-            result=Math.max(result, Math.max(maxProduct, minProduct));
+//             result=Math.max(result, Math.max(maxProduct, minProduct));
+//         }
+//         return result;
+//     }
+// }
+
+//2nd Method
+
+class Solution{
+    public int maxProduct(int[] nums){
+        int pro=1;
+        int max=Integer.MIN_VALUE;
+        for(int i=nums.length-1; i>=0; i--){
+            pro*=nums[i];
+            if(pro>max){
+                max=pro;
+            }
+            if(pro==0){
+                pro=1;
+            }
         }
-        return result;
+        pro=1;
+        for(int i=0; i<nums.length; i++){
+            pro*=nums[i];
+            if(pro>max){
+                max=pro;
+            }
+            if(pro==0){
+                pro=1;
+            }
+        }
+        return max;
     }
 }
