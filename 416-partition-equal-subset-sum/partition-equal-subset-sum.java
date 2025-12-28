3a835727c280f7fcc6931571f1e7a444d9bbc328
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-28 22:40:23 +0530

    Time: 10 ms (99.29%) | Memory: 43.8 MB (87.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -21,18 +21,47 @@
 //     }
 // }
 
+// class Solution{
+//     public boolean canPartition(int[] nums){
+//          int totalSum=0;
+//         for(int num : nums){
+//             totalSum+=num;
+//         }
+//         if(totalSum%2!=0){
+//             return false;
+//         }
+//         int target=totalSum/2;
+//         boolean[] dp=new boolean[target+1];
+//         dp[0]=true;
+//         for(int num : nums){
+//             for(int i=target; i>=num; i--){
+//                 if(dp[i-num]){
+//                     dp[i]=true;
+//                 }
+//             }
+//             if(dp[target]){
+//                 return true;
+//             }
+//         }
+//         return dp[target];
+//     }
+// }
+
 class Solution{
     public boolean canPartition(int[] nums){
-         int totalSum=0;
+        int totalSum=0;
         for(int num : nums){
             totalSum+=num;
         }
+
         if(totalSum%2!=0){
             return false;
         }
+
         int target=totalSum/2;
         boolean[] dp=new boolean[target+1];
         dp[0]=true;
+
         for(int num : nums){
             for(int i=target; i>=num; i--){
                 if(dp[i-num]){
