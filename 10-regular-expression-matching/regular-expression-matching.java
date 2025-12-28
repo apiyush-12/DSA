757f4831b8efb30a5fa893abdefb4961eae5ca90
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-28 23:55:49 +0530

    Time: 2 ms (55.39%) | Memory: 43.9 MB (18.45%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,72 +1,30 @@
-// class Solution {
-//     public boolean isMatch(String s, String p) {
-//         boolean[][] dp= new boolean[s.length()+1][p.length()+1];
-//         dp[0][0]=true;
-//         for(int i=0; i<p.length(); i++){
-//             if(p.charAt(i)=='*' && dp[0][i-1]){
-//                 dp[0][i+1]=true;
-//             }
-//         }
-//         for(int i=0; i<s.length(); i++){
-//             for(int j=0; j<p.length(); j++){
-//                 if(p.charAt(j)=='.'){
-//                     dp[i+1][j+1]=dp[i][j];
-//                 }
-//                 if(p.charAt(j)==s.charAt(i)){
-//                     dp[i+1][j+1]=dp[i][j];
-//                 }
-//                 if(p.charAt(j)=='*'){
-//                     if(p.charAt(j-1)!=s.charAt(i) && p.charAt(j-1)!='.'){
-//                         dp[i+1][j+1]=dp[i+1][j-1];
-//                     }
-//                     else{
-//                         dp[i+1][j+1]= dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1];
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[s.length()][p.length()];
-//     }
-// }
-
 class Solution {
-    private Boolean[][] matched;
-    private String string;
-    private String pattern;
-    
     public boolean isMatch(String s, String p) {
-        this.string = s;
-        this.pattern = p;
-        int sLength = string.length(), pLength = pattern.length();
-        this.matched = new Boolean[sLength + 1][pLength + 1];
-        return find(sLength, pLength);
-    }
-    
-    private boolean find(int sIndex, int pIndex) {
-        if (pIndex == 0) return sIndex == 0;
-        if (matched[sIndex][pIndex] != null) return matched[sIndex][pIndex];
-        boolean canBeMade = false;
-        char pChar = pattern.charAt(pIndex - 1);
-        if (pChar == '*') {
-            char prevChar = pattern.charAt(pIndex - 2);
-            canBeMade = find(sIndex, pIndex - 2); // do not consume
-            if (!canBeMade && sIndex > 0) {
-                char sChar = string.charAt(sIndex - 1);
-                if (prevChar == sChar || prevChar == '.') canBeMade = find(sIndex - 1, pIndex); // consume
-            }
-        } else {
-            if (sIndex > 0) {
-                char sChar = string.charAt(sIndex - 1);
-                if (pChar == sChar || pChar == '.') canBeMade = find(sIndex - 1, pIndex - 1);
-                // if characters are equal or wildcard
+        boolean[][] dp= new boolean[s.length()+1][p.length()+1];
+        dp[0][0]=true;
+        for(int i=0; i<p.length(); i++){
+            if(p.charAt(i)=='*' && dp[0][i-1]){
+                dp[0][i+1]=true;
             }
         }
-        return matched[sIndex][pIndex] = canBeMade;
-    }
-    static{
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try(FileWriter writer = new FileWriter("display_runtime.txt")){writer.write('0');}
-            catch(Exception _){}
-        }));
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<p.length(); j++){
+                if(p.charAt(j)=='.'){
+                    dp[i+1][j+1]=dp[i][j];
+                }
+                if(p.charAt(j)==s.charAt(i)){
+                    dp[i+1][j+1]=dp[i][j];
+                }
+                if(p.charAt(j)=='*'){
+                    if(p.charAt(j-1)!=s.charAt(i) && p.charAt(j-1)!='.'){
+                        dp[i+1][j+1]=dp[i+1][j-1];
+                    }
+                    else{
+                        dp[i+1][j+1]= dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 }
