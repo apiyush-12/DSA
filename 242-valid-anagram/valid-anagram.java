8cfe0eb0c36cb922e1c35e241b460befea48e887
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-29 21:24:38 +0530

    Time: 5 ms (58.40%) | Memory: 44.5 MB (62.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,60 +1,18 @@
 class Solution {
-    // public boolean isAnagram(String s, String t) {
-    //     HashMap<Character, Integer> hm=new HashMap<>();
-    //     for(int i=0; i<s.length(); i++){
-    //         char ch=s.charAt(i);
-    //         if(hm.containsKey(ch)==true){
-    //             hm.put(ch, hm.get(ch)+1);
-    //         }else{
-    //             hm.put(ch,1);
-    //         }
-    //     }
-    //     for(int i=0; i<t.length();i++){
-    //         char ch=t.charAt(i);
-    //         if(hm.containsKey(ch)==true){
-    //             if(hm.get(ch)==1){
-    //                 hm.remove(ch);
-    //             }else{
-    //                 hm.put(ch, hm.get(ch)-1);
-    //             }
-    //         }else{
-    //             return false;
-    //         }
-    //     }
-    //     if(hm.size()==0){
-    //         return true;
-    //     }else{
-    //         return false;
-    //     }
-    // }
-
-    // Piyush //
-    public boolean isAnagram(String s, String t){
-        HashMap<Character, Integer> hm=new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char ch=s.charAt(i);
-            if(hm.containsKey(ch)==true){
-                hm.put(ch, hm.get(ch)+1);
-            }else{
-                hm.put(ch, 1);
-            }
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
         }
-        for(int i=0; i<t.length(); i++){
-            char ch=t.charAt(i);
-            if(hm.containsKey(ch)==true){
-                if(hm.get(ch)==1){
-                    hm.remove(ch);
-                }else{
-                    hm.put(ch, hm.get(ch)-1);
-                }
-            }else{
+        int[] count=new int[26];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int C : count){
+            if(C!=0){
                 return false;
             }
         }
-        if(hm.size()==0){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
     }
 }
