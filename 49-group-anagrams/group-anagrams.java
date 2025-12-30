6627a57325a1963681462905af39a803ff3d9493
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-30 16:05:33 +0530

    Time: 7 ms (74.96%) | Memory: 49.4 MB (89.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,23 +1,43 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map <String, List> ans=new HashMap<>();
-        int[] count=new int[26];
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map <String, List> ans=new HashMap<>();
+//         int[] count=new int[26];
+//         for(String s: strs){
+//             Arrays.fill(count, 0);
+//             for(char c:s.toCharArray()){
+//                 count[c-'a']++;
+//             }
+//             StringBuilder sb=new StringBuilder("");
+//             for(int i=0; i<26; i++){
+//                 sb.append("#");
+//                 sb.append(count[i]);
+//             }
+//             String key = sb.toString();
+//             if(!ans.containsKey(key)){
+//                 ans.put(key, new ArrayList());
+//             }
+//             ans.get(key).add(s);
+//         }  
+//         return new ArrayList(ans.values());   
+//     }
+// }
+
+
+
+
+class Solution{
+    public List<List<String>> groupAnagrams(String[] strs){
+        HashMap <String, List<String>> ans=new HashMap<>();
         for(String s: strs){
-            Arrays.fill(count, 0);
-            for(char c:s.toCharArray()){
-                count[c-'a']++;
-            }
-            StringBuilder sb=new StringBuilder("");
-            for(int i=0; i<26; i++){
-                sb.append("#");
-                sb.append(count[i]);
-            }
-            String key = sb.toString();
+            char[] ch=s.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+
             if(!ans.containsKey(key)){
-                ans.put(key, new ArrayList());
+                ans.put(key, new ArrayList<>());
             }
             ans.get(key).add(s);
-        }  
-        return new ArrayList(ans.values());   
+        }
+        return new ArrayList<>(ans.values());
     }
 }
