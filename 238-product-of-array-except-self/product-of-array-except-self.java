5f1b80f14a32fa567c73a197365c8cf77f4787a7
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2025-12-31 23:10:15 +0530

    Time: 2 ms (94.05%) | Memory: 64.2 MB (92.70%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,57 +1,16 @@
 class Solution {
-    // public int[] productExceptSelf(int[] nums) {
-    //     int n=nums.length;
-    //     int[] right=new int [n];
-    //     int pro=1;
-    //     for(int i=n-1; i>=0; i--){
-    //         pro=pro*nums[i];
-    //         right[i]=pro;
-    //     }
-    //     int[] ans=new int[n];
-    //     int left=1;
-    //     for(int i=0; i<n-1; i++){
-    //         int val=left*right[i+1];
-    //         ans[i]=val;
-    //         left=left*nums[i];
-    //     }
-    //     ans[n-1]=left;
-    //     return ans;
-    // }
-
-    // piyush //
-    // public int[] productExceptSelf(int[] nums){
-    //     int n=nums.length;
-    //     int[] right=new int [n];
-    //     int pro=1;
-    //     for(int i=n-1; i>=0; i--){
-    //         pro=pro*nums[i];
-    //         right[i]=pro;
-    //     }
-    //     int [] ans=new int[n];
-    //     int left=1;
-    //     for(int i=0; i<n-1; i++){
-    //         int val=left*right[i+1];
-    //         ans[i]=val;
-    //         left=left*nums[i];
-    //     }
-    //     ans[n-1]=left;
-    //     return ans;
-    // }
-
-    // piyush
-    public int[] productExceptSelf(int[] nums){
+    public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int[] right=new int[n];
         int pro=1;
+        int[] right=new int[n];
         for(int i=n-1; i>=0; i--){
             pro=pro*nums[i];
             right[i]=pro;
         }
-        int [] ans=new int[n];
+        int[] ans=new int[n];
         int left=1;
         for(int i=0; i<n-1; i++){
-            int val=left*right[i+1];
-            ans[i]=val;
+            ans[i]=left*right[i+1];
             left=left*nums[i];
         }
         ans[n-1]=left;
