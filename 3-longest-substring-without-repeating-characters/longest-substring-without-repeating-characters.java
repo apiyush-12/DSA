f72cb89fd7e3d86cc949d7fd889f7eae456753c7
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-04 17:34:13 +0530

    Time: 6 ms (67.20%) | Memory: 46.4 MB (37.37%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,43 +1,43 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if(s==null || s.length()==0){
-//             return 0;
-//         }
-//         if(s.length()==1){
-//             return 1;
-//         }
-//         int left=0;
-//         int right=0;
-//         int ans=0;
-//         HashSet <Character> set=new HashSet<>();
-//         while(right<s.length()){
-//             char ch=s.charAt(right);
-//             while(set.contains(ch)){
-//                 set.remove(s.charAt(left));
-//                 left++;
-//             }
-//             set.add(ch);
-//             ans=Math.max(ans, right-left+1);
-//             right++;
-//         }
-//         return ans;
-//     }
-// }
-
-
-class Solution{
-    public int lengthOfLongestSubstring(String s){
-        HashMap<Character, Integer> map=new HashMap<>();
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // if(s==null || s.length()==0){
+        //     return 0;
+        // }
+        // if(s.length()==1){
+        //     return 1;
+        // }
         int left=0;
+        int right=0;
         int ans=0;
-        for(int right=0; right<s.length(); right++){
+        HashSet <Character> set=new HashSet<>();
+        while(right<s.length()){
             char ch=s.charAt(right);
-            if(map.containsKey(ch)){
-                left=Math.max(left, map.get(ch)+1);
+            while(set.contains(ch)){
+                set.remove(s.charAt(left));
+                left++;
             }
-            map.put(ch, right);
+            set.add(ch);
             ans=Math.max(ans, right-left+1);
+            right++;
         }
         return ans;
     }
 }
+
+
+// class Solution{
+//     public int lengthOfLongestSubstring(String s){
+//         HashMap<Character, Integer> map=new HashMap<>();
+//         int left=0;
+//         int ans=0;
+//         for(int right=0; right<s.length(); right++){
+//             char ch=s.charAt(right);
+//             if(map.containsKey(ch)){
+//                 left=Math.max(left, map.get(ch)+1);
+//             }
+//             map.put(ch, right);
+//             ans=Math.max(ans, right-left+1);
+//         }
+//         return ans;
+//     }
+// }
