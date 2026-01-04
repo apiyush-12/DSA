cb95bc3b66597f58dc77abb3a980101e52df253b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-04 17:28:26 +0530

    Time: 5 ms (85.27%) | Memory: 44.7 MB (91.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,48 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s==null || s.length()==0){
+//             return 0;
+//         }
+//         if(s.length()==1){
+//             return 1;
+//         }
+//         int left=0;
+//         int right=0;
+//         int ans=0;
+//         HashSet <Character> set=new HashSet<>();
+//         while(right<s.length()){
+//             char ch=s.charAt(right);
+//             while(set.contains(ch)){
+//                 set.remove(s.charAt(left));
+//                 left++;
+//             }
+//             set.add(ch);
+//             ans=Math.max(ans, right-left+1);
+//             right++;
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution{
+    public int lengthOfLongestSubstring(String s){
         if(s==null || s.length()==0){
             return 0;
         }
         if(s.length()==1){
             return 1;
         }
+        HashMap<Character, Integer> map=new HashMap<>();
         int left=0;
-        int right=0;
         int ans=0;
-        HashSet <Character> set=new HashSet<>();
-        while(right<s.length()){
+        for(int right=0; right<s.length(); right++){
             char ch=s.charAt(right);
-            while(set.contains(ch)){
-                set.remove(s.charAt(left));
-                left++;
+            if(map.containsKey(ch)){
+                left=Math.max(left, map.get(ch)+1);
             }
-            set.add(ch);
+            map.put(ch, right);
             ans=Math.max(ans, right-left+1);
-            right++;
         }
         return ans;
     }
