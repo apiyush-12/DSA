1bd4e74f8a2cab7feee9ba32157bb643eccb63a9
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-09 22:48:14 +0530

    Time: 4 ms (42.80%) | Memory: 43.3 MB (42.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,43 @@
-class Solution {
-    public boolean isValid(String s) {
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack<>();
+//         for(int i=0; i<s.length(); i++){
+//             char ch=s.charAt(i);
+//             if(ch=='('|| ch=='{'||ch=='['){
+//                 stack.push(ch);
+//             }else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
+//                 stack.pop();
+//             }else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
+//                 stack.pop();
+//             }else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
+//                 stack.pop();
+//             }else{
+//                 return false;
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
+class Solution{
+    public boolean isValid(String s){
+        HashMap<Character, Character> mapB=new HashMap<>();
+        mapB.put(')','(');
+        mapB.put('}','{');
+        mapB.put(']','[');
         Stack<Character> stack=new Stack<>();
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            if(ch=='('|| ch=='{'||ch=='['){
+            if(!mapB.containsKey(ch)){
                 stack.push(ch);
-            }else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
-                stack.pop();
-            }else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
-                stack.pop();
-            }else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
-                stack.pop();
             }else{
-                return false;
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char topEle=stack.pop();
+                if(topEle!=mapB.get(ch)){
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
