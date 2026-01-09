a84e901e4bfc526ba0c3b0f201465826040ae4ea
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-09 22:48:58 +0530

    Time: 3 ms (87.48%) | Memory: 43.7 MB (8.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,45 +1,45 @@
-// class Solution {
-//     public boolean isValid(String s) {
-//         Stack<Character> stack=new Stack<>();
-//         for(int i=0; i<s.length(); i++){
-//             char ch=s.charAt(i);
-//             if(ch=='('|| ch=='{'||ch=='['){
-//                 stack.push(ch);
-//             }else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
-//                 stack.pop();
-//             }else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
-//                 stack.pop();
-//             }else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
-//                 stack.pop();
-//             }else{
-//                 return false;
-//             }
-//         }
-//         return stack.isEmpty();
-//     }
-// }
-
-class Solution{
-    public boolean isValid(String s){
-        HashMap<Character, Character> mapB=new HashMap<>();
-        mapB.put(')','(');
-        mapB.put('}','{');
-        mapB.put(']','[');
+class Solution {
+    public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            if(!mapB.containsKey(ch)){
+            if(ch=='('|| ch=='{'||ch=='['){
                 stack.push(ch);
+            }else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
+                stack.pop();
+            }else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
+                stack.pop();
+            }else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
+                stack.pop();
             }else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                char topEle=stack.pop();
-                if(topEle!=mapB.get(ch)){
-                    return false;
-                }
+                return false;
             }
         }
         return stack.isEmpty();
     }
 }
+
+// class Solution{
+//     public boolean isValid(String s){
+//         HashMap<Character, Character> mapB=new HashMap<>();
+//         mapB.put(')','(');
+//         mapB.put('}','{');
+//         mapB.put(']','[');
+//         Stack<Character> stack=new Stack<>();
+//         for(int i=0; i<s.length(); i++){
+//             char ch=s.charAt(i);
+//             if(!mapB.containsKey(ch)){
+//                 stack.push(ch);
+//             }else{
+//                 if(stack.isEmpty()){
+//                     return false;
+//                 }
+//                 char topEle=stack.pop();
+//                 if(topEle!=mapB.get(ch)){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
