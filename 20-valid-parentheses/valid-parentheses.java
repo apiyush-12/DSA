6f4dbe52de39bb629ca1d9767a32594335c9a25b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-09 21:56:04 +0530

    Time: 3 ms (87.48%) | Memory: 43.3 MB (59.17%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,19 +3,15 @@ class Solution {
         Stack<Character> stack=new Stack<>();
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
+            if(ch=='('|| ch=='{'||ch=='['){
                 stack.push(ch);
-            }
-            else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
+            }else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
                 stack.pop();
-            }
-            else if (!stack.isEmpty() && ch==']' && stack.peek()=='['){
+            }else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
                 stack.pop();
-            }
-            else if (!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
+            }else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
                 stack.pop();
-            }
-            else {
+            }else{
                 return false;
             }
         }
