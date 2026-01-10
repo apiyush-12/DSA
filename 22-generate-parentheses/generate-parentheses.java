2875a0ea1432ce5d9f19c067b4b602a293b0a82f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-10 16:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (87.02%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -25,13 +25,37 @@
 
 
 // piyush
+// class Solution{
+//     public List<String> generateParenthesis(int n){
+//         List<String> ans=new ArrayList();
+//         backTrack(ans, new StringBuilder(), 0, 0, n);
+//         return ans;
+//     }
+
+//     public void backTrack(List<String> ans, StringBuilder curr, int open, int close, int max){
+//         if(curr.length()==max*2){
+//             ans.add(curr.toString());
+//             return;
+//         }
+//         if(open<max){
+//             curr.append("(");
+//             backTrack(ans, curr, open+1, close, max);
+//             curr.deleteCharAt(curr.length()-1);
+//         }
+//         if(close<open){
+//             curr.append(")");
+//             backTrack(ans, curr, open, close+1, max);
+//             curr.deleteCharAt(curr.length()-1);
+//         }
+//     }
+// }
+
 class Solution{
     public List<String> generateParenthesis(int n){
         List<String> ans=new ArrayList();
         backTrack(ans, new StringBuilder(), 0, 0, n);
         return ans;
     }
-
     public void backTrack(List<String> ans, StringBuilder curr, int open, int close, int max){
         if(curr.length()==max*2){
             ans.add(curr.toString());
