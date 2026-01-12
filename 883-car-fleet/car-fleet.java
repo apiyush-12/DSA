0bdd67d5f230daa6f57599457c2e2f7c3a156f67
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-12 20:03:40 +0530

    Time: 66 ms (90.08%) | Memory: 122 MB (6.41%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,20 +1,40 @@
-class Solution {
-    public int carFleet(int target, int[] position, int[] speed) {
+// class Solution {
+//     public int carFleet(int target, int[] position, int[] speed) {
+//         int n=position.length;
+//         double[][] cars=new double[n][2];
+//         for(int i=0; i<n; i++){
+//             cars[i][0]=position[i];
+//             cars[i][1]=(double)(target-position[i])/speed[i];
+//         }
+//         Arrays.sort(cars, (a,b)->Double.compare(b[0], a[0]));
+//         int count=0;
+//         double prevTime=0;
+//         for(double[] car: cars){
+//             if(car[1]>prevTime){
+//                 count++;
+//                 prevTime=car[1];
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+class Solution{
+    public int carFleet(int target, int[] position, int[] speed){
+        HashMap<Integer, Integer> hs=new HashMap<>();
         int n=position.length;
-        double[][] cars=new double[n][2];
+        Stack<Double> st=new Stack<>();
         for(int i=0; i<n; i++){
-            cars[i][0]=position[i];
-            cars[i][1]=(double)(target-position[i])/speed[i];
+            hs.put(position[i],speed[i]);
         }
-        Arrays.sort(cars, (a,b)->Double.compare(b[0], a[0]));
-        int count=0;
-        double prevTime=0;
-        for(double[] car: cars){
-            if(car[1]>prevTime){
-                count++;
-                prevTime=car[1];
+        Arrays.sort(position);
+        for(int i=n-1; i>=0; i--){
+            double time=(double)(target-position[i])/hs.get(position[i]);
+            if(st.isEmpty() || time> st.peek()){
+                st.add(time);
             }
         }
-        return count;
+        return st.size();
     }
 }
