685b3ccb84451979a3edc70d088977232cfe12cf
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-13 21:43:43 +0530

    Time: 20 ms (94.46%) | Memory: 85.2 MB (13.56%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,17 +1,49 @@
+// class Solution {
+//     public int largestRectangleArea(int[] heights) {
+//         int maxArea=0;
+//         Stack<Integer> stack=new Stack<>();
+//         int n=heights.length;
+//         for(int i=0; i<=n; i++){
+//             int currH=(i==n) ? 0 : heights[i];
+//             while(!stack.isEmpty() && currH < heights[stack.peek()]){
+//                 int height=heights[stack.pop()];
+//                 int width=stack.isEmpty() ? i : i-stack.peek()-1;
+//                 maxArea=Math.max(maxArea, height*width);
+//             }
+//             stack.push(i);
+//         }
+//         return maxArea;
+//     }
+// }
+
+// piyush
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int maxArea=0;
-        Stack<Integer> stack=new Stack<>();
-        int n=heights.length;
-        for(int i=0; i<=n; i++){
-            int currH=(i==n) ? 0 : heights[i];
-            while(!stack.isEmpty() && currH < heights[stack.peek()]){
-                int height=heights[stack.pop()];
-                int width=stack.isEmpty() ? i : i-stack.peek()-1;
-                maxArea=Math.max(maxArea, height*width);
+    public static int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Deque<int[]> stack = new ArrayDeque<>(); 
+
+        for (int i = 0; i < heights.length; i++) {
+            int start = i;
+
+            
+            while (!stack.isEmpty() && heights[i] < stack.peek()[0]) {
+                int[] top = stack.pop();
+                int height = top[0];
+                int index = top[1];
+                maxArea = Math.max(maxArea, height * (i - index));
+                start = index; 
             }
-            stack.push(i);
+
+            stack.push(new int[]{heights[i], start});
         }
+
+          while (!stack.isEmpty()) {
+            int[] top = stack.pop();
+            int height = top[0];
+            int index = top[1];
+            maxArea = Math.max(maxArea, height * (heights.length - index));
+        }
+
         return maxArea;
     }
 }
