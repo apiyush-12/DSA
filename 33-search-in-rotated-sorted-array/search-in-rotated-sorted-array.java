b47e7faf51739098d445f1c2f2eb4bfb838a6d53
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-15 17:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (96.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }else if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && nums[mid]>=target){
+                if(nums[start]<=target && nums[mid]>target){
                     end=mid-1;
                 }else{
                     start=mid+1;
