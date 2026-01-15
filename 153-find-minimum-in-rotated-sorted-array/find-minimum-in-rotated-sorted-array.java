4ff0525fe9fc00e0814091ad8c1f45cd01260f36
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-15 16:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (60.73%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,48 +1,23 @@
 class Solution {
-    // public int findMin(int[] nums) {
-    //     if(nums.length==1){
-    //         return nums[0];
-    //     }else if(nums[0]<nums[nums.length-1]){
-    //         return nums[0];
-    //     }
-
-    //     int start=0;
-    //     int end=nums.length-1;
-    //     while(start<=end){
-    //         int mid=(start+end)/2;
-    //         if(mid!=0 && nums[mid-1]>nums[mid]){
-    //             return nums[mid];
-    //         }else if(mid!=nums.length-1 && nums[mid]>nums[mid+1]){
-    //             return nums[mid+1];
-    //         }else if(nums[start]<=nums[mid]){
-    //             start =mid+1;
-    //         }else{
-    //             end=mid-1;
-    //         }
-    //     }
-    //     return -1;
-    // }
-
-    // piyush //
-    public int findMin(int[] nums){
+    public int findMin(int[] nums) {
         if(nums.length==1){
             return nums[0];
-        }else if(nums[0]<nums[nums.length-1]){
+        }
+        if(nums[0]<nums[nums.length-1]){
             return nums[0];
         }
-
         int start=0;
         int end=nums.length-1;
-        while(start<=end){
-            int mid=(start+end)/2;
+        for(int i=0; i<nums.length; i++){
+            int mid= start+(end-start)/2;
             if(mid!=0 && nums[mid]<nums[mid-1]){
                 return nums[mid];
             }else if(mid!=nums.length-1 && nums[mid+1]<nums[mid]){
                 return nums[mid+1];
-            }else if(nums[start]<=nums[mid]){
-                start=mid+1;
+            }else if(nums[start]>nums[end]){
+                end--;
             }else{
-                end=mid-1;
+                start++;
             }
         }
         return -1;
