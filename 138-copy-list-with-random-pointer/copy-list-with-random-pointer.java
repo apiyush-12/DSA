a4c8eb235f8a7b62fd3adee6abc3a8d72963a82e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-20 00:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (7.13%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -13,9 +13,26 @@ class Node {
 }
 */
 
-class Solution {
+// class Solution {
+//     HashMap<Node, Node> visitedNode=new HashMap<Node, Node>();
+//     public Node copyRandomList(Node head) {
+//         if(head==null){
+//             return null;
+//         }
+//         if(this.visitedNode.containsKey(head)){
+//             return this.visitedNode.get(head);
+//         }
+//         Node node=new Node(head.val, null, null);
+//         this.visitedNode.put(head, node);
+//         node.next=copyRandomList(head.next);
+//         node.random=copyRandomList(head.random);
+//         return node;
+//     }
+// }
+
+class Solution{
     HashMap<Node, Node> visitedNode=new HashMap<Node, Node>();
-    public Node copyRandomList(Node head) {
+    public Node copyRandomList(Node head){
         if(head==null){
             return null;
         }
