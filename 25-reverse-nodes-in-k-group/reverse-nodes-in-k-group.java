8bb9c4868cdeefedfe23253f6db517555c52648e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-23 01:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (64.89%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -8,8 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
+// class Solution {
+//     public ListNode reverseKGroup(ListNode head, int k) {
+//         ListNode ptr=head;
+//         ListNode ktail=null;
+//         ListNode newHead=null;
+//         while(ptr!=null){
+//             int count=0;
+//             ptr=head;
+//             while(count<k && ptr!=null){
+//                 ptr=ptr.next;
+//                 count++;
+//             }
+//             if(count==k){
+//                 ListNode revHead=reverseLL(head, k);
+//                 if(newHead==null){
+//                     newHead=revHead;
+//                 }
+//                 if(ktail!=null){
+//                     ktail.next=revHead;
+//                 }
+//                 ktail=head;
+//                 head=ptr;
+//             }
+//         }
+//         if(ktail!=null){
+//             ktail.next=head;
+//         }
+//         return newHead==null ? head: newHead;
+//     }
+//     public ListNode reverseLL(ListNode head, int k){
+//         ListNode newHead=null;
+//         ListNode ptr=head;
+//         while(k>0){
+//             ListNode nextNode=ptr.next;
+//             ptr.next=newHead;
+//             newHead=ptr;
+//             ptr=nextNode;
+//             k--;
+//         }
+//         return newHead;
+//     }
+// }
+
+
+class Solution{
+    public ListNode reverseKGroup(ListNode head, int k){
         ListNode ptr=head;
         ListNode ktail=null;
         ListNode newHead=null;
@@ -35,7 +79,7 @@ class Solution {
         if(ktail!=null){
             ktail.next=head;
         }
-        return newHead==null ? head: newHead;
+        return newHead ==null ? head : newHead;
     }
     public ListNode reverseLL(ListNode head, int k){
         ListNode newHead=null;
