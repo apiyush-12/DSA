295998bdb33b7aa4dddf82cee8f3cc2677b01d04
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-22 23:39:48 +0530

    Time: 5 ms (43.45%) | Memory: 47.1 MB (20.52%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,21 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
-        for(ListNode list : lists){
-            while(list!=null){
-                minHeap.add(list.val);
-                list=list.next;
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+//         for(ListNode list : lists){
+//             while(list!=null){
+//                 minHeap.add(list.val);
+//                 list=list.next;
+//             }
+//         }
+//         ListNode dummy=new ListNode(1);
+//         ListNode merge=dummy;
+//         while(!minHeap.isEmpty()){
+//             merge.next=new ListNode(minHeap.remove());
+//             merge=merge.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+class Solution{
+    public ListNode mergeKLists(ListNode[] lists){
+        if(lists.length==0){
+            return null;
+        }else if(lists.length==1){
+            return lists[0];
+        }
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(int i=0; i<lists.length; i++){
+            ListNode tempH=lists[i];
+            while(tempH!=null){
+                pq.add(tempH);
+                tempH=tempH.next;
             }
         }
-        ListNode dummy=new ListNode(1);
-        ListNode merge=dummy;
-        while(!minHeap.isEmpty()){
-            merge.next=new ListNode(minHeap.remove());
-            merge=merge.next;
+        ListNode dummy=new ListNode(-1);
+        ListNode ans=dummy;
+        while(pq.size()>0){
+            dummy.next=pq.remove();
+            dummy=dummy.next;
         }
-        return dummy.next;
+        dummy.next=null;
+        return ans.next;
     }
 }
