678dbee7c03c895e04519bbe75d9195da07bfec5
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-25 16:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (90.60%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -13,8 +13,23 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p==null && q==null){
+//             return true;
+//         }
+//         if(p==null || q==null){
+//             return false;
+//         }
+//         if(p.val!=q.val){
+//             return false;
+//         }
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
+
+class Solution{
+    public boolean isSameTree(TreeNode p, TreeNode q){
         if(p==null && q==null){
             return true;
         }
@@ -27,3 +42,10 @@ class Solution {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+
+
+
+
+
+
