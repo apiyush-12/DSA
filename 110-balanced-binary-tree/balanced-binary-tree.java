b87b3d20a0a1a6a39c87189c786c5756b5f2dbb7
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-25 16:13:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (69.14%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -36,24 +36,44 @@
 // }
 
 // piyush
+// class Solution{
+//     public boolean isBalanced(TreeNode root){
+//         if(root==null){
+//             return true;
+//         }
+//         int leftH=getH(root.left);
+//         int rightH=getH(root.right);
+//         if(Math.abs(leftH-rightH)>1){
+//             return false;
+//         }
+//         return isBalanced(root.left) && isBalanced(root.right);
+//     }
+//     private int getH(TreeNode node){
+//         if(node==null){
+//             return 0;
+//         }
+//         int left=getH(node.left);
+//         int right=getH(node.right);
+//         return Math.max(left, right) +1;
+//     }
+// }
+
+
 class Solution{
-    public boolean isBalanced(TreeNode root){
+    boolean balanceFactor=true;
+    public int height(TreeNode root){
         if(root==null){
-            return true;
-        }
-        int leftH=getH(root.left);
-        int rightH=getH(root.right);
-        if(Math.abs(leftH-rightH)>1){
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
-    }
-    private int getH(TreeNode node){
-        if(node==null){
             return 0;
         }
-        int left=getH(node.left);
-        int right=getH(node.right);
-        return Math.max(left, right) +1;
+        int lH=height(root.left);
+        int rH=height(root.right);
+        if(Math.abs(lH-rH)>1){
+            balanceFactor=false;
+        }
+        return Math.max(lH, rH)+1;
+    }
+    public boolean isBalanced(TreeNode root){
+        int h=height(root);
+        return balanceFactor;
     }
 }
