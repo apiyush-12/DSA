11b1268153207acf0326d0ca385c39d566a1d1e9
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-26 19:31:40 +0530

    Time: 1 ms (72.83%) | Memory: 43.7 MB (60.11%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,29 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result=new ArrayList<>();
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> result=new ArrayList<>();
+//         if(root==null){
+//             return result;
+//         }
+//         Queue<TreeNode> queue=new LinkedList<>();
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             int levelSize=queue.size();
+//             for(int i=0; i<levelSize; i++){
+//                 TreeNode currNode=queue.poll();
+//                 if(i==levelSize-1){
+//                     result.add(currNode.val);
+//                 }
+//                 if(currNode.left!=null){
+//                     queue.add(currNode.left);
+//                 }
+//                 if(currNode.right!=null){
+//                     queue.add(currNode.right);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
+class Solution{
+    public List<Integer> rightSideView(TreeNode root){
+        List<Integer> ans=new ArrayList<>();
+        helper(root, ans, 0);
+        return ans;
+    }
+    public void helper(TreeNode root, List<Integer> ans, int level){
         if(root==null){
-            return result;
+            return;
         }
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int levelSize=queue.size();
-            for(int i=0; i<levelSize; i++){
-                TreeNode currNode=queue.poll();
-                if(i==levelSize-1){
-                    result.add(currNode.val);
-                }
-                if(currNode.left!=null){
-                    queue.add(currNode.left);
-                }
-                if(currNode.right!=null){
-                    queue.add(currNode.right);
-                }
-            }
+        if(ans.size()==level){
+            ans.add(root.val);
         }
-        return result;
+        helper(root.right, ans, level+1);
+        helper(root.left, ans, level+1);
     }
 }
