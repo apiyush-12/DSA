30affd724fa7b9cf58ec5d6e6043c30902399c31
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-28 22:29:38 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (12.33%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -13,18 +13,42 @@
  *     }
  * }
  */
-class Solution {
-    public ArrayList<Integer> inOrder(TreeNode root, ArrayList<Integer> arr){
-        if(root==null){
-            return arr;
-        }
-        inOrder(root.left, arr);
-        arr.add(root.val);
-        inOrder(root.right, arr);
-        return arr;
+// class Solution {
+//     public ArrayList<Integer> inOrder(TreeNode root, ArrayList<Integer> arr){
+//         if(root==null){
+//             return arr;
+//         }
+//         inOrder(root.left, arr);
+//         arr.add(root.val);
+//         inOrder(root.right, arr);
+//         return arr;
+//     }
+//     public int kthSmallest(TreeNode root, int k) {
+//         ArrayList<Integer> nums=inOrder(root, new ArrayList<Integer>());
+//         return nums.get(k-1);
+//     }
+// }
+
+class Solution{
+    int count=0;
+    int ans=0;
+    public int kthSmallest(TreeNode root, int k){
+        helper(root, k);
+        return ans;
     }
-    public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> nums=inOrder(root, new ArrayList<Integer>());
-        return nums.get(k-1);
+    public void helper(TreeNode root, int k){
+        if(root==null){
+            return;
+        }
+        if(count>=k){
+            return;
+        }
+        helper(root.left, k);
+        count=count+1;
+        if(count==k){
+            ans=root.val;
+            return;
+        }
+        helper(root.right, k);
     }
 }
