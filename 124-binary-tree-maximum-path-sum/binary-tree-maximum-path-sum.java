8a8e41ef51ad8777e21bcc6aaf8cc32e0d234df3
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-01-31 23:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (30.96%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -33,16 +33,34 @@
 
 
 // piyush
+// class Solution{
+//     int maxSum=Integer.MIN_VALUE;
+//     public int maxGain(TreeNode node){
+//         if(node==null){
+//             return 0;
+//         }
+//         int leftGain=Math.max(maxGain(node.left), 0);
+//         int rightGain=Math.max(maxGain(node.right), 0);
+//         int newPath=node.val+leftGain+rightGain;
+//         maxSum=Math.max(maxSum, newPath);
+//         return node.val+Math.max(leftGain, rightGain);
+//     }
+//     public int maxPathSum(TreeNode root){
+//         maxGain(root);
+//         return maxSum;
+//     }
+// }
+
 class Solution{
     int maxSum=Integer.MIN_VALUE;
     public int maxGain(TreeNode node){
         if(node==null){
             return 0;
         }
-        int leftGain=Math.max(maxGain(node.left), 0);
-        int rightGain=Math.max(maxGain(node.right), 0);
-        int newPath=node.val+leftGain+rightGain;
-        maxSum=Math.max(maxSum, newPath);
+        int leftGain=Math.max(maxGain(node.left),0);
+        int rightGain=Math.max(maxGain(node.right),0);
+        int newGain=node.val+leftGain+rightGain;
+        maxSum=Math.max(maxSum, newGain);
         return node.val+Math.max(leftGain, rightGain);
     }
     public int maxPathSum(TreeNode root){
