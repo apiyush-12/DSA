e9ef887c8a061bd35f3b66e9f4a09482e2a6327f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-01 20:27:47 +0530

    Time: 30 ms (94.92%) | Memory: 61.9 MB (81.90%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -22,53 +22,53 @@
 //     }
 // }
 
-class TrieNode{
-    TrieNode[] children;
-    boolean isEnd;
-    public TrieNode() {
-        children =new TrieNode[26];
-        isEnd=false;
-    }
-}
+// class TrieNode{
+//     TrieNode[] children;
+//     boolean isEnd;
+//     public TrieNode() {
+//         children =new TrieNode[26];
+//         isEnd=false;
+//     }
+// }
 
-class Trie {
-    private TrieNode root;
-    public Trie() {
-        root=new TrieNode();
-    }
+// class Trie {
+//     private TrieNode root;
+//     public Trie() {
+//         root=new TrieNode();
+//     }
+
+//     public void insert(String word) {
+//         TrieNode node=root;
+//         for(char c : word.toCharArray()){
+//             int index=c-'a';
+//             if(node.children[index]==null){
+//                 node.children[index]=new TrieNode();
+//             }
+//             node=node.children[index];
+//         }
+//         node.isEnd=true;
+//     }
+//     public TrieNode searchPrefix(String prefix){
+//         TrieNode node=root;
+//         for(char c : prefix.toCharArray()){
+//             int index=c-'a';
+//             if(node.children[index]==null){
+//                 return null;
+//             }
+//             node=node.children[index];
+//         }
+//         return node;
+//     }
     
-    public void insert(String word) {
-        TrieNode node=root;
-        for(char c : word.toCharArray()){
-            int index=c-'a';
-            if(node.children[index]==null){
-                node.children[index]=new TrieNode();
-            }
-            node=node.children[index];
-        }
-        node.isEnd=true;
-    }
-    public TrieNode searchPrefix(String prefix){
-        TrieNode node=root;
-        for(char c : prefix.toCharArray()){
-            int index=c-'a';
-            if(node.children[index]==null){
-                return null;
-            }
-            node=node.children[index];
-        }
-        return node;
-    }
+//     public boolean search(String word) {
+//         TrieNode node=searchPrefix(word);
+//         return node!=null && node.isEnd;
+//     }
     
-    public boolean search(String word) {
-        TrieNode node=searchPrefix(word);
-        return node!=null && node.isEnd;
-    }
-    
-    public boolean startsWith(String prefix) {
-        return searchPrefix(prefix)!=null;
-    }
-}
+//     public boolean startsWith(String prefix) {
+//         return searchPrefix(prefix)!=null;
+//     }
+// }
 
 /**
  * Your Trie object will be instantiated and called as such:
@@ -77,3 +77,54 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+ class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+    
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+    
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
