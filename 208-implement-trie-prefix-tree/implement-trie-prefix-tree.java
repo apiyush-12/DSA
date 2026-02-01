1b59e56f51e675b4c950e45ff272966558ef243e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-01 20:25:55 +0530

    Time: 33 ms (66.60%) | Memory: 62.1 MB (62.17%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,24 +1,33 @@
+// class TrieNode{
+//     private TrieNode[] links;
+//     private final int R=26;
+//     private boolean isEnd;
+//     public TrieNode(){
+//         links=new TrieNode[R];
+//     }
+//     public boolean containsKey(char ch){
+//         return links[ch-'a']!=null;
+//     }
+//     public TrieNode get(char ch){
+//         return links[ch-'a'];
+//     }
+//     public void put(char ch, TrieNode node){
+//         links[ch-'a']=node;
+//     }
+//     public void setEnd(){
+//         isEnd=true;
+//     }
+//     public boolean isEnd(){
+//         return isEnd;
+//     }
+// }
+
 class TrieNode{
-    private TrieNode[] links;
-    private final int R=26;
-    private boolean isEnd;
-    public TrieNode(){
-        links=new TrieNode[R];
-    }
-    public boolean containsKey(char ch){
-        return links[ch-'a']!=null;
-    }
-    public TrieNode get(char ch){
-        return links[ch-'a'];
-    }
-    public void put(char ch, TrieNode node){
-        links[ch-'a']=node;
-    }
-    public void setEnd(){
-        isEnd=true;
-    }
-    public boolean isEnd(){
-        return isEnd;
+    TrieNode[] children;
+    boolean isEnd;
+    public TrieNode() {
+        children =new TrieNode[26];
+        isEnd=false;
     }
 }
 class Trie {
@@ -29,32 +38,30 @@ class Trie {
     
     public void insert(String word) {
         TrieNode node=root;
-        for(int i=0; i<word.length(); i++){
-            char currentChar=word.charAt(i);
-            if(!node.containsKey(currentChar)){
-                node.put(currentChar, new TrieNode());
+        for(char c : word.toCharArray()){
+            int index=c-'a';
+            if(node.children[index]==null){
+                node.children[index]=new TrieNode();
             }
-            node=node.get(currentChar);
+            node=node.children[index];
         }
-        node.setEnd();
+        node.isEnd=true;
     }
-    public TrieNode searchPrefix(String word){
+    public TrieNode searchPrefix(String prefix){
         TrieNode node=root;
-        for(int i=0; i<word.length(); i++){
-            char currentChar=word.charAt(i);
-            if(node.containsKey(currentChar)){
-                node=node.get(currentChar);
-            }
-            else{
+        for(char c : prefix.toCharArray()){
+            int index=c-'a';
+            if(node.children[index]==null){
                 return null;
             }
+            node=node.children[index];
         }
         return node;
     }
     
     public boolean search(String word) {
         TrieNode node=searchPrefix(word);
-        return node!=null && node.isEnd();
+        return node!=null && node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
