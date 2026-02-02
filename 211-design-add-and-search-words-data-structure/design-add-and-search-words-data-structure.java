e601134553521dc7e649be1bfa02336ec2617fab
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-02 22:28:19 +0530

    Time: 173 ms (88.90%) | Memory: 277 MB (27.29%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -53,53 +53,117 @@
  */
 
 
- class TrieNode {
-    Map<Character, TrieNode> children = new HashMap<>();
-    boolean word = false;
-}
+//  class TrieNode {
+//     Map<Character, TrieNode> children = new HashMap<>();
+//     boolean word = false;
+// }
+
+// class WordDictionary {
+//     TrieNode trie;
+
+//     public WordDictionary() {
+//         trie = new TrieNode();
+//     }
+
+//     public void addWord(String word) {
+//         TrieNode node = trie;
+//         for (char ch : word.toCharArray()) {
+//             node.children.putIfAbsent(ch, new TrieNode());
+//             node = node.children.get(ch);
+//         }
+//         node.word = true;
+//     }
+
+//     public boolean search(String word) {
+//         return dfs(word, 0, trie);
+//     }
+
+//     private boolean dfs(String word, int index, TrieNode node) {
+//         if (index == word.length()) {
+//             return node.word;
+//         }
+
+//         char ch = word.charAt(index);
+
+//         // Case 1: Wildcard '.'
+//         if (ch == '.') {
+//             for (TrieNode child : node.children.values()) {
+//                 if (dfs(word, index + 1, child)) {
+//                     return true;
+//                 }
+//             }
+//             return false;
+//         }
+
+//         // Case 2: Normal character
+//         if (!node.children.containsKey(ch)) {
+//             return false;
+//         }
+
+//         return dfs(word, index + 1, node.children.get(ch));
+//     }
+// }
+
 
 class WordDictionary {
-    TrieNode trie;
-
+    private class Trie{
+        Trie links[];
+        boolean flag;
+        Trie(){
+            links = new Trie[26];
+            flag = false;
+        }
+        boolean containsChar(char c){
+            return links[c-'a']!=null;
+        }
+        void put(char c,Trie node){
+            links[c-'a'] = node;
+        }
+        Trie get(char c){
+            return links[c-'a'];
+        }
+    }
+    Trie solve;
+    String search;
     public WordDictionary() {
-        trie = new TrieNode();
+        solve = new Trie();
+        search = "";
     }
-
+    
     public void addWord(String word) {
-        TrieNode node = trie;
-        for (char ch : word.toCharArray()) {
-            node.children.putIfAbsent(ch, new TrieNode());
-            node = node.children.get(ch);
+        Trie node = solve;
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            if(!node.containsChar(c))
+                node.put(c,new Trie());
+            node = node.get(c);
         }
-        node.word = true;
+        node.flag = true;
     }
-
+    
     public boolean search(String word) {
-        return dfs(word, 0, trie);
+        search = word;
+        return searchWord(solve,0);
     }
 
-    private boolean dfs(String word, int index, TrieNode node) {
-        if (index == word.length()) {
-            return node.word;
-        }
+    private boolean searchWord(Trie node, int indx) {
+        if (indx == search.length())
+            return node.flag;
 
-        char ch = word.charAt(index);
+        char c = search.charAt(indx);
 
-        // Case 1: Wildcard '.'
-        if (ch == '.') {
-            for (TrieNode child : node.children.values()) {
-                if (dfs(word, index + 1, child)) {
-                    return true;
+        if (c == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (node.links[i] != null) {
+                    if (searchWord(node.links[i], indx + 1))
+                        return true;
                 }
             }
             return false;
         }
-
-        // Case 2: Normal character
-        if (!node.children.containsKey(ch)) {
+        if (!node.containsChar(c))
             return false;
-        }
-
-        return dfs(word, index + 1, node.children.get(ch));
+            
+        return searchWord(node.get(c), indx + 1);
     }
 }
