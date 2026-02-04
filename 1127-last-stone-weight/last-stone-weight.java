10e8ef2640aee94a922f7ea0d4c227a6619e0e2d
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-04 22:10:17 +0530

    Time: 1 ms (99.36%) | Memory: 43 MB (27.57%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -12,11 +12,12 @@ class Solution {
                 pq.add(nStone);
             }
         }    
-        if(pq.size()==0){
-            return 0;
-        }
-        else{
-           return pq.peek();
-        }   
+        // if(pq.size()==0){
+        //     return 0;
+        // }
+        // else{
+        //    return pq.peek();
+        // }  
+        return pq.isEmpty() ? 0 : pq.peek(); 
     }
 }
