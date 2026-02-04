f8c5fb169dc8753a52eddaeac5af22fbad5482c0
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-04 21:37:09 +0530

    Time: 32 ms (6.60%) | Memory: 52.1 MB (84.97%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,27 +1,47 @@
-class KthLargest {
-    PriorityQueue<Integer> minHeap;
-    int k;
-    public KthLargest(int k, int[] nums) {
-        this.k=k;
-        this.minHeap=new PriorityQueue<>(k);
-        for(int num : nums){
-            add(num);
-        }
-    }
+// class KthLargest {
+//     PriorityQueue<Integer> minHeap;
+//     int k;
+//     public KthLargest(int k, int[] nums) {
+//         this.k=k;
+//         this.minHeap=new PriorityQueue<>(k);
+//         for(int num : nums){
+//             add(num);
+//         }
+//     }
     
-    public int add(int val) {
-        if(minHeap.size()<k){
-            minHeap.add(val);
-        }else if(val>minHeap.peek()){
-            minHeap.poll();
-            minHeap.offer(val);
-        }
-        return minHeap.peek();
-    }
-}
+//     public int add(int val) {
+//         if(minHeap.size()<k){
+//             minHeap.add(val);
+//         }else if(val>minHeap.peek()){
+//             minHeap.poll();
+//             minHeap.offer(val);
+//         }
+//         return minHeap.peek();
+//     }
+// }
 
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest obj = new KthLargest(k, nums);
  * int param_1 = obj.add(val);
  */
+
+ class KthLargest{
+    PriorityQueue<Integer> pq;
+    int k;
+    public KthLargest(int k, int[] nums){
+        this.k=k;
+        this.pq=new PriorityQueue<>();
+
+        for(int num : nums){
+            add(num);
+        }
+    }
+    public int add(int val){
+        pq.add(val);
+        if(pq.size()>k){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+ }
