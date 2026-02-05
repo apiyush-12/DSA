a5bb21e7d9c25ea33139becb4a32f16921db3dbc
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-05 20:47:09 +0530

    Time: 19 ms (95.43%) | Memory: 62.2 MB (5.29%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,16 +1,33 @@
-class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b)->Integer.compare(b[0]*b[0]+b[1]*b[1], a[0]*a[0]+a[1]*a[1]));
+// class Solution {
+//     public int[][] kClosest(int[][] points, int k) {
+//         PriorityQueue<int[]> pq=new PriorityQueue<>((a, b)->Integer.compare(b[0]*b[0]+b[1]*b[1], a[0]*a[0]+a[1]*a[1]));
+//         for(int[] point: points){
+//             pq.add(point);
+//             if(pq.size()>k){
+//                 pq.poll();
+//             }
+//         }
+//         int[][] result=new int[k][2];
+//         for(int i=0; i<k; i++){
+//             result[i]=pq.poll();
+//         }
+//         return result;
+//     }
+// }
+
+class Solution{
+    public int[][] kClosest(int[][] points, int k){
+        PriorityQueue<int[]> pq=new PriorityQueue<>(k,new Comparator<int[]>(){
+        public int compare(int[]a, int[]b){
+            return ((b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
+        }
+        });
         for(int[] point: points){
             pq.add(point);
             if(pq.size()>k){
                 pq.poll();
             }
         }
-        int[][] result=new int[k][2];
-        for(int i=0; i<k; i++){
-            result[i]=pq.poll();
-        }
-        return result;
+        return pq.toArray(new int[0][0]);
     }
 }
