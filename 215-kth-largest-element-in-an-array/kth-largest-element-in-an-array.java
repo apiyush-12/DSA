df500c58b3b3cf3cb51f3fba14ce594167d812bf
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-06 23:32:31 +0530

    Time: 3 ms (99.43%) | Memory: 71.1 MB (6.97%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -37,7 +37,9 @@ class Solution{
             count[num+10000]++;
         }
         for(int i=count.length-1; i>=0; i--){
+            if(count[i]>0){
             k -= count[i];
+            }
             if(k<=0){
                 return i-10000;
             }
