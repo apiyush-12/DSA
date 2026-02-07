d3c99d51559707ddaba608557c5dacb2078ee491
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-07 23:43:49 +0530

    Time: 1 ms (99.83%) | Memory: 48.2 MB (63.84%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,27 +1,49 @@
-class Solution {
-    public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> freqMap=new HashMap<>();
-        for(char task : tasks){
-            freqMap.put(task, freqMap.getOrDefault(task, 0) +1);
-        }
+// class Solution {
+//     public int leastInterval(char[] tasks, int n) {
+//         Map<Character, Integer> freqMap=new HashMap<>();
+//         for(char task : tasks){
+//             freqMap.put(task, freqMap.getOrDefault(task, 0) +1);
+//         }
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
-        pq.addAll(freqMap.values());
-        int time=0;
-        while(!pq.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0; i<n+1; i++){
-                if(!pq.isEmpty()){
-                    temp.add(pq.poll());
-                }
-            }
-            for(int freq : temp){
-                if(--freq>0){
-                    pq.add(freq);
-                }
-            }
-            time +=pq.isEmpty() ? temp.size() : n+1;
+//         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+//         pq.addAll(freqMap.values());
+//         int time=0;
+//         while(!pq.isEmpty()){
+//             List<Integer> temp = new ArrayList<>();
+//             for(int i=0; i<n+1; i++){
+//                 if(!pq.isEmpty()){
+//                     temp.add(pq.poll());
+//                 }
+//             }
+//             for(int freq : temp){
+//                 if(--freq>0){
+//                     pq.add(freq);
+//                 }
+//             }
+//             time +=pq.isEmpty() ? temp.size() : n+1;
+//         }
+//         return time;
+//     }
+// }
+
+class Solution{
+    public int leastInterval(char[] tasks, int n){
+        int[] freq=new int[26];
+        for(char task : tasks){
+            freq[task-'A']++;
         }
-        return time;
+        int maxFreq=0;
+        for(int f : freq){
+            maxFreq=Math.max(maxFreq, f);
+        }
+        int maxCount=0;
+        for(int f: freq){
+            if(f==maxFreq){
+                maxCount++;
+            }
+        }
+        int result=0;
+        result=(maxFreq-1)*(n+1)+maxCount;
+        return Math.max(result, tasks.length);
     }
 }
