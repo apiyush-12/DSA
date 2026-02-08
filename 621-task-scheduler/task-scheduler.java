ab75378cae3f4949d5e7c31764cbd41ee4424eb8
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-08 21:01:58 +0530

    Time: 2 ms (98.69%) | Memory: 48.6 MB (10.44%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -28,29 +28,6 @@
 
 
 // piyush
-class Solution{
-    public int leastInterval(char[] tasks, int n){
-        int[] freq=new int[26];
-        for(char task : tasks){
-            freq[task-'A']++;
-        }
-        int maxFreq=0;
-        for(int f : freq){
-            maxFreq=Math.max(maxFreq, f);
-        }
-        int maxCount=0;
-        for(int f: freq){
-            if(f==maxFreq){
-                maxCount++;
-            }
-        }
-        int result=0;
-        result=(maxFreq-1)*(n+1)+maxCount;
-        return Math.max(result, tasks.length);
-    }
-}
-
-// piyush
 // class Solution{
 //     public int leastInterval(char[] tasks, int n){
 //         int[] freq=new int[26];
@@ -58,14 +35,11 @@ class Solution{
 //             freq[task-'A']++;
 //         }
 //         int maxFreq=0;
-//         // for(int i=0; i<freq.length; i++){
-//         //     maxFreq=Math.max(maxFreq, freq[i]);
-//         // }
 //         for(int f : freq){
 //             maxFreq=Math.max(maxFreq, f);
 //         }
 //         int maxCount=0;
-//         for(int f : freq){
+//         for(int f: freq){
 //             if(f==maxFreq){
 //                 maxCount++;
 //             }
@@ -75,3 +49,29 @@ class Solution{
 //         return Math.max(result, tasks.length);
 //     }
 // }
+
+// piyush
+class Solution{
+    public int leastInterval(char[] tasks, int n){
+        int[] freq=new int[26];
+        for(char task : tasks){
+            freq[task-'A']++;
+        }
+        int maxFreq=0;
+        // for(int i=0; i<freq.length; i++){
+        //     maxFreq=Math.max(maxFreq, freq[i]);
+        // }
+        for(int f : freq){
+            maxFreq=Math.max(maxFreq, f);
+        }
+        int maxCount=0;
+        for(int f : freq){
+            if(f==maxFreq){
+                maxCount++;
+            }
+        }
+        int result=0;
+        result=(maxFreq-1)*(n+1)+maxCount;
+        return Math.max(result, tasks.length);
+    }
+}
