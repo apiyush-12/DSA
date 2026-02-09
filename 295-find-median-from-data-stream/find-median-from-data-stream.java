f118fda424393f21e666b2b0a49651e378f65aa1
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-09 23:15:57 +0530

    Time: 117 ms (91.06%) | Memory: 111.3 MB (85.13%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -26,33 +26,60 @@
  */
 
 // piyush
-class MedianFinder {
-    private PriorityQueue<Integer> maxH; 
-    private PriorityQueue<Integer> minH; 
-    public MedianFinder() {
-        maxH = new PriorityQueue<>((a, b) -> b - a);
-        minH = new PriorityQueue<>();
+// class MedianFinder {
+//     private PriorityQueue<Integer> maxH; 
+//     private PriorityQueue<Integer> minH; 
+//     public MedianFinder() {
+//         maxH = new PriorityQueue<>((a, b) -> b - a);
+//         minH = new PriorityQueue<>();
+//     }
+//     public void addNum(int num) {
+//         if (maxH.isEmpty() || num <= maxH.peek()) {
+//             maxH.offer(num);
+//         } else {
+//             minH.offer(num);
+//         }
+//         if (maxH.size() > minH.size() + 1) {    //check balance
+//             minH.offer(maxH.poll());
+//         } 
+//         else if (minH.size() > maxH.size()) {
+//             maxH.offer(minH.poll());
+//         }
+//     }
+//     public double findMedian() {
+//         // return maxH.size() > minH.size() ? maxH.peek() : (maxH.peek() + minH.peek()) / 2.0;
+//         if (maxH.size() > minH.size()) {
+//             return maxH.peek();
+//         }
+
+//         return (maxH.peek() + minH.peek()) / 2.0;
+//     }
+// }
+
+class MedianFinder{
+    private PriorityQueue<Integer> maxH=new PriorityQueue<>((a, b) -> b-a);
+    private PriorityQueue<Integer> minH=new PriorityQueue<>();
+    public MedianFinder(){
     }
-    public void addNum(int num) {
-        if (maxH.isEmpty() || num <= maxH.peek()) {
-            maxH.offer(num);
-        } else {
-            minH.offer(num);
+    public void addNum(int num){
+        if(maxH.isEmpty() || num <= maxH.peek()){
+            maxH.add(num);
+        }else{
+            minH.add(num);
         }
-        if (maxH.size() > minH.size() + 1) {    //check balance
-            minH.offer(maxH.poll());
-        } 
-        else if (minH.size() > maxH.size()) {
-            maxH.offer(minH.poll());
+
+        if(maxH.size()>minH.size() +1){
+            minH.add(maxH.poll());
+        }else if(minH.size()>maxH.size()){
+            maxH.add(minH.poll());
         }
     }
-    public double findMedian() {
-        // return maxH.size() > minH.size() ? maxH.peek() : (maxH.peek() + minH.peek()) / 2.0;
-        if (maxH.size() > minH.size()) {
+    public double findMedian(){
+        if(minH.size() < maxH.size()){
             return maxH.peek();
+        }else{
+            return (maxH.peek()+minH.peek())/2.0;
         }
-
-        return (maxH.peek() + minH.peek()) / 2.0;
     }
-}
 
+}
