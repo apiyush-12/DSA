abef87cab1324acaa67501dec519aa10e7886274
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-12 22:08:05 +0530

    Time: 2 ms (99.34%) | Memory: 45.3 MB (23.06%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,38 @@
-class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         List<List<Integer>> result=new ArrayList<>();
+//         Arrays.sort(nums);
+//         backTrack(result, new ArrayList<>(), nums, 0);
+//         return result;
+//     }
+//     public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] nums, int start){
+//         result.add(new ArrayList<>(curr));
+//         for(int i=start; i<nums.length; i++){
+//             if(i>start && nums[i]==nums[i-1]){
+//                 continue;
+//             }
+//             curr.add(nums[i]);
+//             backTrack(result, curr, nums, i+1);
+//             curr.remove(curr.size()-1);
+//         }
+//     }
+// }
+
+class Solution{
+    public List<List<Integer>> subsetsWithDup(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        backTrack(result, new ArrayList<>(), nums, 0);
+        backTrack(result, nums, new ArrayList<>(), 0);
         return result;
     }
-    public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] nums, int start){
+    public void backTrack(List<List<Integer>> result, int[] nums, List<Integer> curr, int start){
         result.add(new ArrayList<>(curr));
         for(int i=start; i<nums.length; i++){
             if(i>start && nums[i]==nums[i-1]){
                 continue;
             }
             curr.add(nums[i]);
-            backTrack(result, curr, nums, i+1);
+            backTrack(result, nums, curr, i+1);
             curr.remove(curr.size()-1);
         }
     }
