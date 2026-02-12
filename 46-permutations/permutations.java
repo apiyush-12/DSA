64dfc291e620d517e538143e094a5de39632b70a
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-12 21:16:43 +0530

    Time: 1 ms (86.27%) | Memory: 45.6 MB (62.83%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,47 @@
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> result=new ArrayList<>();
+//         boolean[] used=new boolean[nums.length];
+//         backTrack(result, new ArrayList<>(), nums, used);
+//         return result;
+//     }
+//     public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] nums, boolean[] used){
+//         if(curr.size()==nums.length){
+//             result.add(new ArrayList<>(curr));
+//             return;
+//         }
+//         for(int i=0; i<nums.length; i++){
+//             if(!used[i]){
+//                 curr.add(nums[i]);
+//                 used[i]=true;
+//                 backTrack(result, curr, nums, used);
+//                 used[i]=false;
+//                 curr.remove(curr.size()-1);
+//             }
+//         }
+//     }
+// }
+
+
+class Solution{
+    public List<List<Integer>> permute(int[] nums){
         List<List<Integer>> result=new ArrayList<>();
         boolean[] used=new boolean[nums.length];
-        backTrack(result, new ArrayList<>(), nums, used);
+        backTrack(result, used, nums, new ArrayList<>());
         return result;
     }
-    public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] nums, boolean[] used){
+    public void backTrack(List<List<Integer>> result, boolean[] used, int[] nums, List<Integer> curr){
         if(curr.size()==nums.length){
             result.add(new ArrayList<>(curr));
             return;
         }
         for(int i=0; i<nums.length; i++){
             if(!used[i]){
-                curr.add(nums[i]);
-                used[i]=true;
-                backTrack(result, curr, nums, used);
-                used[i]=false;
-                curr.remove(curr.size()-1);
+            curr.add(nums[i]);
+            used[i]=true;
+            backTrack(result, used, nums, curr);
+            used[i]=false;
+            curr.remove(curr.size()-1);
             }
         }
     }
