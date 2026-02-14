e898885c8d8f482d2a94422b2a6933d878936945
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-14 22:40:17 +0530

    Time: 6 ms (73.09%) | Memory: 45.5 MB (27.69%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -26,20 +26,46 @@
 
 
 // piyush
+// class Solution{
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target){
+//         List<List<Integer>> result=new ArrayList<>();
+//         Arrays.sort(candidates);
+//         int n=candidates.length;
+//         backTrack(result, new ArrayList<>(), candidates, target, 0, n);
+//         return result;
+//     }
+//     public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] candidates, int target, int start, int n){
+//         if(target==0){
+//             result.add(new ArrayList<>(curr));
+//             return;
+//         }
+//         for(int i=start; i<n; i++){
+//             if(i>start && candidates[i]==candidates[i-1]){
+//                 continue;
+//             }
+//             if(target-candidates[i]<0){
+//                 break;
+//             }
+//             curr.add(candidates[i]);
+//             backTrack(result, curr, candidates, target-candidates[i], i+1, n);
+//             curr.remove(curr.size()-1);
+//         }
+//     }
+// }
+
 class Solution{
     public List<List<Integer>> combinationSum2(int[] candidates, int target){
         List<List<Integer>> result=new ArrayList<>();
         Arrays.sort(candidates);
-        int n=candidates.length;
-        backTrack(result, new ArrayList<>(), candidates, target, 0, n);
+        backTrack(result, new ArrayList<>(), candidates, target, 0);
         return result;
     }
-    public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] candidates, int target, int start, int n){
+    public void backTrack(List<List<Integer>> result, List<Integer> curr, int[] candidates, int target, int start){
         if(target==0){
             result.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=start; i<n; i++){
+        for(int i=start; i<candidates.length; i++){
             if(i>start && candidates[i]==candidates[i-1]){
                 continue;
             }
@@ -47,7 +73,7 @@ class Solution{
                 break;
             }
             curr.add(candidates[i]);
-            backTrack(result, curr, candidates, target-candidates[i], i+1, n);
+            backTrack(result, curr, candidates, target-candidates[i], i+1);
             curr.remove(curr.size()-1);
         }
     }
