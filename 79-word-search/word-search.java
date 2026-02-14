e7f8f5e4c4e63ce0853acc9839797f4912c925f5
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-14 23:27:05 +0530

    Time: 121 ms (92.09%) | Memory: 43.1 MB (44.36%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,40 +1,72 @@
+// class Solution {
+//     public char[][] board;
+//     public int rows;
+//     public int cols;
+//     public boolean exist(char[][] board, String word) {
+//         this.board=board;
+//         this.rows=board.length;
+//         this.cols=board[0].length;
+
+//         for(int r=0; r<rows; r++){
+//             for(int c=0; c<cols; c++){
+//                 if(backTrack(r, c, word, 0)){      //true
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+//     public boolean backTrack(int row, int col, String word, int index){
+//         if(index>=word.length()){
+//             return true;
+//         }
+//         if(row<0 || row>=rows || col<0 || col>=cols || this.board[row][col]!=word.charAt(index)){
+//             return false;
+//         }
+//         int[] rowdir={0,1,0,-1};
+//         int[] coldir={1,0,-1,0};
+//         this.board[row][col]='#';
+
+//         boolean ret=false;
+//         for(int d=0; d<4; d++){
+//             ret=backTrack(row+rowdir[d], col+coldir[d], word, index+1);
+//             if(ret){   //true
+//                 break;
+//             }
+//         }
+//         board[row][col]=word.charAt(index);
+//         return ret;
+//     }
+// }
+
 class Solution {
-    public char[][] board;
-    public int rows;
-    public int cols;
-    public boolean exist(char[][] board, String word) {
-        this.board=board;
-        this.rows=board.length;
-        this.cols=board[0].length;
-
-        for(int r=0; r<rows; r++){
-            for(int c=0; c<cols; c++){
-                if(backTrack(r, c, word, 0)){      //true
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    public boolean backTrack(int row, int col, String word, int index){
-        if(index>=word.length()){
-            return true;
-        }
-        if(row<0 || row>=rows || col<0 || col>=cols || this.board[row][col]!=word.charAt(index)){
+    private boolean find(char[][]board, String words, int indx, int r, int c) {
+        if(r<0 || r>=board.length || c<0 || c>=board[0].length || words.charAt(indx) != board[r][c])
             return false;
-        }
-        int[] rowdir={0,1,0,-1};
-        int[] coldir={1,0,-1,0};
-        this.board[row][col]='#';
+        if(indx == words.length()-1) return true;
 
-        boolean ret=false;
-        for(int d=0; d<4; d++){
-            ret=backTrack(row+rowdir[d], col+coldir[d], word, index+1);
-            if(ret){   //true
-                break;
+        char cur = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = find(board, words, indx+1, r+1, c) ||
+        find(board, words, indx+1, r, c+1) ||
+        find(board, words, indx+1, r-1, c) ||
+        find(board, words, indx+1, r, c-1);
+
+        board[r][c] = cur;
+        return found;        
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n= board[0].length;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(word.charAt(0) == board[i][j] && find(board, word, 0, i, j))
+                    return true;
             }
         }
-        board[row][col]=word.charAt(index);
-        return ret;
+
+        return false;
     }
 }
