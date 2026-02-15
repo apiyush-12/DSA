aa595db284f6a5acb22a4480d92300622c5d4b41
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-15 22:57:11 +0530

    Time: 119 ms (92.44%) | Memory: 43 MB (60.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -41,28 +41,63 @@
 
 
 // piyush
-class Solution {
-    private boolean find(char[][]board, String words, int indx, int r, int c) {
-        if(r<0 || r>=board.length || c<0 || c>=board[0].length || words.charAt(indx) != board[r][c])
-            return false;
-        if(indx == words.length()-1) return true;
+// class Solution {
+//     private boolean find(char[][]board, String words, int indx, int r, int c) {
+//         if(r<0 || r>=board.length || c<0 || c>=board[0].length || words.charAt(indx) != board[r][c])
+//             return false;
+//         if(indx == words.length()-1) return true;
 
-        char cur = board[r][c];
-        board[r][c] = '#';
-        boolean found = find(board, words, indx+1, r+1, c) || find(board, words, indx+1, r, c+1) || find(board, words, indx+1, r-1, c) || find(board, words, indx+1, r, c-1);
-        board[r][c] = cur;
-        return found;        
-    }
-    public boolean exist(char[][] board, String word) {
-        int m = board.length, n= board[0].length;
+//         char cur = board[r][c];
+//         board[r][c] = '#';
+//         boolean found = find(board, words, indx+1, r+1, c) || 
+//                         find(board, words, indx+1, r, c+1) || 
+//                         find(board, words, indx+1, r-1, c) || 
+//                         find(board, words, indx+1, r, c-1);
+//         board[r][c] = cur;
+//         return found;        
+//     }
+//     public boolean exist(char[][] board, String word) {
+//         int m = board.length, n= board[0].length;
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(word.charAt(0) == board[i][j] && find(board, word, 0, i, j))
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(word.charAt(0) == board[i][j] && find(board, word, 0, i, j))
+//                     return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+
+class Solution{
+    public boolean exist(char[][] board, String word){
+        int m=board.length, n=board[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(word.charAt(0)==board[i][j] && backTrack(board, word, i, j, 0)){
                     return true;
+                }
             }
         }
-
         return false;
+    }
+
+    private boolean backTrack(char[][]board, String word, int r, int c, int index){
+        if(r<0 || r>=board.length || c<0 || c>=board[0].length || word.charAt(index)!=board[r][c]){
+            return false;
+        }
+        if(index==word.length()-1){
+            return true;
+        }
+        char curr=board[r][c];
+        board[r][c]='#';
+        boolean found= backTrack(board, word, r+1, c, index+1) ||
+                       backTrack(board, word, r, c+1, index+1) ||
+                       backTrack(board, word, r-1, c, index+1) ||
+                       backTrack(board, word, r, c-1, index+1);
+        board[r][c]=curr;
+        return found;
     }
 }
