51f4f020ba776280c68e5095f812dc586f57be42
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-17 00:00:44 +0530

    Time: 10 ms (17.39%) | Memory: 65.4 MB (40.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,12 +1,42 @@
-class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
+// class Solution {
+//     public List<List<String>> partition(String s) {
+//         List<List<String>> result = new ArrayList<>();
+//         backTrack(result, new ArrayList<>(), s, 0);
+//         return result;
+//     }
+//     private void backTrack(List<List<String>> result, List<String> curr, String s, int start){
+//         if(start==s.length()){
+//             result.add(new ArrayList(curr));
+//             return;
+//         }
+//         for(int end=start; end<s.length(); end++){
+//             if(isPalindrome(s, start, end)){
+//                 curr.add(s.substring(start, end+1));
+//                 backTrack(result, curr, s, end+1);
+//                 curr.remove(curr.size()-1);
+//             }
+//         }
+//     }
+//     private boolean isPalindrome(String s, int start, int end){
+//         while(start<end){
+//             if(s.charAt(start++)!=s.charAt(end--)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+class Solution{
+    public List<List<String>> partition(String s){
+        List<List<String>> result= new ArrayList<>();
         backTrack(result, new ArrayList<>(), s, 0);
         return result;
     }
     private void backTrack(List<List<String>> result, List<String> curr, String s, int start){
         if(start==s.length()){
-            result.add(new ArrayList(curr));
+            result.add(new ArrayList<>(curr));
             return;
         }
         for(int end=start; end<s.length(); end++){
@@ -16,6 +46,7 @@ class Solution {
                 curr.remove(curr.size()-1);
             }
         }
+
     }
     private boolean isPalindrome(String s, int start, int end){
         while(start<end){
