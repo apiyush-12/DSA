91198e3d411638c23c24800a9f8784820cb62713
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-19 00:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (40.60%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,21 +1,48 @@
-class Solution {
-    private String[] digitToLetters={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    public List<String> letterCombinations(String digits) {
-        List<String> combinations=new ArrayList<>();
-        if(digits==null || digits.length()==0){
-            return combinations;
-        }
-        backTrack(combinations, digits, "", 0);
-        return combinations;
+// class Solution {
+//     private String[] digitToLetters={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+//     public List<String> letterCombinations(String digits) {
+//         List<String> combinations=new ArrayList<>();
+//         if(digits==null || digits.length()==0){
+//             return combinations;
+//         }
+//         backTrack(combinations, digits, "", 0);
+//         return combinations;
+//     }
+//     private void backTrack(List<String> combinations, String digits, String currCombinations, int index){
+//         if(index==digits.length()){
+//             combinations.add(currCombinations);
+//             return;
+//         }
+//         String letters=digitToLetters[digits.charAt(index)-'0'];
+//         for(char letter : letters.toCharArray()){
+//             backTrack(combinations, digits, currCombinations+letter, index+1);
+//         }
+//     }
+// }
+
+
+
+class Solution{
+      String[] digitMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits){
+        List<String> res=new ArrayList<>();
+        StringBuilder path=new StringBuilder();
+        backTrack(digits, res, path, 0);
+        return res;
     }
-    private void backTrack(List<String> combinations, String digits, String currCombinations, int index){
-        if(index==digits.length()){
-            combinations.add(currCombinations);
+    private void backTrack(String digits, List<String> res, StringBuilder path, int i){
+        if(i==digits.length()){
+            String p=path.toString();
+            res.add(p);
             return;
         }
-        String letters=digitToLetters[digits.charAt(index)-'0'];
-        for(char letter : letters.toCharArray()){
-            backTrack(combinations, digits, currCombinations+letter, index+1);
+        int digit=digits.charAt(i)-'0';
+        String chars=digitMap[digit];
+        for(char ch: chars.toCharArray()){
+            path.append(ch);
+            backTrack(digits, res, path, i+1);
+            path.deleteCharAt(path.length()-1);
         }
     }
 }
