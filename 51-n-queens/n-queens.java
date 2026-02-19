9f02d93e1323d8bf9d804405a21f731d22e36dad
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-19 21:32:28 +0530

    Time: 2 ms (80.59%) | Memory: 46.8 MB (45.46%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -24,18 +24,16 @@ class Solution {
         }
     } 
     private boolean isValid(char[][] board, int row, int col){
-        for(int i=0; i<col; i++){
+        for(int i=0; i<col; i++){                    
             if(board[row][i]=='Q'){
                 return false;
             }
         }
-
         for(int i=row, j=col; i>=0 && j>=0; i--, j--){
             if(board[i][j]=='Q'){
                 return false;
             }
         }
-
         for(int i=row, j=col; i<board.length && j>=0; i++, j--){
             if(board[i][j]=='Q'){
                 return false;
