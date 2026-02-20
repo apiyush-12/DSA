c8d0012964f4826d350ac3d93c2292457e8be825
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-20 22:34:54 +0530

    Time: 2 ms (99.82%) | Memory: 52.5 MB (45.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,26 +1,52 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-        int rows=grid.length;
-        int cols=grid[0].length;
-        int island=0;
-        for(int r=0; r<rows; r++){
-            for(int c=0; c<cols; c++){
+// class Solution {
+//     public int numIslands(char[][] grid) {
+//         int rows=grid.length;
+//         int cols=grid[0].length;
+//         int island=0;
+//         for(int r=0; r<rows; r++){
+//             for(int c=0; c<cols; c++){
+//                 if(grid[r][c]=='1'){
+//                     island++;
+//                     dfs(r, c, grid, rows, cols);
+//                 }
+//             }
+//         }
+//         return island;
+//     }
+//     private void dfs(int row, int col, char[][] grid, int rows, int cols){
+//         int[][] directions=new int[][]{{0,1}, {1, 0}, {0, -1}, {-1, 0}};
+//         if(row<0 || col <0 || row>=rows || col>=cols || grid[row][col]=='0'){
+//             return;
+//         }
+//         grid[row][col]='0';
+//         for(int[] dir : directions){
+//             dfs(row+dir[0], col+dir[1], grid, rows, cols);
+//         }
+//     }
+// }
+
+class Solution{
+    int m, n;
+    public int numIslands(char[][] grid){
+        m=grid.length;
+        n=grid[0].length;
+        int islands=0;
+        for(int r=0; r<m; r++){
+            for(int c=0; c<n; c++){
                 if(grid[r][c]=='1'){
-                    island++;
-                    dfs(r, c, grid, rows, cols);
+                    islands++;
+                    dfs(grid, r, c);
                 }
             }
         }
-        return island;
+        return islands;
     }
-    private void dfs(int row, int col, char[][] grid, int rows, int cols){
-        int[][] directions=new int[][]{{0,1}, {1, 0}, {0, -1}, {-1, 0}};
-        if(row<0 || col <0 || row>=rows || col>=cols || grid[row][col]=='0'){
-            return;
-        }
+    private void dfs(char[][] grid, int row, int col){
+        if(row<0 || row>=m || col<0 || col>=n || grid[row][col]=='0') return;
         grid[row][col]='0';
-        for(int[] dir : directions){
-            dfs(row+dir[0], col+dir[1], grid, rows, cols);
-        }
+        dfs(grid, row+1, col);
+        dfs(grid, row, col+1);
+        dfs(grid, row-1, col);
+        dfs(grid, row, col-1);
     }
 }
