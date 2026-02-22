02697a778b7f8d158d99bd093b91e7b57304d7fa
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-02-22 22:59:00 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (69.04%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,28 +1,55 @@
-class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+// class Solution {
+//     public int maxAreaOfIsland(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         int maxArea=0;
+//         for(int r=0; r<n; r++){
+//             for(int c=0; c<m; c++){
+//                 if(grid[r][c]==1){
+//                     int area=dfs(grid, r, c, n, m);
+//                     maxArea=Math.max(area, maxArea);
+//                 }
+//             }
+//         }
+//         return maxArea;
+//     }
+//     private int dfs(int[][] grid, int i, int j, int n, int m){
+//         if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0){
+//             return 0;
+//         }
+//         grid[i][j]=0;
+//         int area=1;
+//         area+=dfs(grid, i+1, j, n, m);
+//         area+=dfs(grid, i, j+1, n, m);
+//         area+=dfs(grid, i-1, j, n, m);
+//         area+=dfs(grid, i, j-1, n, m);
+//         return area;
+//     }
+// }
+
+class Solution{
+    public int maxAreaOfIsland(int[][] grid){
         int maxArea=0;
-        for(int r=0; r<n; r++){
-            for(int c=0; c<m; c++){
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
                 if(grid[r][c]==1){
-                    int area=dfs(grid, r, c, n, m);
+                    int area=dfs(grid, r, c);
                     maxArea=Math.max(area, maxArea);
                 }
             }
         }
         return maxArea;
     }
-    private int dfs(int[][] grid, int i, int j, int n, int m){
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0){
+    private int dfs(int[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0){
             return 0;
         }
         grid[i][j]=0;
         int area=1;
-        area+=dfs(grid, i+1, j, n, m);
-        area+=dfs(grid, i, j+1, n, m);
-        area+=dfs(grid, i-1, j, n, m);
-        area+=dfs(grid, i, j-1, n, m);
+        area+=dfs(grid, i+1, j);
+        area+=dfs(grid, i-1, j);
+        area+=dfs(grid, i, j+1);
+        area+=dfs(grid, i, j-1);
         return area;
     }
 }
