369b6e67ee170e7d4af7a8f6307dbc2b43f80802
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-10 00:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (30.83%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,28 +1,63 @@
+// class Solution {
+//     public int[] findRedundantConnection(int[][] edges) {
+//         int[] parent=new int[edges.length+1];
+//         for(int i=1; i<edges.length; i++){
+//             parent[i]=i;
+//         }
+//         for(int[] edge : edges){
+//             int node1=edge[0];
+//             int node2=edge[1];
+
+//             int root1=find(parent, node1);
+//             int root2=find(parent, node2);
+
+//             if(root1==root2){
+//                 return edge;
+//             }
+//             parent[root2]=root1;
+//         }
+//         return new int[0];
+//     }
+//     private int find(int[] parent, int node){
+//         while(node!=parent[node]){
+//             parent[node]=parent[parent[node]];
+//             node=parent[node];
+//         }
+//         return node;
+//     }
+// }
+
 class Solution {
+    int[] parent;
+    public int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]); // path compression
+        }
+        return parent[x];
+    }
+
+    public boolean union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA == rootB){
+            return false; // cycle detected
+        }
+        parent[rootA] = rootB;
+        return true;
+    }
+
     public int[] findRedundantConnection(int[][] edges) {
-        int[] parent=new int[edges.length+1];
-        for(int i=1; i<edges.length; i++){
-            parent[i]=i;
+
+        int n = edges.length;
+        parent = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            parent[i] = i;
         }
         for(int[] edge : edges){
-            int node1=edge[0];
-            int node2=edge[1];
-
-            int root1=find(parent, node1);
-            int root2=find(parent, node2);
-
-            if(root1==root2){
+            if(!union(edge[0], edge[1])){
                 return edge;
             }
-            parent[root2]=root1;
         }
         return new int[0];
-    }
-    private int find(int[] parent, int node){
-        while(node!=parent[node]){
-            parent[node]=parent[parent[node]];
-            node=parent[node];
-        }
-        return node;
     }
 }
