e1ede3c87a701ed4ada5d50efec474309a456cdc
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-11 23:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (25.85%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -29,34 +29,34 @@
 
 
 // piyush
-class Solution{
-    public int[] findRedundantConnection(int[][] edges){
-        int[] parent= new int[edges.length+1];
-        for(int i=0; i<edges.length; i++){
-            parent[i]=i;
-        }
-        for(int[] edge : edges){
-            int node1=edge[0];
-            int node2=edge[1];
+// class Solution{
+//     public int[] findRedundantConnection(int[][] edges){
+//         int[] parent= new int[edges.length+1];
+//         for(int i=0; i<edges.length; i++){
+//             parent[i]=i;
+//         }
+//         for(int[] edge : edges){
+//             int node1=edge[0];
+//             int node2=edge[1];
 
-            int root1=find(parent, node1);
-            int root2=find(parent, node2);
+//             int root1=find(parent, node1);
+//             int root2=find(parent, node2);
 
-            if(root1==root2){
-                return edge;
-            }
-            parent[root2]=root1;   
-        }
-        return new int[0];
-    }
-    private int find(int[] parent, int node){
-        while(node!=parent[node]){
-            parent[node]=parent[parent[node]];
-            node=parent[node];
-        }
-        return node;
-    }
-}
+//             if(root1==root2){
+//                 return edge;
+//             }
+//             parent[root2]=root1;   
+//         }
+//         return new int[0];
+//     }
+//     private int find(int[] parent, int node){
+//         while(node!=parent[node]){
+//             parent[node]=parent[parent[node]];
+//             node=parent[node];
+//         }
+//         return node;
+//     }
+// }
 
 // class Solution {
 //     int[] parent;
@@ -92,34 +92,34 @@ class Solution{
 
 
 // piyush
-// class Solution{
-//     int[] parent;
-//     private int find(int x){
-//         if(parent[x]!=x){
-//             parent[x]=find(parent[x]);
-//         }
-//         return parent[x];
-//     }
-//     private boolean union(int a, int b){
-//         int rootA=find(a);
-//         int rootB=find(b);
-//         if(rootA==rootB){
-//             return false;
-//         }
-//         parent[rootB]=rootA;
-//         return true;
-//     }
-//     public int[] findRedundantConnection(int[][] edges){
-//         int n= edges.length;
-//         parent=new int[n+1];
-//         for(int i=0; i<n+1; i++){
-//             parent[i]=i;
-//         }
-//         for(int[] edge : edges){
-//             if(!union(edge[0], edge[1])){
-//                 return edge;
-//             }
-//         }
-//         return new int[0];
-//     }
-// }
+class Solution{
+    int[] parent;
+    private int find(int x){
+        if(parent[x]!=x){
+            parent[x]=find(parent[x]);
+        }
+        return parent[x];
+    }
+    private boolean union(int a, int b){
+        int rootA=find(a);
+        int rootB=find(b);
+        if(rootA==rootB){
+            return false;
+        }
+        parent[rootB]=rootA;
+        return true;
+    }
+    public int[] findRedundantConnection(int[][] edges){
+        int n= edges.length;
+        parent=new int[n+1];
+        for(int i=0; i<n+1; i++){
+            parent[i]=i;
+        }
+        for(int[] edge : edges){
+            if(!union(edge[0], edge[1])){
+                return edge;
+            }
+        }
+        return new int[0];
+    }
+}
