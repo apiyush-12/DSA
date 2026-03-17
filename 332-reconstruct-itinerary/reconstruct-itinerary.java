c77a8b5bd0bb711f8c2fdb7e8b5509c62af4bc72
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-17 15:10:36 +0530

    Time: 6 ms (79.94%) | Memory: 47.2 MB (43.31%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,26 +1,46 @@
-class Solution {
-    public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, List<String>> graph=new HashMap<>();
-        for(List<String> ticket : tickets){
-            String from=ticket.get(0);
-            String to=ticket.get(1);
-            graph.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
-        }
+// class Solution {
+//     public List<String> findItinerary(List<List<String>> tickets) {
+//         Map<String, List<String>> graph=new HashMap<>();
+//         for(List<String> ticket : tickets){
+//             String from=ticket.get(0);
+//             String to=ticket.get(1);
+//             graph.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+//         }
 
-        for(List<String> destination : graph.values()){
-            Collections.sort(destination);
+//         for(List<String> destination : graph.values()){
+//             Collections.sort(destination);
+//         }
+//         LinkedList<String> itinery=new LinkedList<>();
+//         dfs("JFK", graph, itinery);
+//         return itinery;
+//     }
+
+//     private static void dfs(String airport, Map<String, List<String>> graph, LinkedList<String> itinery){
+//         List<String> destinations= graph.get(airport);
+//         while(destinations!=null && !destinations.isEmpty()){
+//             String next=destinations.remove(0);
+//             dfs(next, graph, itinery);
+//         }
+//         itinery.addFirst(airport);
+//     }
+// }
+
+
+class Solution{
+    Map<String, PriorityQueue<String>> map = new HashMap<>();
+    LinkedList<String> result = new LinkedList<>();
+    public List<String> findItinerary(List<List<String>> tickets){
+        for(List<String> t : tickets){
+            map.computeIfAbsent(t.get(0), k -> new PriorityQueue<>()).add(t.get(1));
         }
-        LinkedList<String> itinery=new LinkedList<>();
-        dfs("JFK", graph, itinery);
-        return itinery;
+        dfs("JFK");
+        return result;
     }
-
-    private static void dfs(String airport, Map<String, List<String>> graph, LinkedList<String> itinery){
-        List<String> destinations= graph.get(airport);
-        while(destinations!=null && !destinations.isEmpty()){
-            String next=destinations.remove(0);
-            dfs(next, graph, itinery);
+    private void dfs(String src){
+        PriorityQueue<String> pq=map.get(src);
+        while(pq!=null && !pq.isEmpty()){
+            dfs(pq.poll());
         }
-        itinery.addFirst(airport);
+        result.addFirst(src);
     }
 }
