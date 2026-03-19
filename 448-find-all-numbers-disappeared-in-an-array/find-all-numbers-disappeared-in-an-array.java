59c440d13bfa27ca1715b1aa5b7b44fb1021efa6
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-19 12:59:02 +0530

    Time: 6 ms (81.15%) | Memory: 66.9 MB (89.94%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,42 @@
-class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n = nums.length;
-        int i=0;
-        while(i<n){
-            int correct=nums[i]-1;
-            if(nums[i] != nums[correct]){
-                int temp = nums[i];
-                nums[i]=nums[correct];
-                nums[correct]=temp;
-            }else{
-                i++;
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int n = nums.length;
+//         int i=0;
+//         while(i<n){
+//             int correct=nums[i]-1;
+//             if(nums[i] != nums[correct]){
+//                 int temp = nums[i];
+//                 nums[i]=nums[correct];
+//                 nums[correct]=temp;
+//             }else{
+//                 i++;
+//             }
+//         }
+//         List<Integer> result = new ArrayList<>();
+//         for(i=0; i<n; i++){
+//             if(nums[i] != i+1){
+//                 result.add(i+1);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
+class Solution{
+    public List<Integer> findDisappearedNumbers(int[] nums){
+        for(int i=0; i<nums.length; i++){
+            int index= Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index] =- nums[index];
             }
         }
-        List<Integer> result = new ArrayList<>();
-        for(i=0; i<n; i++){
-            if(nums[i] != i+1){
+        List<Integer> result=new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0){
                 result.add(i+1);
             }
         }
         return result;
-    }
+    } 
 }
