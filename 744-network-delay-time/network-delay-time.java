9256425ec15aefc7cff041972be794c474eacc2d
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-19 23:27:34 +0530

    Time: 19 ms (22.88%) | Memory: 50.5 MB (17.96%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -4,10 +4,8 @@ class Solution {
         for(int[] edge : times){
             graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
         }
-
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
         pq.offer(new int[]{k, 0});
-
         int[] distances = new int[n+1];
         Arrays.fill(distances, Integer.MAX_VALUE);
         distances[k]=0;
@@ -15,11 +13,9 @@ class Solution {
             int[] current = pq.poll();
             int currentNode = current[0];
             int currentDist = current[1];
-
             if(currentDist > distances[currentNode]){
                 continue;
             }
-
             if(graph.containsKey(currentNode)){
                 for(int[] neighbor : graph.get(currentNode)){
                     int nextNode = neighbor[0];
