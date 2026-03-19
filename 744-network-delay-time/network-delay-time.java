76e2bf4c565b039eb31644f817f3756a9fb04a59
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-19 23:27:14 +0530

    Time: 18 ms (25.29%) | Memory: 50.7 MB (10.07%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for(int[] edge : times){
+            graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{k, 0});
+
+        int[] distances = new int[n+1];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[k]=0;
+        while(!pq.isEmpty()){
+            int[] current = pq.poll();
+            int currentNode = current[0];
+            int currentDist = current[1];
+
+            if(currentDist > distances[currentNode]){
+                continue;
+            }
+
+            if(graph.containsKey(currentNode)){
+                for(int[] neighbor : graph.get(currentNode)){
+                    int nextNode = neighbor[0];
+                    int nextDist = currentDist + neighbor[1];
+                    if(nextDist < distances[nextNode]){
+                        distances[nextNode]=nextDist;
+                        pq.offer(new int[]{nextNode, nextDist});
+                    }
+                }
+            }
+        }
+        int maxDist = Arrays.stream(distances).skip(1).max().getAsInt();
+        return maxDist==Integer.MAX_VALUE ? -1 : maxDist;
+    }
+}
