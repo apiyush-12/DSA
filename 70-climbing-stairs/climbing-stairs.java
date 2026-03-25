ddda6cc28546c794cf260a4ec77731e7ad4fde68
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-26 02:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (95.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,47 @@
-class Solution {
-    public int climbStairs(int n) {
-        if(n==0 || n==1) return 1;
-        int prev1=1, prev2=1, curr=0;
-        for(int i=2; i<=n; i++){
-            curr = prev1 + prev2;
-            prev1=prev2;
-            prev2=curr;
-        }
-        return curr;
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n==0 || n==1) return 1;
+//         int prev1=1, prev2=1, curr=0;
+//         for(int i=2; i<=n; i++){
+//             curr = prev1 + prev2;
+//             prev1=prev2;
+//             prev2=curr;
+//         }
+//         return curr;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+class Solution{
+    public int climbStairs(int n){
+    //     int i;
+    //     int prev1 = i-1;
+    //     int prev2 = i;
+    //    int curr = 0;
+
+    //    for(i = 1 ; i < n ; i++) {
+      
+    //     int curr = prev1 + prev2;
+
+    //     prev1 = prev2;
+    //     prev2
+    //    }
+    if(n==1) return 1;
+    int prev = 1;
+    int prev1 = 1;
+    int curr=0;
+    for(int i=2; i<=n; i++){
+        curr = prev + prev1;
+        prev = prev1;
+        prev1 = curr;
+    }
+    return curr;
     }
 }
