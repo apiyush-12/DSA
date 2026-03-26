3e951426274ded06f8767effe6735c0770d476aa
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-27 02:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (83.63%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,13 +1,25 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        // if(cost.length==0) return cost[0];
-        // if(cost.length==1) return Math.min(cost[0], cost[1]);
-        int n=cost.length;
-        int first=cost[0], sec=cost[1], currCost=0;
-        for(int i=2; i<n; i++){
-            currCost=Math.min(first, sec) + cost[i];
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         // if(cost.length==0) return cost[0];
+//         // if(cost.length==1) return Math.min(cost[0], cost[1]);
+//         int n=cost.length;
+//         int first=cost[0], sec=cost[1], currCost=0;
+//         for(int i=2; i<n; i++){
+//             currCost=Math.min(first, sec) + cost[i];
+//             first=sec;
+//             sec=currCost;
+//         }
+//         return Math.min(first, sec);
+//     }
+// }
+
+class Solution{
+    public int minCostClimbingStairs(int[] cost){
+        int first=cost[0], sec = cost[1];
+        for(int i=2; i<cost.length; i++){
+            int curr=Math.min(first, sec)+cost[i];
             first=sec;
-            sec=currCost;
+            sec=curr;
         }
         return Math.min(first, sec);
     }
