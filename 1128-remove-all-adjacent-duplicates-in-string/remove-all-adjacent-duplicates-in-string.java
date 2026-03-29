21c84c72f148315b214c8ad2edc40079d3bfef08
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-30 02:13:17 +0530

    Time: 11 ms (89.03%) | Memory: 47.4 MB (23.78%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -16,14 +16,30 @@
 
 
 // piyush
-class Solution{
-    public String removeDuplicates(String s){
-        StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
-            int len = sb.length();
-            if(len > 0 && sb.charAt(len-1)==ch) sb.deleteCharAt(len-1);
-            else sb.append(ch);
+// class Solution{
+//     public String removeDuplicates(String s){
+//         StringBuilder sb = new StringBuilder();
+//         for(char ch : s.toCharArray()){
+//             int len = sb.length();
+//             if(len > 0 && sb.charAt(len-1)==ch) sb.deleteCharAt(len-1);
+//             else sb.append(ch);
+//         }
+//         return sb.toString();
+//     }
+// }
+
+class Solution {
+    public String removeDuplicates(String s) {
+     StringBuilder sb=new StringBuilder();
+     for(char c:s.toCharArray()){
+        int len = sb.length();
+
+        if(len>0 && sb.charAt(len-1)==c){
+            sb.deleteCharAt(len-1);
+        }else{
+            sb.append(c);
         }
-        return sb.toString();
+     }
+     return sb.toString();
     }
 }
