8ae0a417bf9f007d4b0e13ba4cdfa7b9bf405b6b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-31 01:50:14 +0530

    Time: 10 ms (96.20%) | Memory: 47 MB (79.05%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -16,12 +16,24 @@
 
 
 // piyush
+// class Solution{
+//     public String removeDuplicates(String s){
+//         StringBuilder sb = new StringBuilder();
+//         for(char ch : s.toCharArray()){
+//             int len = sb.length();
+//             if(len > 0 && sb.charAt(len-1)==ch) sb.deleteCharAt(len-1);
+//             else sb.append(ch);
+//         }
+//         return sb.toString();
+//     }
+// }
+
 class Solution{
     public String removeDuplicates(String s){
         StringBuilder sb = new StringBuilder();
         for(char ch : s.toCharArray()){
             int len = sb.length();
-            if(len > 0 && sb.charAt(len-1)==ch) sb.deleteCharAt(len-1);
+            if(len > 0 && sb.charAt(len-1) == ch) sb.deleteCharAt(len-1);
             else sb.append(ch);
         }
         return sb.toString();
