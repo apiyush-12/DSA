f2affa9341e3f28a641b10a97467071b18011d29
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-31 18:08:53 +0530

    Time: 3 ms (99.48%) | Memory: 48.3 MB (78.45%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,15 +1,30 @@
-class Solution {
-    public int[] nextGreaterElements(int[] nums) {
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n=nums.length;
+//         int[] res = new int[n];
+//         Arrays.fill(res, -1);
+//         Stack<Integer> st = new Stack<>();
+//         for(int i=n*2-1; i>=0; i--){
+//             int index=i%n;
+//             while(!st.isEmpty() && nums[st.peek()]<=nums[index]) st.pop();
+//             if(!st.isEmpty() && i<n) res[index]=nums[st.peek()];
+//             st.push(index);
+//         }
+//         return res;
+//     }
+// }
+
+class Solution{
+    public int[] nextGreaterElements(int[] nums){
         int n=nums.length;
-        int[] res = new int[n];
-        Arrays.fill(res, -1);
-        Stack<Integer> st = new Stack<>();
-        for(int i=n*2-1; i>=0; i--){
-            int index=i%n;
-            while(!st.isEmpty() && nums[st.peek()]<=nums[index]) st.pop();
-            if(!st.isEmpty() && i<n) res[index]=nums[st.peek()];
-            st.push(index);
+        Deque<Integer> st = new ArrayDeque<>();
+        int[] result=new int[n];
+        Arrays.fill(result, -1);
+        for(int i=0; i<2*n; i++){
+            int num=nums[i%n];
+            while(!st.isEmpty() && nums[st.peek()] < num) result[st.pop()]=num;
+            if(i<n) st.push(i);
         }
-        return res;
+        return result;
     }
 }
