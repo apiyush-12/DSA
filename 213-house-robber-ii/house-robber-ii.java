b01d80b2d36424886b3fca001738b19729b08abf
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-03-31 19:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.02%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,40 @@
-class Solution {
-    public int rob(int[] nums) {
-        int rob1=0;
-        int rob2=0;
-        int max1=nums[0];
-        int max2=0;
-        for(int i=0; i<nums.length-1; i++){
-            max1=Math.max(rob1+nums[i], rob2);
+// class Solution {
+//     public int rob(int[] nums) {
+//         int rob1=0;
+//         int rob2=0;
+//         int max1=nums[0];
+//         int max2=0;
+//         for(int i=0; i<nums.length-1; i++){
+//             max1=Math.max(rob1+nums[i], rob2);
+//             rob1=rob2;
+//             rob2=max1;
+//         }
+//         rob1=0;
+//         rob2=0;
+//         for(int i=1; i<nums.length; i++){
+//             max2=Math.max(rob1+nums[i], rob2);
+//             rob1=rob2;
+//             rob2=max2;
+//         }
+//         return Math.max(max1, max2);
+//     }
+// }
+
+
+class Solution{
+    public int rob(int[] nums){
+        if(nums.length==1) return nums[0];
+        int x=Max(nums, 0, nums.length-1);
+        int y=Max(nums, 1, nums.length);
+        return Math.max(x, y);
+    }
+    private int Max(int[] nums, int s, int e){
+        int rob1=0, rob2=0, max=0;
+        for(int i=s; i<e; i++){
+            max=Math.max(rob1+nums[i], rob2);
             rob1=rob2;
-            rob2=max1;
+            rob2=max;
         }
-        rob1=0;
-        rob2=0;
-        for(int i=1; i<nums.length; i++){
-            max2=Math.max(rob1+nums[i], rob2);
-            rob1=rob2;
-            rob2=max2;
-        }
-        return Math.max(max1, max2);
+        return max;
     }
 }
