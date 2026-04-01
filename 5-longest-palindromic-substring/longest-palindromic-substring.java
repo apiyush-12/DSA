2fd70c60818ee67a76f8f446813a155e50c53366
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-01 23:55:08 +0530

    Time: 20 ms (36.49%) | Memory: 43.4 MB (85.33%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,6 @@
-// class Solution {
-//     public String longestPalindrome(String s) {
-//         if(s==null || s.length()<1){
-//             return "";
-//         }
-//         int left=0, right=0;
-//         for(int i=0; i<s.length(); i++){
-//             int len1=checkPalindrome(s, i, i);
-//             int len2=checkPalindrome(s, i, i+1);
-//             int len=Math.max(len1, len2);
-//             if(len>right-left){
-//                 left=i-(len-1)/2;
-//                 right=i+len/2;
-//             }
-//         }
-//         return s.substring(left, right+1);        
-//     }
-//     public int checkPalindrome(String s, int left, int right){
-//         int L=left, R=right;
-//         while(L>=0 && R<s.length() && s.charAt(L)==s.charAt(R)){
-//             L--;
-//             R++;
-//         }
-//         return R-L-1;
-//     }
-// }
-
-//  piyush
-
-class Solution{
-    public String longestPalindrome(String s){
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length()<1 || s==null) return "";
         int left=0, right=0;
         for(int i=0; i<s.length(); i++){
             int len1=checkPalindrome(s, i, i);
@@ -37,13 +9,12 @@ class Solution{
             if(len > right-left){
                 left=i-(len-1)/2;
                 right=i+len/2;
-            }
+            } 
         }
         return s.substring(left, right+1);
     }
-    public int checkPalindrome(String s, int left, int right){
-        int L=left, R=right;
-        while(L>=0 && R<s.length() && s.charAt(L)==s.charAt(R)){
+    public int checkPalindrome(String s, int L, int R){
+        while(L>=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
             L--;
             R++;
         }
