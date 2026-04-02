887aedc4acbbe2679bda186af39baca8f450de42
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-03 01:35:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (31.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -40,12 +40,6 @@
 // }
 
 class Solution{
-    public int rob(int[] nums){
-        if(nums.length==1) return nums[0];
-        int x = Max(nums, 0, nums.length-1);
-        int y = Max(nums, 1, nums.length);
-        return Math.max(x, y);
-    }
     private int Max(int[] nums, int start, int end){
         int rob1=0, rob2=0, max=0;
         for(int i=start; i<end; i++){
@@ -55,4 +49,11 @@ class Solution{
         }
         return max;
     }
+    public int rob(int[] nums){
+        if(nums.length==1) return nums[0];
+        int x = Max(nums, 0, nums.length-1);
+        int y = Max(nums, 1, nums.length);
+        return Math.max(x, y);
+    }
+    
 }
