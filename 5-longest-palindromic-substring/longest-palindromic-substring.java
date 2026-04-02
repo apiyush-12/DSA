17706346ed07c23b0c00c5982167272d7e57a378
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-02 22:40:28 +0530

    Time: 14 ms (92.91%) | Memory: 43.6 MB (75.38%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,20 +1,45 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        if(s.length()<1 || s==null) return "";
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if(s.length()<1 || s==null) return "";
+//         int left=0, right=0;
+//         for(int i=0; i<s.length(); i++){
+//             int len1=checkPalindrome(s, i, i);
+//             int len2=checkPalindrome(s, i, i+1);
+//             int len=Math.max(len1, len2);
+//             if(len > right-left){
+//                 left=i-(len-1)/2;
+//                 right=i+len/2;
+//             } 
+//         }
+//         return s.substring(left, right+1);
+//     }
+//     public int checkPalindrome(String s, int L, int R){
+//         while(L>=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
+//             L--;
+//             R++;
+//         }
+//         return R-L-1;
+//     }
+// }
+
+
+class Solution{
+    public String longestPalindrome(String s){
+        if(s==null || s.length() < 1) return "";
         int left=0, right=0;
         for(int i=0; i<s.length(); i++){
-            int len1=checkPalindrome(s, i, i);
-            int len2=checkPalindrome(s, i, i+1);
+            int len1=Max(s, i, i);
+            int len2=Max(s, i, i+1);
             int len=Math.max(len1, len2);
-            if(len > right-left){
-                left=i-(len-1)/2;
-                right=i+len/2;
-            } 
+            if(len > right - left){
+                left = i - (len-1)/2;
+                right = i + len/2;
+            }
         }
         return s.substring(left, right+1);
     }
-    public int checkPalindrome(String s, int L, int R){
-        while(L>=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
+    public int Max(String s, int L, int R){
+        while(L >=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
             L--;
             R++;
         }
