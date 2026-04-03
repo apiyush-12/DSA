2c28684df8e844c26efd6c5dd4e6afdaf71368a2
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-03 23:00:44 +0530

    Time: 6 ms (63.05%) | Memory: 42.6 MB (96.88%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,16 +1,39 @@
-class Solution {
-    public int countSubstrings(String s) {
-        if(s==null || s.length()<1) return 0;
-        int ans=0;
+// class Solution {
+//     public int countSubstrings(String s) {
+//         if(s==null || s.length()<1) return 0;
+//         int ans=0;
+//         for(int i=0; i<s.length(); i++){
+//             ans += checkP(s, i, i);
+//             ans += checkP(s, i, i+1);
+//         }
+//         return ans;
+//     }
+//     private int checkP(String s, int L, int R){
+//         int count= 0;
+//         while(L>=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
+//             L--;
+//             R++;
+//             count++;
+//         }
+//         return count;
+//     }
+// }
+
+
+class Solution{
+    public int countSubstrings(String s){
+        if(s==null || s.length()==0) return 0;
+        int len=0;
         for(int i=0; i<s.length(); i++){
-            ans += checkP(s, i, i);
-            ans += checkP(s, i, i+1);
+            len +=Max(s, i, i);
+            len +=Max(s, i, i+1);
+            
         }
-        return ans;
+        return len;
     }
-    private int checkP(String s, int L, int R){
-        int count= 0;
-        while(L>=0 && R < s.length() && s.charAt(L)==s.charAt(R)){
+    private int Max(String s, int L, int R){
+        int count=0;
+        while(L>=0 && R<s.length() && s.charAt(L)==s.charAt(R)){
             L--;
             R++;
             count++;
