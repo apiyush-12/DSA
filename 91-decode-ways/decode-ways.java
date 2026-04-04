0fa86332823b478a59c69a672e4223d25a4c3099
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-05 01:33:45 +0530

    Time: 2 ms (12.48%) | Memory: 43.2 MB (44.88%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,12 +1,28 @@
-class Solution {
-    public int numDecodings(String s) {
+// class Solution {
+//     public int numDecodings(String s) {
+//         if(s.charAt(0)=='0') return 0;
+//         int num1=1, num2=1;
+//         for(int i=1; i<s.length(); i++){
+//             int curr=0;
+//             if(s.charAt(i) != '0') curr = num1;
+//             int value=Integer.parseInt(s.substring(i-1, i+1));
+//             if(value>= 10 && value<=26) curr = curr + num2;
+//             num2=num1;
+//             num1=curr;
+//         }
+//         return num1;
+//     }
+// }
+
+class Solution{
+    public int numDecodings(String s){
         if(s.charAt(0)=='0') return 0;
         int num1=1, num2=1;
         for(int i=1; i<s.length(); i++){
             int curr=0;
-            if(s.charAt(i) != '0') curr = num1;
+            if(s.charAt(i) !='0') curr=num1;
             int value=Integer.parseInt(s.substring(i-1, i+1));
-            if(value>= 10 && value<=26) curr = curr + num2;
+            if(value>=10 && value<=26) curr = curr+num2;
             num2=num1;
             num1=curr;
         }
