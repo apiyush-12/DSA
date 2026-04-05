dda466c038bc556b7fe451b782238d23dc1247df
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-06 01:59:54 +0530

    Time: 10 ms (99.79%) | Memory: 46.2 MB (76.89%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,16 +1,30 @@
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] amt = new int[amount+1];
-        Arrays.fill(amt, amount+1);
-        amt[0]=0;
-        for(int i=1; i<=amount; i++){
-            for(int j=0; j<coins.length; j++){
-                if(i >= coins[j]){
-                    amt[i]=Math.min(amt[i], 1+amt[i-coins[j]]);
-                }
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int[] amt = new int[amount+1];
+//         Arrays.fill(amt, amount+1);
+//         amt[0]=0;
+//         for(int i=1; i<=amount; i++){
+//             for(int j=0; j<coins.length; j++){
+//                 if(i >= coins[j]){
+//                     amt[i]=Math.min(amt[i], 1+amt[i-coins[j]]);
+//                 }
+//             }
+//         }
+//         if(amt[amount] < amount+1) return amt[amount];
+//         return -1;
+//     }
+// }
+
+class Solution{
+    public int coinChange(int[] coins, int amount){
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0]=0;
+        for(int coin : coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i]=Math.min(dp[i], 1+dp[i-coin]);
             }
         }
-        if(amt[amount] < amount+1) return amt[amount];
-        return -1;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
