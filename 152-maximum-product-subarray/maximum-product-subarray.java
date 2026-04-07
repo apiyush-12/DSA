99621f9ff3c1e9e64d38aa41b0b34acfc0376213
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-08 02:55:25 +0530

    Time: 2 ms (69.15%) | Memory: 47.2 MB (78.68%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,41 @@
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         if(nums.length==0) return 0;
+//         int min = nums[0];
+//         int max = nums[0];
+//         int result = max;
+//         for(int i=1; i<nums.length; i++){
+//             int curr = nums[i];
+//             int temp= Math.max(curr, Math.max(curr*min, curr*max));
+//             min= Math.min(curr, Math.min(curr*min, curr*max));
+//             max=temp;
+//             result = Math.max(result, max);
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int maxProduct(int[] nums) {
-        if(nums.length==0) return 0;
-        int min = nums[0];
-        int max = nums[0];
-        int result = max;
-        for(int i=1; i<nums.length; i++){
+        int max = nums[0], min = nums[0], result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
             int curr = nums[i];
-            int temp= Math.max(curr, Math.max(curr*min, curr*max));
-            min= Math.min(curr, Math.min(curr*min, curr*max));
-            max=temp;
+
+            // If negative → swap
+            if (curr < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+
+            max = Math.max(curr, curr * max);
+            min = Math.min(curr, curr * min);
+
             result = Math.max(result, max);
         }
+
         return result;
     }
 }
