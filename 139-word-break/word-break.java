d6cf9a7b0491ec8647c825131a0d9f7f6fcd2d3c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-11 03:07:25 +0530

    Time: 4 ms (72.00%) | Memory: 43.7 MB (75.73%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -25,6 +25,7 @@ class Solution{
             for(int j = i-1; j>=0; j--){
                 if(dp[j] && wordBreak.contains(s.substring(j, i))){
                     dp[i]=true;
+                    break;
                 }
             }
         }
