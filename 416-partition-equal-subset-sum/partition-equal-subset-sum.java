e0607c3a32cbec2d834646b9f7bd74af9181d2ee
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-12 22:40:41 +0530

    Time: 15 ms (97.10%) | Memory: 43.6 MB (94.51%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -4,17 +4,13 @@ class Solution {
         for(int num : nums){
             totalSum += num;
         }
-        if(totalSum % 2 != 0){
-            return false;
-        }
+        if(totalSum % 2 != 0) return false;
         int target = totalSum/2;
         boolean[] dp = new boolean[target + 1];
         dp[0]= true;
         for(int num : nums){
             for(int i = target; i>= num; i--){
-                if(dp[i-num]){
-                    dp[i]=true;
-                }
+                if(dp[i-num]) dp[i]=true;
             }
         }
         return dp[target];
