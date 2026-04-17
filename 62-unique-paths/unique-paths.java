7dc0089a68b34b028d346c7e008cd750df8f3f47
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-18 01:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (32.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,11 @@
 class Solution {
-    // public int uniquePaths(int m, int n) {
-    //     int[][] dp=new int[m][n];
-    //     for(int[] row:dp){
-    //         Arrays.fill(row, 1);
-    //     }
-    //     for(int i=1; i<m; i++){
-    //         for(int j=1; j<n; j++){
-    //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-    // }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
 
-    // piyush
-    public int uniquePaths(int m, int n){
-        int[][] dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row, 1);
-        }
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+        for(int[] row : dp) Arrays.fill(row, 1);
+        for(int r = 1; r<m; r++){
+            for(int c=1; c<n; c++){
+                dp[r][c] = dp[r-1][c] + dp[r][c-1];
             }
         }
         return dp[m-1][n-1];
