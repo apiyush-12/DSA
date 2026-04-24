1add7786c1c1ab000db89684bba7a5900e6d4a35
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-04-24 15:19:25 +0530

    Time: 2 ms (99.91%) | Memory: 43 MB (85.89%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -4,10 +4,8 @@ class Solution {
         for(int num : nums){
             sum += num;
         }
-
         if((target + sum)%2 != 0 || sum < Math.abs(target)) return 0;
         int s1 = (target + sum) / 2;
-        
         int[] dp = new int[s1 +1];
         dp[0] = 1;
         for(int num :  nums){
