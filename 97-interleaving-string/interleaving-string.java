64e0bb2fa24c211b0a724538053dc89e8ac616b9
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-02 23:38:00 +0530

    Time: 1 ms (91.93%) | Memory: 43.5 MB (28.03%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -23,23 +23,45 @@
 // }
 
 
+// class Solution{
+//     Boolean[][] dp;
+//     public boolean isInterleave(String s1, String s2, String s3){
+//         int m=s1.length();
+//         int n=s2.length();
+//         int l=s3.length();
+//         if(m + n != l) return false;
+//         dp=new Boolean[m+1][n+1];
+//         return solve(s1, s2, s3, 0, 0, m, n, l);
+//     }
+//     private boolean solve(String s1, String s2, String s3, int i, int j, int m, int n, int l){
+//         if(i==m && j == n) return true;
+//         if(dp[i][j] != null) return dp[i][j];
+//         char c = s3.charAt(i+j);
+//         boolean ans = false;
+//         if(i<m && s1.charAt(i) == c) ans = solve(s1, s2, s3, i+1, j, m, n, l);
+//         if(!ans && j < n && s2.charAt(j) == c) ans = solve(s1, s2, s3, i, j+1, m, n, l);
+//         return dp[i][j] = ans;
+//     }
+// }
+
+
 class Solution{
     Boolean[][] dp;
     public boolean isInterleave(String s1, String s2, String s3){
-        int m=s1.length();
-        int n=s2.length();
-        int l=s3.length();
-        if(m + n != l) return false;
-        dp=new Boolean[m+1][n+1];
-        return solve(s1, s2, s3, 0, 0, m, n, l);
+        int m = s1.length();
+        int n = s2.length();
+        int l = s3.length();
+        if(m+n != l) return false;
+        dp = new Boolean[m+1][n+1];
+        return solve(s1, s2, s3, 0, 0, m, n, l); 
     }
     private boolean solve(String s1, String s2, String s3, int i, int j, int m, int n, int l){
-        if(i==m && j == n) return true;
+        if(i==m && j ==n) return true;
         if(dp[i][j] != null) return dp[i][j];
         char c = s3.charAt(i+j);
         boolean ans = false;
         if(i<m && s1.charAt(i) == c) ans = solve(s1, s2, s3, i+1, j, m, n, l);
-        if(!ans && j < n && s2.charAt(j) == c) ans = solve(s1, s2, s3, i, j+1, m, n, l);
+        if(!ans && j<n && s2.charAt(j) == c) ans = solve(s1, s2, s3, i, j+1, m, n, l);
         return dp[i][j] = ans;
     }
 }
