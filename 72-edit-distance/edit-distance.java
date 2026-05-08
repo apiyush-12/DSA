7409aec7e173be7750d661ed8104eeba70b6b6e6
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-08 11:59:28 +0530

    Time: 5 ms (68.62%) | Memory: 47.2 MB (51.61%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -23,12 +23,8 @@ class Solution{
         int m = word1.length();
         int n = word2.length();
         int[][] dp = new int[m+1][n+1];
-        for(int i=0; i<=m; i++){
-            dp[i][0] = i;
-        }
-        for(int i = 0; i<=n; i++){
-            dp[0][i] = i;
-        }
+        for(int i=0; i<=m; i++) dp[i][0] = i;
+        for(int i = 0; i<=n; i++) dp[0][i] = i;
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
                 if(word1.charAt(i-1) == word2.charAt(j-1)){
