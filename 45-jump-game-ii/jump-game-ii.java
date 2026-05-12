c37f6b960356e516fdfaef59d3a56b5ad566688e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-13 00:03:32 +0530

    Time: 1 ms (99.61%) | Memory: 47.3 MB (55.74%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,12 +1,27 @@
-class Solution {
-    public int jump(int[] nums) {
-        int jump=0, currMax=0, currEnd=0;
+// class Solution {
+//     public int jump(int[] nums) {
+//         int jump=0, currMax=0, currEnd=0;
+//         for(int i=0; i<nums.length-1; i++){
+//             currMax=Math.max(currMax, i+nums[i]);
+//             if(i == currEnd){
+//                 jump++;
+//                 currEnd = currMax;
+//             }    
+//         }
+//         return jump;
+//     }
+// }
+
+
+class Solution{
+    public int jump(int[] nums){
+        int jump =0, currMax=0, currEnd=0;
         for(int i=0; i<nums.length-1; i++){
             currMax=Math.max(currMax, i+nums[i]);
             if(i == currEnd){
                 jump++;
                 currEnd = currMax;
-            }    
+            }
         }
         return jump;
     }
