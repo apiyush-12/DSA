fa3d3e5272306da1a10de427f5f86a93e2bc7248
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-17 23:09:26 +0530

    Time: 28 ms (87.73%) | Memory: 48.4 MB (17.65%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,21 +1,49 @@
+// class Solution {
+//     public boolean isNStraightHand(int[] hand, int groupSize) {
+//         if(hand.length % groupSize != 0) return false;
+//         TreeMap <Integer, Integer> cardCounts = new TreeMap<>();
+//         for(int card : hand){
+//             cardCounts.put(card, cardCounts.getOrDefault(card, 0) + 1);
+//         }
+//         while(!cardCounts.isEmpty()){
+//             int first = cardCounts.firstKey();
+//             for(int i=0; i<groupSize; i++){
+//                 int currCard = first + i;
+//                 if(!cardCounts.containsKey(currCard)) return false;
+//                 int count = cardCounts.get(currCard);
+//                 if(count == 1){
+//                     cardCounts.remove(currCard);
+//                 }else{
+//                     cardCounts.put(currCard, count-1);
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length % groupSize != 0) return false;
-        TreeMap <Integer, Integer> cardCounts = new TreeMap<>();
-        for(int card : hand){
-            cardCounts.put(card, cardCounts.getOrDefault(card, 0) + 1);
+        int n = hand.length;
+        if(n%groupSize!=0){
+            return false;
         }
-        while(!cardCounts.isEmpty()){
-            int first = cardCounts.firstKey();
-            for(int i=0; i<groupSize; i++){
-                int currCard = first + i;
-                if(!cardCounts.containsKey(currCard)) return false;
-                int count = cardCounts.get(currCard);
-                if(count == 1){
-                    cardCounts.remove(currCard);
-                }else{
-                    cardCounts.put(currCard, count-1);
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int num : hand){
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+        Arrays.sort(hand);
+        for(int num : hand){
+            if(freq.get(num)==0){
+                continue;
+            }
+            for(int j=0;j<groupSize;j++){
+                int cur =  num + j;
+                if(freq.getOrDefault(cur,0)==0){
+                    return false;
                 }
+                freq.put(cur,freq.get(cur)-1);
             }
         }
         return true;
