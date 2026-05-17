d305db892f501801826b756aed3524ba16949022
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-17 20:45:47 +0530

    Time: 3 ms (28.34%) | Memory: 119.4 MB (64.06%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,16 +1,37 @@
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0, startIndex = 0, tank = 0;
-        for(int i =0; i<gas.length; i++){
+// class Solution {
+//     public int canCompleteCircuit(int[] gas, int[] cost) {
+//         int totalGas = 0, totalCost = 0, startIndex = 0, tank = 0;
+//         for(int i =0; i<gas.length; i++){
+//             totalGas += gas[i];
+//             totalCost += cost[i];
+//             tank += gas[i] - cost[i];
+//             if(tank < 0){
+//                 tank = 0;
+//                 startIndex = i+1;
+//             }
+//         }
+//         if(totalGas < totalCost) return -1;
+//         return startIndex;
+//     }
+// }
+
+class Solution{
+    public int canCompleteCircuit(int[] gas, int[] cost){
+        int totalGas = 0;
+        int totalCost = 0;
+        int tank = 0;
+        int startInd = 0;
+
+        for(int i = 0; i< gas.length; i++){
             totalGas += gas[i];
             totalCost += cost[i];
             tank += gas[i] - cost[i];
             if(tank < 0){
                 tank = 0;
-                startIndex = i+1;
+                startInd = i + 1;
             }
         }
-        if(totalGas < totalCost) return -1;
-        return startIndex;
+        if(totalCost > totalGas) return -1;
+        return startInd;
     }
 }
