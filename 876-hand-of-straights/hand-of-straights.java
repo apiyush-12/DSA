976ea411c914bedf3a853e749084e8c5c1db9675
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-18 22:23:54 +0530

    Time: 29 ms (83.04%) | Memory: 48.4 MB (11.60%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -23,27 +23,48 @@
 // }
 
 
-class Solution {
-    public boolean isNStraightHand(int[] hand, int groupSize) {
+// class Solution {
+//     public boolean isNStraightHand(int[] hand, int groupSize) {
+//         int n = hand.length;
+//         if(n%groupSize!=0){
+//             return false;
+//         }
+//         HashMap<Integer,Integer> freq = new HashMap<>();
+//         for(int num : hand){
+//             freq.put(num,freq.getOrDefault(num,0)+1);
+//         }
+//         Arrays.sort(hand);
+//         for(int num : hand){
+//             if(freq.get(num)==0){
+//                 continue;
+//             }
+//             for(int j=0;j<groupSize;j++){
+//                 int cur =  num + j;
+//                 if(freq.getOrDefault(cur,0)==0){
+//                     return false;
+//                 }
+//                 freq.put(cur,freq.get(cur)-1);
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+class Solution{
+    public boolean isNStraightHand(int[] hand, int groupSize){
         int n = hand.length;
-        if(n%groupSize!=0){
-            return false;
-        }
-        HashMap<Integer,Integer> freq = new HashMap<>();
+        if(n % groupSize != 0) return false;
+        HashMap<Integer, Integer> freq = new HashMap<>();
         for(int num : hand){
-            freq.put(num,freq.getOrDefault(num,0)+1);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
         Arrays.sort(hand);
         for(int num : hand){
-            if(freq.get(num)==0){
-                continue;
-            }
-            for(int j=0;j<groupSize;j++){
-                int cur =  num + j;
-                if(freq.getOrDefault(cur,0)==0){
-                    return false;
-                }
-                freq.put(cur,freq.get(cur)-1);
+            if(freq.get(num) == 0) continue;
+            for(int j=0; j<groupSize; j++){
+                int curr = num + j;
+                if(freq.getOrDefault(curr, 0) == 0) return false;
+                freq.put(curr, freq.get(curr)-1);
             }
         }
         return true;
