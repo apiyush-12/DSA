40d29c5755a1750949d669654fa93e4e484a021f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-23 23:40:47 +0530

    Time: 2 ms (88.73%) | Memory: 168.8 MB (45.32%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -14,10 +14,14 @@
 //     }
 // }
 
+
 class Solution{
     public boolean mergeTriplets(int[][] triplets, int[] target){
         int[] maxVal = new int[3];
         for(int[] triplet : triplets){
+            if(maxVal[0] == target[0] && maxVal[1] == target[1] && maxVal[2] == target[2]){
+                return true;
+            }
             if(triplet[0] <= target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]){
                 maxVal[0] = Math.max(maxVal[0], triplet[0]);
                 maxVal[1] = Math.max(maxVal[1], triplet[1]);
