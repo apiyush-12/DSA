44478dc8e2e5941a0300576b5ecd243048099050
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-25 00:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (65.42%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int maxOpen = 0, minOpen = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                minOpen++;
+                maxOpen++;
+            }else if(c == ')'){
+                minOpen--;
+                maxOpen--;
+            }else{    // we found star -> '*'
+                minOpen--;
+                maxOpen++;
+            }
+
+            if(maxOpen < 0){
+                return false;
+            }
+            minOpen = Math.max(minOpen, 0);
+        }
+        return minOpen == 0;
+    }
+}
