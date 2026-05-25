3df81799984600eb96766694ddb2c7a964251507
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-25 22:03:24 +0530

    Time: 1 ms (98.06%) | Memory: 46.9 MB (86.96%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,33 +1,57 @@
+// class Solution {
+//     public int[][] insert(int[][] intervals, int[] newInterval) {
+//         int newStart = newInterval[0];
+//         int newEnd = newInterval[1];
+//         int left = 0, right = intervals.length;
+//         LinkedList<int[]> output = new LinkedList<int[]>();
+//         while(left < right && newStart > intervals[left][0]){
+//             output.add(intervals[left]);
+//             left++;
+//         }
+//         int[] interval = new int[2];
+//         if(output.isEmpty() || output.getLast()[1] < newStart){
+//             output.add(newInterval);
+//         }else{
+//             interval = output.removeLast();
+//             interval[1] = Math.max(interval[1], newEnd);
+//             output.add(interval);
+//         }
+//         while(left < right){
+//             interval = intervals[left];
+//             left++;
+//             int start = interval[0], end = interval[1];
+//             if(output.getLast()[1] <  start){
+//                 output.add(interval);
+//             }else{
+//                 interval = output.removeLast();
+//                 interval[1] = Math.max(interval[1], end);
+//                 output.add(interval);
+//             }
+//         }
+//         return output.toArray(new int[output.size()][2]);
+//     }
+// }
+
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int newStart = newInterval[0];
-        int newEnd = newInterval[1];
-        int left = 0, right = intervals.length;
-        LinkedList<int[]> output = new LinkedList<int[]>();
-        while(left < right && newStart > intervals[left][0]){
-            output.add(intervals[left]);
-            left++;
+        List<int[]> merge = new ArrayList<>();
+        int i = 0;
+        int l = intervals.length;
+        while(i < l && intervals[i][1] < newInterval[0]){
+            merge.add(intervals[i]);
+            i++;
         }
-        int[] interval = new int[2];
-        if(output.isEmpty() || output.getLast()[1] < newStart){
-            output.add(newInterval);
-        }else{
-            interval = output.removeLast();
-            interval[1] = Math.max(interval[1], newEnd);
-            output.add(interval);
+        while(i < l && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
         }
-        while(left < right){
-            interval = intervals[left];
-            left++;
-            int start = interval[0], end = interval[1];
-            if(output.getLast()[1] <  start){
-                output.add(interval);
-            }else{
-                interval = output.removeLast();
-                interval[1] = Math.max(interval[1], end);
-                output.add(interval);
-            }
+        merge.add(newInterval);
+        while(i < l){
+            merge.add(intervals[i]);
+            i++;
         }
-        return output.toArray(new int[output.size()][2]);
+        return merge.toArray(new int[][]{});
     }
 }
