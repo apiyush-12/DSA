100723934fe90e4e93d5925a0819cc651f292c2c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-25 21:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (81.03%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,21 +1,43 @@
-class Solution {
-    public boolean checkValidString(String s) {
-        int maxOpen = 0, minOpen = 0;
-        for(char c : s.toCharArray()){
-            if(c == '('){
+// class Solution {
+//     public boolean checkValidString(String s) {
+//         int maxOpen = 0, minOpen = 0;
+//         for(char c : s.toCharArray()){
+//             if(c == '('){
+//                 minOpen++;
+//                 maxOpen++;
+//             }else if(c == ')'){
+//                 minOpen--;
+//                 maxOpen--;
+//             }else{    // we found star -> '*'
+//                 minOpen--;
+//                 maxOpen++;
+//             }
+
+//             if(maxOpen < 0){
+//                 return false;
+//             }
+//             minOpen = Math.max(minOpen, 0);
+//         }
+//         return minOpen == 0;
+//     }
+// }
+
+class Solution{
+    public boolean checkValidString(String s){
+        int minOpen = 0, maxOpen =0;
+        for(int ch : s.toCharArray()){
+            if(ch == '('){
                 minOpen++;
                 maxOpen++;
-            }else if(c == ')'){
+            }else if(ch == ')'){
                 minOpen--;
                 maxOpen--;
-            }else{    // we found star -> '*'
+            }else{
                 minOpen--;
                 maxOpen++;
             }
 
-            if(maxOpen < 0){
-                return false;
-            }
+            if(maxOpen < 0) return false;
             minOpen = Math.max(minOpen, 0);
         }
         return minOpen == 0;
