1e2c94c6768b3a42d5c209cc312a5cb99574d004
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-26 23:41:39 +0530

    Time: 8 ms (90.62%) | Memory: 49.2 MB (38.93%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,11 +1,28 @@
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+//         LinkedList<int[]> res = new LinkedList<>();   
+//         for(int[] interval : intervals){
+//             if(res.isEmpty() || res.getLast()[1] < interval[0]) res.add(interval);
+//             else res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
+//         }
+//         return res.toArray(new int[res.size()][]);
+//     }
+// }
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> res = new LinkedList<>();   
-        for(int[] interval : intervals){
-            if(res.isEmpty() || res.getLast()[1] < interval[0]) res.add(interval);
-            else res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]> l = new ArrayList<>();
+        int[]prev=intervals[0];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=prev[1]) prev[1]=Math.max(prev[1],intervals[i][1]);
+            else{
+                l.add(prev);
+                prev=intervals[i];
+            }
         }
-        return res.toArray(new int[res.size()][]);
+        l.add(prev);
+        return l.toArray(new int[l.size()][]);
     }
 }
