ca5a3e1e2c856102ea769be51892125528650b3e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-27 23:13:02 +0530

    Time: 46 ms (68.45%) | Memory: 116 MB (21.57%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -14,17 +14,34 @@
 //     }
 // }
 
+// class Solution {
+//     public int eraseOverlapIntervals(int[][] intervals) {
+//         int result = 0;
+//         Arrays.sort(intervals, (a,b)->{return a[1] - b[1]; });
+//         int[] prev = intervals[0];
+//         for(int i = 1;i<intervals.length;i++) {
+//             int[] curr = intervals[i];
+//             if(prev[1] > curr[0]) {
+//                 result++; 
+//             } else prev = curr;
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int result = 0;
-        Arrays.sort(intervals, (a,b)->{return a[1] - b[1]; });
-        int[] prev = intervals[0];
-        for(int i = 1;i<intervals.length;i++) {
-            int[] curr = intervals[i];
-            if(prev[1] > curr[0]) {
-                result++; 
-            } else prev = curr;
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int count = 0;
+        int prevEnd = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) { 
+                count++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
         }
-        return result;
+        return count;
     }
 }
