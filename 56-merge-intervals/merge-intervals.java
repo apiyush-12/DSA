38980afe2d2d599ff5c83736b39ee512cf80908b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-05-27 22:32:31 +0530

    Time: 8 ms (90.59%) | Memory: 49.2 MB (24.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -28,27 +28,48 @@
 // }
 
 
-class Solution {
-    public int[][] merge(int[][] intervals) {
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         List<int[]> res = new ArrayList<>();
+//         int rows = intervals.length;
+//         if (rows == 0) {
+//             return res.toArray(new int[res.size()][]);
+//         }
+//         Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+//         int start = intervals[0][0];
+//         int end = intervals[0][1];
+//         for (int i = 1; i < rows; i++) {
+//             if (intervals[i][0] <= end) {
+//                 end = Math.max(end, intervals[i][1]);
+//             } else {
+//                 res.add(new int[] {start, end});
+//                 start = intervals[i][0];
+//                 end = intervals[i][1];
+//             }
+//         }
+//         res.add(new int[] {start, end});
+//         return res.toArray(new int[res.size()][]);
+//     }
+// }
+
+class Solution{
+    public int[][] merge(int[][] intervals){
         List<int[]> res = new ArrayList<>();
         int rows = intervals.length;
-        if (rows == 0) {
-            return res.toArray(new int[res.size()][]);
-        }
-        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+        if(rows == 0) return res.toArray(new int[res.size()][]);
+        Arrays.sort(intervals, (a, b) -> a[0]-b[0]);
         int start = intervals[0][0];
         int end = intervals[0][1];
-        for (int i = 1; i < rows; i++) {
-            if (intervals[i][0] <= end) {
+        for(int i = 1; i< rows; i++){
+            if(intervals[i][0] <=end){
                 end = Math.max(end, intervals[i][1]);
-            } else {
-                res.add(new int[] { start, end });
+            }else{
+                res.add(new int[] {start, end});
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        res.add(new int[] { start, end });
+        res.add(new int[] {start, end});
         return res.toArray(new int[res.size()][]);
-
     }
 }
