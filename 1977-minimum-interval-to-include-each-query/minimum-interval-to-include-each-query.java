8307473a51c922417333bdbd3fe7338d29f179d6
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-03 23:12:42 +0530

    Time: 108 ms (46.96%) | Memory: 138.2 MB (73.93%) - LeetSync

---
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        int n = queries.length;
+        int[] result = new int[n];
+
+        Integer[] queryIndices = new Integer[n];
+        for(int i = 0; i<n; i++){
+            queryIndices[i] = i;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(queryIndices, (a,b) -> Integer.compare(queries[a], queries[b]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[1]-a[0], b[1]-b[0]));
+        int intervalIndex = 0;
+        for(int i = 0; i<n; i++){
+            int query = queries[queryIndices[i]];
+
+            while(intervalIndex < intervals.length && intervals[intervalIndex][0] <= query){
+                int left = intervals[intervalIndex][0];
+                int right = intervals[intervalIndex][1];
+                if(right >= query) minHeap.offer(new int[]{left, right});
+                intervalIndex++;
+            }
+
+            while(!minHeap.isEmpty() && minHeap.peek()[1] < query) minHeap.poll();
+            if(minHeap.isEmpty()) result[queryIndices[i]] = -1;
+            else{
+                int[] smallInterval = minHeap.peek();
+                result[queryIndices[i]] = smallInterval[1]-smallInterval[0]+1;
+            }
+        }
+        return result;
+    }
+}
