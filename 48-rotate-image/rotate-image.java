1e9512c6213efc4f5cae7e117de1adb8786237ba
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-05 17:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (67.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,56 +1,15 @@
 class Solution {
-    // piyush //
-    // public void rotate(int[][] matrix) {
-    //     int n=matrix.length;
-    //     for(int i=0; i<n; i++){
-    //         for(int j=i; j<n; j++){
-    //             int k=matrix[i][j];
-    //             matrix[i][j]=matrix[j][i];
-    //             matrix[j][i]=k;
-    //         }
-    //     }
-    //     for(int i=0; i<matrix.length; i++){
-    //         int [] tempArr=matrix[i];
-    //         reverse(tempArr);
-    //     }
-    //     return ;
-    // }
-    // public static void reverse(int [] arr){
-    //     int start=0;
-    //     int end=arr.length-1;
-    //     while(start<=end){
-    //         int k=arr[start];
-    //         arr[start]=arr[end];
-    //         arr[end]=k;
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0; i< (n+1)/2; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
 
-    //         start++;
-    //         end--;
-    //     }
-    // }
-    public void rotate(int [][] matrix){
-        int n=matrix.length;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                int k=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=k;
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
             }
-        }
-        for(int i=0; i<n; i++){
-            int [] tempArr=matrix[i];
-            reverse(tempArr);
-        }
-        return;
-    }
-    public static void reverse(int []arr){
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int k=arr[start];
-            arr[start]=arr[end];
-            arr[end]=k;
-            start++;
-            end--;
         }
     }
 }
