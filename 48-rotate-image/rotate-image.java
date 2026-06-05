83d3671e2ea4306b4e17cae8a7f1f7daa4463268
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-05 17:22:04 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (15.33%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i=0; i< (n+1)/2; i++){
             for(int j=0; j<n/2; j++){
                 int temp = matrix[i][j];
-
                 matrix[i][j] = matrix[n-1-j][i];
                 matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
                 matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
