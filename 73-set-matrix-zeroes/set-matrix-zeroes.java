c7a81168d5823cc27766fbbf2b10c489a33c534b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-08 16:13:34 +0530

    Time: 1 ms (98.45%) | Memory: 47.4 MB (90.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,59 @@
-class Solution {
-    public void setZeroes(int[][] matrix) {
-        boolean firstCol = false;
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         boolean firstCol = false;
+//         int r = matrix.length;
+//         int c = matrix[0].length;
+//         for(int i = 0; i<r; i++){
+//             if(matrix[i][0]== 0) firstCol = true;
+//             for(int j=1; j<c; j++){
+//                 if(matrix[i][j] == 0){
+//                     matrix[i][0] = 0;
+//                     matrix[0][j] = 0;
+//                 }
+//             }
+//         } 
+//         for(int i=1; i<r; i++){
+//             for(int j=1; j<c; j++){
+//                 if(matrix[i][0]==0 || matrix[0][j]==0){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//         if(matrix[0][0] == 0){
+//             for(int j=0; j<c; j++) matrix[0][j] = 0;
+//         }
+//         if(firstCol){
+//             for(int i=0; i<r; i++) matrix[i][0] = 0;
+//         }
+//     }
+// }
+
+class Solution{
+    public void setZeroes(int[][] matrix){
+        boolean firstrow = false;
         int r = matrix.length;
         int c = matrix[0].length;
-        for(int i = 0; i<r; i++){
-            if(matrix[i][0]== 0) firstCol = true;
+        for(int i=0; i<r; i++){
+            if(matrix[i][0]==0) firstrow = true;
             for(int j=1; j<c; j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
-            }
-        } 
+            } 
+        }
+
         for(int i=1; i<r; i++){
             for(int j=1; j<c; j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j] = 0;
-                }
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j] = 0;
             }
         }
+
         if(matrix[0][0] == 0){
-            for(int j=0; j<c; j++) matrix[0][j] = 0;
+            for(int j=1; j<c; j++) matrix[0][j] = 0;
         }
-        if(firstCol){
+
+        if(firstrow){
             for(int i=0; i<r; i++) matrix[i][0] = 0;
         }
     }
