e9f5afe264f9230c03dd1b77fbf6c1c6a07e9eea
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-11 00:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (55.98%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        long N = n;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        double result = 1;
+        double currentPro = x;
+        while(N > 0){
+            if(N % 2 == 1){
+                result *= currentPro;
+            }
+            currentPro *= currentPro;
+            N = N/2;
+        }
+        return result;
+    }
+}
