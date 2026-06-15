729fa3ceaaf968f47b2f2d6fd0ba3132bee70f48
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-15 22:06:15 +0530

    Time: 4 ms (98.31%) | Memory: 100.9 MB (81.10%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,11 +10,40 @@
  */
 
 
-class Solution {
-    public int pairSum(ListNode head) {
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast.next != null &&fast.next.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         ListNode prev = null;
+//         ListNode curr = slow.next;
+//         ListNode next;
+//         while(curr != null){
+//             next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
+//         int ans = 0;
+//         while(prev != null){
+//             int temp = prev.val + head.val;
+//             prev = prev.next;
+//             head = head.next;
+//             ans = Math.max(ans, temp);
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution{
+    public int pairSum(ListNode head){
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next != null &&fast.next.next != null){
+        while(fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
