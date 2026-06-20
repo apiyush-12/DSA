63325672b95e05882aee3ac9194fc0b59412f268
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-21 00:04:35 +0530

    Time: 58 ms (26.47%) | Memory: 123.4 MB (82.35%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.java
+++ 1968-maximum-building-height/maximum-building-height.java
@@ -28,19 +28,14 @@ class Solution {
             int d = list.get(i+1)[0] - list.get(i)[0];
             list.get(i)[1] = Math.min(list.get(i)[1], list.get(i+1)[1] + d);
         }
-
         long ans = 0;
         for(int i=1; i<m; i++){
             long x1 = list.get(i-1)[0];
             long h1 = list.get(i-1)[1];
-
             long x2 = list.get(i)[0];
             long h2 = list.get(i)[1];
-
             long d = x2 - x1;
-
             long peak = (h1 + h2 + d) / 2;
-
             ans = Math.max(ans, peak);
         }
         return (int)ans;
