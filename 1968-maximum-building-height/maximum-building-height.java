9fec53a5a87fe1fb83088785eff227badb01f3ae
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-21 17:26:29 +0530

    Time: 54 ms (80.56%) | Memory: 126.7 MB (5.56%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.java
+++ 1968-maximum-building-height/maximum-building-height.java
@@ -44,32 +44,60 @@
 
 
 
-class Solution {
-    public int maxBuilding(int n, int[][] restrictions) {
-        Arrays.sort(restrictions, (a, b) -> a[0] - b[0]);
+// class Solution {
+//     public int maxBuilding(int n, int[][] restrictions) {
+//         Arrays.sort(restrictions, (a, b) -> a[0] - b[0]);
+//         int len = restrictions.length + 2;
+//         int[][] arr = new int[len][2];
+//         arr[0][0] = 1;
+//         arr[0][1] = 0;
+//         arr[len-1][0] = n;
+//         arr[len-1][1] = n-1;
+//         for(int i = 1; i<len-1; ++i){
+//             arr[i] = restrictions[i-1];
+//         }
+//         for(int i = 0; i<len-1; ++i){
+//             arr[i+1][1] = Math.min(arr[i+1][1], arr[i][1] + (arr[i+1][0] - arr[i][0]));
+//         }
+//         for(int i = len-1; i>0; --i){
+//             arr[i-1][1] = Math.min(arr[i-1][1], arr[i][1] + (arr[i][0] - arr[i-1][0]));
+//         }
+//         int max = 0;
+//         for(int i = 0; i<len-1; ++i){
+//             int x1 = arr[i][0], h1 = arr[i][1], x2 = arr[i+1][0], h2 = arr[i+1][1];            
+//             int diff = Math.max(h1, h2) - Math.min(h1, h2);
+//             x1 += diff;
+//             max = Math.max(max, Math.max(h1, h2) + (x2 - x1)/2);
+//         }
+        
+//         return max;
+//     }
+// }
+
+class Solution{
+    public int maxBuilding(int n, int[][] restrictions){
+        Arrays.sort(restrictions, (a, b) -> a[0]- b[0]);
         int len = restrictions.length + 2;
         int[][] arr = new int[len][2];
         arr[0][0] = 1;
         arr[0][1] = 0;
         arr[len-1][0] = n;
         arr[len-1][1] = n-1;
-        for(int i = 1; i<len-1; ++i){
-            arr[i] = restrictions[i-1];
+        for(int i=1; i<len-1; i++) arr[i] = restrictions[i-1];
+
+        for(int i = 0; i<len-1; i++){
+            arr[i+1][1] = Math.min(arr[i+1][1], arr[i][1]+ (arr[i+1][0]-arr[i][0]));
         }
-        for(int i = 0; i<len-1; ++i){
-            arr[i+1][1] = Math.min(arr[i+1][1], arr[i][1] + (arr[i+1][0] - arr[i][0]));
-        }
-        for(int i = len-1; i>0; --i){
-            arr[i-1][1] = Math.min(arr[i-1][1], arr[i][1] + (arr[i][0] - arr[i-1][0]));
+        for(int i = len-1; i>0; i--){
+            arr[i-1][1] = Math.min(arr[i-1][1], arr[i][1]+(arr[i][0]-arr[i-1][0]));
         }
         int max = 0;
-        for(int i = 0; i<len-1; ++i){
-            int x1 = arr[i][0], h1 = arr[i][1], x2 = arr[i+1][0], h2 = arr[i+1][1];            
+        for(int i=0; i<len-1; i++){
+            int x1 = arr[i][0], h1 = arr[i][1], x2 = arr[i+1][0], h2 = arr[i+1][1];
             int diff = Math.max(h1, h2) - Math.min(h1, h2);
             x1 += diff;
-            max = Math.max(max, Math.max(h1, h2) + (x2 - x1)/2);
+            max = Math.max(max, Math.max(h1, h2) + (x2-x1)/2);
         }
-        
         return max;
     }
 }
