9b474d5efbd5944fce2f15edb4a75b5f8f7bf205
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-22 23:40:35 +0530

    Time: 4 ms (99.24%) | Memory: 83 MB (87.17%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,18 +1,35 @@
-class Solution {
-    public int maxIceCream(int[] costs, int coins) {
+// class Solution {
+//     public int maxIceCream(int[] costs, int coins) {
+//         int maxCost = 0;
+//         for(int cost : costs) maxCost = Math.max(maxCost, cost);
+        
+//         int[] freq = new int[maxCost + 1];
+
+//         for(int cost : costs) freq[cost]++;
+
+//         int bars = 0;
+//         for(int cost = 1; cost<=maxCost && coins >= cost; cost++){
+//             if(freq[cost]==0) continue;
+//             int canBuy = Math.min(freq[cost], coins / cost);
+//             bars += canBuy;
+//             coins -= canBuy * cost;
+//         }
+//         return bars;
+//     }
+// }
+
+class Solution{
+    public int maxIceCream(int[] costs, int coins){
         int maxCost = 0;
         for(int cost : costs) maxCost = Math.max(maxCost, cost);
-        
         int[] freq = new int[maxCost + 1];
-
         for(int cost : costs) freq[cost]++;
-
         int bars = 0;
-        for(int cost = 1; cost<=maxCost && coins >= cost; cost++){
-            if(freq[cost]==0) continue;
-            int canBuy = Math.min(freq[cost], coins / cost);
+        for(int i=1; i<=maxCost && coins>=i; i++){
+            if(freq[i] == 0) continue;
+            int canBuy = Math.min(freq[i], coins/i);
             bars += canBuy;
-            coins -= canBuy * cost;
+            coins -= canBuy * i;
         }
         return bars;
     }
