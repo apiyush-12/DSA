618252dfe45a51fc286fddc0bd0c19862e27892b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-22 23:32:01 +0530

    Time: 2 ms (96.67%) | Memory: 43.1 MB (87.41%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -13,17 +13,32 @@
 // }
 
 // piyush
-class Solution {
-    public int maxNumberOfBalloons(String text) {
-        int[] count = new int[26];
-        for(char c : text.toCharArray()){
-            count[c - 'a']++;
-        }
-        int b = count['b' - 'a'];
-        int a = count['a' - 'a'];
-        int l = count['l' - 'a'] / 2;
-        int o = count['o' - 'a'] / 2;
-        int n = count['n' - 'a'];
-        return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
+// class Solution {
+//     public int maxNumberOfBalloons(String text) {
+//         int[] count = new int[26];
+//         for(char c : text.toCharArray()){
+//             count[c - 'a']++;
+//         }
+//         int b = count['b' - 'a'];
+//         int a = count['a' - 'a'];
+//         int l = count['l' - 'a'] / 2;
+//         int o = count['o' - 'a'] / 2;
+//         int n = count['n' - 'a'];
+//         return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
+//     }
+// }
+
+class Solution{
+    public int maxNumberOfBalloons(String text){
+        int[] freq = new int[26];
+        for(char ch : text.toCharArray()) freq[ch - 'a']++;
+        int ans = Integer.MAX_VALUE;
+        ans = Math.min(ans, freq['b'-'a']);
+        ans = Math.min(ans, freq['a'-'a']);
+        ans = Math.min(ans, freq['l'-'a']/2);
+        ans = Math.min(ans, freq['o'-'a']/2);
+        ans = Math.min(ans, freq['n'-'a']);
+
+        return ans;
     }
 }
