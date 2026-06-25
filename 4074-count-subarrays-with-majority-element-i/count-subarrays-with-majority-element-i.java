463c6e68dfe9b9c9c7e1d1cc28ac4971d67ba98c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-25 17:04:39 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (88.92%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -1,12 +1,33 @@
+// class Solution {
+//     public int countMajoritySubarrays(int[] nums, int target) {
+//         int n = nums.length, count = 0;
+//         for(int i=0; i<n; i++){
+//             int tempCount = 0;
+//             for(int j=i; j<n; j++){
+//                 if(nums[j] == target) tempCount++;
+//                 if(tempCount * 2 > j-i+1) count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
-        int n = nums.length, count = 0;
-        for(int i=0; i<n; i++){
-            int tempCount = 0;
-            for(int j=i; j<n; j++){
-                if(nums[j] == target) tempCount++;
-                if(tempCount * 2 > j-i+1) count++;
-            }
+        int n = nums.length;
+        int pos = n+1;
+        int len = 2*n+2;
+        int[] freq = new int[len];
+        int[] cum = new int[len];
+        freq[pos] = 1;
+        cum[pos] = 1;
+        int count = 0;
+        for(int num: nums) {
+            int offset = (num == target) ? 1 : -1;
+            pos += offset;
+            freq[pos]++;
+            cum[pos] = cum[pos-1] + freq[pos];
+            count += cum[pos-1];
         }
         return count;
     }
