6f5840a8053f9ef5d42ee84c5817f9cda30ef496
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-26 23:14:23 +0530

    Time: 2 ms (100.00%) | Memory: 88 MB (72.13%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -1,21 +1,45 @@
+// class Solution {
+//     public long countMajoritySubarrays(int[] nums, int target) {
+//         int n = nums.length;
+//         int len = 2*n+2;
+//         long[] countAtSum = new long[len];
+//         long[] cumSum = new long[len];
+//         int idx = n+1;
+//         countAtSum[idx] = 1;
+//         cumSum[idx] = 1;
+//         long ans = 0;
+//         for(int num : nums){
+//             if(num == target) idx++;
+//             else idx--;
+//             countAtSum[idx]++;
+//             ans += cumSum[idx-1];
+//             cumSum[idx] = cumSum[idx-1]+countAtSum[idx];
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-        int len = 2*n+2;
-        long[] countAtSum = new long[len];
-        long[] cumSum = new long[len];
-        int idx = n+1;
-        countAtSum[idx] = 1;
-        cumSum[idx] = 1;
+        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1, ..., n, with index offset by n.
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
         long ans = 0;
-        for(int num : nums){
-            if(num == target) idx++;
-            else idx--;
-            countAtSum[idx]++;
-            ans += cumSum[idx-1];
-            cumSum[idx] = cumSum[idx-1]+countAtSum[idx];
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
         }
         return ans;
     }
 }
-
