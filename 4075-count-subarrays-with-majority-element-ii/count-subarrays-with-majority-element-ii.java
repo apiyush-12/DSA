e9ab37ff26a33eca15b4a2e4f0b8550715eb1f4d
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-26 23:10:40 +0530

    Time: 4 ms (85.25%) | Memory: 90.8 MB (49.18%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int len = 2*n+2;
+        long[] countAtSum = new long[len];
+        long[] cumSum = new long[len];
+
+        int idx = n+1;
+        countAtSum[idx] = 1;
+        cumSum[idx] = 1;
+        long ans = 0;
+        for(int num : nums){
+            if(num == target) idx++;
+            else idx--;
+            countAtSum[idx]++;
+            ans += cumSum[idx-1];
+            cumSum[idx] = cumSum[idx-1]+countAtSum[idx];
+        }
+        return ans;
+    }
+}
+
