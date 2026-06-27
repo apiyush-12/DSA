8c63fd1ef6e312d5cd72b0b6ca438989d6486415
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-27 17:07:51 +0530

    Time: 52 ms (86.05%) | Memory: 67.5 MB (56.98%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -1,28 +1,58 @@
-class Solution {
-    public int maximumLength(int[] nums) {
-        HashMap<Long, Integer> hmap = new HashMap<>();
-        for(int num : nums){
-            hmap.put((long)num, hmap.getOrDefault((long)num, 0) + 1);
-        }
+// class Solution {
+//     public int maximumLength(int[] nums) {
+//         HashMap<Long, Integer> hmap = new HashMap<>();
+//         for(int num : nums){
+//             hmap.put((long)num, hmap.getOrDefault((long)num, 0) + 1);
+//         }
+//         int ans = 0;
+//         if(hmap.containsKey(1L)){
+//             int ones = hmap.get(1L);
+//             ans = (ones%2==0) ? ones-1 : ones;
+//             hmap.remove(1L);
+//         }
+//         for(long num : hmap.keySet()){
+//             int count = 0;
+//             long curr = num;
+//             while(hmap.containsKey(curr)){
+//                 if(hmap.get(curr) >= 2) count += 2;
+//                 else{
+//                     count++;
+//                     break;
+//                 }
+//                 curr = curr * curr;
+//             }
+//             if(count%2 == 0) count--;
+//             ans = Math.max(ans, count);
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution{
+    public int maximumLength(int[] nums){
+        Map<Long, Integer> cnt = new HashMap<>();
+        for(int x : nums) cnt.merge((long)x, 1, Integer::sum);
         int ans = 0;
-        if(hmap.containsKey(1L)){
-            int ones = hmap.get(1L);
-            ans = (ones%2==0) ? ones-1 : ones;
-            hmap.remove(1L);
+        if(cnt.containsKey(1L)){
+            int ones = cnt.get(1L);
+            ans = (ones % 2 == 1) ? ones : ones-1;
+            cnt.remove(1L);
         }
-        for(long num : hmap.keySet()){
-            int count = 0;
-            long curr = num;
-            while(hmap.containsKey(curr)){
-                if(hmap.get(curr) >= 2) count += 2;
-                else{
-                    count++;
-                    break;
-                }
-                curr = curr * curr;
+
+        for(long start : cnt.keySet()){
+            long x = start;
+            int len = 0;
+            while(cnt.getOrDefault(x,0) > 1){
+                len += 2;
+                x = x*x;
             }
-            if(count%2 == 0) count--;
-            ans = Math.max(ans, count);
+            if(cnt.getOrDefault(x, 0) == 1){
+                len +=1;
+            }else{
+                len -=1;
+            }
+            ans = Math.max(ans, len);
         }
         return ans;
     }
