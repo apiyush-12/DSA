daab9bc39faa34f70e0328b5d6fee0764251a2f6
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-06-28 16:18:21 +0530

    Time: 52 ms (86.05%) | Memory: 67.6 MB (56.98%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -55,7 +55,7 @@
 //     }
 // }
 
-
+// piyush
 class Solution{
     public int maximumLength(int[] nums){
         Map<Long, Integer> cnt = new HashMap<>();
@@ -66,6 +66,7 @@ class Solution{
             ans = (ones%2 == 1) ? ones : ones-1;
             cnt.remove(1L);
         }
+
         for(long start : cnt.keySet()){
             long x = start;
             int len = 0;
@@ -73,6 +74,7 @@ class Solution{
                 len += 2;
                 x = x*x;
             }
+
             if(cnt.getOrDefault(x, 0) == 1) len += 1;
             else len -= 1;
             ans = Math.max(ans, len);
