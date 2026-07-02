c256765c26eeb7855b3803b99790dbad98b96f70
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-02 23:05:14 +0530

    Time: 9 ms (97.32%) | Memory: 46.4 MB (44.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -32,17 +32,35 @@
 
 
 // piyush
-class Solution{
-    public int numberOfSubstrings(String s){
-        int[] last = {-1, -1, -1};
-        int count = 0;
-        for(int i=0; i<s.length(); i++){
-            last[s.charAt(i)-'a'] = i;
-            if(last[0] != -1 && last[1] != -1 && last[2] != -1){
-                int minIdx = Math.min(last[0], Math.min(last[1], last[2]));
-                count +=(minIdx + 1);
+// class Solution{
+//     public int numberOfSubstrings(String s){
+//         int[] last = {-1, -1, -1};
+//         int count = 0;
+//         for(int i=0; i<s.length(); i++){
+//             last[s.charAt(i)-'a'] = i;
+//             if(last[0] != -1 && last[1] != -1 && last[2] != -1){
+//                 int minIdx = Math.min(last[0], Math.min(last[1], last[2]));
+//                 count +=(minIdx + 1);
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n=s.length();
+        int count=0;
+        int left=0;  
+        char freq[]=new char[3];
+        for(int right=0; right<n; right++){
+            freq[s.charAt(right)-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+                count += n-right;
+                freq[s.charAt(left++)-'a']--;
             }
-        }
+        }      
         return count;
     }
 }
