5798ec12f351beaee45ee45d24b8fac53bd3eb44
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-02 23:03:38 +0530

    Time: 13 ms (69.38%) | Memory: 46.4 MB (44.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -40,7 +40,7 @@ class Solution{
             last[s.charAt(i)-'a'] = i;
             if(last[0] != -1 && last[1] != -1 && last[2] != -1){
                 int minIdx = Math.min(last[0], Math.min(last[1], last[2]));
-                count += (minIdx + 1);
+                count +=(minIdx + 1);
             }
         }
         return count;
