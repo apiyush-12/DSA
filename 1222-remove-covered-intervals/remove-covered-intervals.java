80985f4d733a65ede4ca9ec00a678d3059e82d33
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-06 22:39:54 +0530

    Time: 7 ms (52.31%) | Memory: 46.4 MB (62.65%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,22 +1,37 @@
+// class Solution {
+//     public int removeCoveredIntervals(int[][] intervals) {
+//         int n = intervals.length;
+//         Arrays.sort(intervals, (a, b) -> {
+//             if (a[0] == b[0])
+//                 return b[1] - a[1];
+//             return a[0] - b[0];
+//         });
+
+//         int remove = 0;
+//         int prev = 0;
+//         for (int i = 1; i < n; i++) {
+//             int start = intervals[i][0], end = intervals[i][1];
+//             int pStart = intervals[prev][0], pEnd = intervals[prev][1];
+//             if (start >= pStart && end <= pEnd)
+//                 remove++;
+//             else
+//                 prev = i;
+//         }
+//         return n - remove;
+//     }
+// }
+
+
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        int n = intervals.length;
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] == b[0])
-                return b[1] - a[1];
-            return a[0] - b[0];
-        });
-
-        int remove = 0;
-        int prev = 0;
-        for (int i = 1; i < n; i++) {
-            int start = intervals[i][0], end = intervals[i][1];
-            int pStart = intervals[prev][0], pEnd = intervals[prev][1];
-            if (start >= pStart && end <= pEnd)
-                remove++;
-            else
-                prev = i;
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int count = 0, maxEnd = 0;
+        for (int[] interval : intervals) {
+            if (interval[1] > maxEnd) {
+                count++;
+                maxEnd = interval[1];
+            }
         }
-        return n - remove;
+        return count;
     }
 }
