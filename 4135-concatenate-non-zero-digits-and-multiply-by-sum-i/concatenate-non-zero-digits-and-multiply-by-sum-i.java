cbf4ab8e6e7e312075d40f516a5076a182bc64e9
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-08 20:15:01 +0530

    Time: 1 ms (99.85%) | Memory: 42.8 MB (19.12%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,5 +1,20 @@
-class Solution {
-    public long sumAndMultiply(int n) {
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         String s = Integer.toString(n);
+//         long ans = 0, sum = 0;
+//         for(int i=0; i<s.length(); i++){
+//             int digit = s.charAt(i)-'0';
+//             if(digit != 0){
+//                 sum += digit;
+//                 ans = ans*10 + digit;
+//             }
+//         }
+//         return ans*sum;
+//     }
+// }
+
+class Solution{
+    public long sumAndMultiply(int n){
         String s = Integer.toString(n);
         long ans = 0, sum = 0;
         for(int i=0; i<s.length(); i++){
