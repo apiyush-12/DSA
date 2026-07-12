c55b204c919089f75f3ace05eaf22036bc102d8f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-12 22:06:52 +0530

    Time: 30 ms (71.06%) | Memory: 76.9 MB (38.30%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,22 +1,41 @@
-class Solution {
-    public int[] arrayRankTransform(int[] arr) {
+// class Solution {
+//     public int[] arrayRankTransform(int[] arr) {
+//         int n = arr.length;
+//         if(n==0) return new int[]{};
+//         int[][] temp = new int[n][2];
+//         for(int i=0; i<n; i++){
+//             temp[i][0] = arr[i];
+//             temp[i][1] = i;
+//         }
+//         Arrays.sort(temp, (a, b) -> Integer.compare(a[0], b[0]));
+//         int[] ans = new int[n];
+//         int rank = 1, prev = temp[0][0], ind = temp[0][1];
+//         ans[ind] = 1;
+//         for(int i=1; i<n; i++){
+//             int idx = temp[i][1], val = temp[i][0];
+//             if(prev != val) rank++;
+//             ans[idx] = rank;
+//             prev = val;
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution{
+    public int[] arrayRankTransform(int[] arr){
         int n = arr.length;
-        if(n==0) return new int[]{};
-        int[][] temp = new int[n][2];
+        int[] sort = new int[n];
         for(int i=0; i<n; i++){
-            temp[i][0] = arr[i];
-            temp[i][1] = i;
+            sort[i] = arr[i];
         }
-        Arrays.sort(temp, (a, b) -> Integer.compare(a[0], b[0]));
-        int[] ans = new int[n];
-        int rank = 1, prev = temp[0][0], ind = temp[0][1];
-        ans[ind] = 1;
-        for(int i=1; i<n; i++){
-            int idx = temp[i][1], val = temp[i][0];
-            if(prev != val) rank++;
-            ans[idx] = rank;
-            prev = val;
+        Arrays.sort(sort);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i=1;
+        for(int e : sort){
+            if(!map.containsKey(e)) map.put(e, i++);
         }
-        return ans;
+        for(int k=0; k<n; k++) arr[k] = map.get(arr[k]);
+        return arr;
     }
 }
