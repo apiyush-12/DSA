c38fb7fa2d3574121bd7cf290d13f13fa26ea006
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-12 21:36:58 +0530

    Time: 9 ms (82.71%) | Memory: 47.9 MB (20.62%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -39,46 +39,81 @@
 // }
 
 
-class Solution {
-    public int countCompleteComponents(int n, int[][] edges) {
+// class Solution {
+//     public int countCompleteComponents(int n, int[][] edges) {
+//         ArrayList<Integer>[] graph = new ArrayList[n];
+//         for (int i = 0; i < n; i++) {
+//             graph[i] = new ArrayList<>();
+//         }
+//         for (int[] e : edges) {
+//             int a = e[0];
+//             int b = e[1];
+//             graph[a].add(b);
+//             graph[b].add(a);
+//         }
+
+//         boolean[] visited = new boolean[n];
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 ArrayList<Integer> component = new ArrayList<>();
+//                 dfs(i, graph, visited, component);
+//                 int nodes = component.size();
+//                 int degreeSum = 0;
+//                 for (int node : component) {
+//                     degreeSum += graph[node].size();
+//                 }
+//                 int edgesCount = degreeSum / 2;
+//                 if (edgesCount == nodes * (nodes - 1) / 2) {
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+
+//     void dfs(int node, ArrayList<Integer>[] graph, boolean[] visited, ArrayList<Integer> component) {
+//         visited[node] = true;
+//         component.add(node);
+//         for (int next : graph[node]) {
+//             if (!visited[next]) {
+//                 dfs(next, graph, visited, component);
+//             }
+//         }
+//     }
+// }
+
+
+class Solution{
+    public int countCompleteComponents(int n, int[][] edges){
         ArrayList<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int[] e : edges) {
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+        for(int[] e : edges){
             int a = e[0];
             int b = e[1];
             graph[a].add(b);
             graph[b].add(a);
         }
-
         boolean[] visited = new boolean[n];
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
                 ArrayList<Integer> component = new ArrayList<>();
                 dfs(i, graph, visited, component);
                 int nodes = component.size();
                 int degreeSum = 0;
-                for (int node : component) {
-                    degreeSum += graph[node].size();
-                }
-                int edgesCount = degreeSum / 2;
-                if (edgesCount == nodes * (nodes - 1) / 2) {
-                    ans++;
-                }
+                for(int node : component) degreeSum += graph[node].size();
+                int edgesCount = degreeSum;
+                if(edgesCount == nodes*(nodes-1)) ans++;
             }
         }
         return ans;
     }
-
-    void dfs(int node, ArrayList<Integer>[] graph, boolean[] visited, ArrayList<Integer> component) {
+    void dfs(int node, ArrayList<Integer>[] graph, boolean[] visited, ArrayList<Integer> component){
         visited[node] = true;
         component.add(node);
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                dfs(next, graph, visited, component);
-            }
+        for(int next : graph[node]){
+            if(!visited[next]) dfs(next, graph, visited, component);
         }
     }
 }
