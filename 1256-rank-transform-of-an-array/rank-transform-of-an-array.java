76229b3ea3202cffe645749529ca944c14911cd5
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-13 22:06:45 +0530

    Time: 27 ms (98.34%) | Memory: 76.9 MB (32.21%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -22,22 +22,35 @@
 // }
 
 
+// class Solution{
+//     public int[] arrayRankTransform(int[] arr){
+//         int n = arr.length;
+//         int[] sort = new int[n];
+//         for(int i=0; i<n; i++){
+//             sort[i] = arr[i];
+//         }
+//         Arrays.sort(sort);
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int i=1;
+//         for(int e : sort){
+//             if(!map.containsKey(e)) map.put(e, i++);
+//         }
+//         for(int k=0; k<n; k++) arr[k] = map.get(arr[k]);
+//         return arr;
+//     }
+// }
+
 class Solution{
     public int[] arrayRankTransform(int[] arr){
         int n = arr.length;
         int[] sort = new int[n];
-        for(int i=0; i<n; i++){
-            sort[i] = arr[i];
-        }
-
+        for(int i=0; i<n; i++) sort[i] = arr[i];
         Arrays.sort(sort);
         HashMap<Integer, Integer> map = new HashMap<>();
-        int i=1;
-        
+        int i = 1;
         for(int e : sort){
             if(!map.containsKey(e)) map.put(e, i++);
         }
-
         for(int k=0; k<n; k++) arr[k] = map.get(arr[k]);
         return arr;
     }
