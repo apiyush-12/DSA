d57624bc0a0c20b6eb6ec09f70be362273dee022
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-14 22:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (95.18%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,14 +1,34 @@
-class Solution {
-    public List<Integer> sequentialDigits(int low, int high) {
+// class Solution {
+//     public List<Integer> sequentialDigits(int low, int high) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int i=1; i<9; i++){
+//             int k = i+1;
+//             int n = i*10 + k;
+//             if(n>=low && n<=high) list.add(n);
+//             k++;
+//             while(n<=high && k<10){
+//                 n = n*10 + k;
+//                 if(n>=low && n<=high) list.add(n);
+//                 k++;
+//             }
+//         }
+//         Collections.sort(list);
+//         return list;
+//     }
+// }
+
+
+class Solution{
+    public List<Integer> sequentialDigits(int low, int high){
         List<Integer> list = new ArrayList<>();
         for(int i=1; i<9; i++){
             int k = i+1;
-            int n = i*10 + k;
-            if(n>=low && n<=high) list.add(n);
+            int n = i * 10 + k;
+            if(n>=low && n <= high) list.add(n);
             k++;
-            while(n<=high && k<10){
+            while(n<=high && k < 10){
                 n = n*10 + k;
-                if(n>=low && n<=high) list.add(n);
+                if(n>= low && n <= high) list.add(n);
                 k++;
             }
         }
