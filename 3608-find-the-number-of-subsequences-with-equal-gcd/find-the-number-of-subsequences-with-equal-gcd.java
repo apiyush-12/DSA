f46f5be9f9dbe1b6eb684182b828f454617ad299
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-15 22:47:05 +0530

    Time: 165 ms (72.50%) | Memory: 47.5 MB (62.50%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -30,25 +30,60 @@
 // }
 
 
-class Solution {
-    static final int MOD = 1000000007;
-    public int subsequencePairCount(int[] nums) {
-        int m = 0;
-        for (int num : nums) {
-            m = Math.max(m, num);
-        }
-        int[][] dp = new int[m + 1][m + 1];
-        dp[0][0] = 1;
+// class Solution {
+//     static final int MOD = 1000000007;
+//     public int subsequencePairCount(int[] nums) {
+//         int m = 0;
+//         for (int num : nums) {
+//             m = Math.max(m, num);
+//         }
+//         int[][] dp = new int[m + 1][m + 1];
+//         dp[0][0] = 1;
 
-        for (int num : nums) {
-            int[][] ndp = new int[m + 1][m + 1];
-            for (int j = 0; j <= m; j++) {
+//         for (int num : nums) {
+//             int[][] ndp = new int[m + 1][m + 1];
+//             for (int j = 0; j <= m; j++) {
+//                 int divisor1 = gcd(j, num);
+//                 for (int k = 0; k <= m; k++) {
+//                     int val = dp[j][k];
+//                     if (val == 0) {
+//                         continue;
+//                     }
+//                     int divisor2 = gcd(k, num);
+//                     ndp[j][k] = (ndp[j][k] + val) % MOD;
+//                     ndp[divisor1][k] = (ndp[divisor1][k] + val) % MOD;
+//                     ndp[j][divisor2] = (ndp[j][divisor2] + val) % MOD;
+//                 }
+//             }
+//             dp = ndp;
+//         }
+//         int ans = 0;
+//         for (int j = 1; j <= m; j++) {
+//             ans = (ans + dp[j][j]) % MOD;
+//         }
+//         return ans;
+//     }
+//     private int gcd(int a, int b) {
+//         if(a==0) return b;
+//         return gcd(b%a, a);
+//     }
+// }
+
+
+class Solution{
+    static final int MOD = 1000000007;
+    public int subsequencePairCount(int[] nums){
+        int m = 0;
+        for(int num : nums) m = Math.max(m, num);
+        int[][] dp = new int[m+1][m+1];
+        dp[0][0] = 1;
+        for(int num : nums){
+            int[][] ndp = new int[m+1][m+1];
+            for(int j=0; j<=m; j++){
                 int divisor1 = gcd(j, num);
-                for (int k = 0; k <= m; k++) {
+                for(int k=0; k<=m; k++){
                     int val = dp[j][k];
-                    if (val == 0) {
-                        continue;
-                    }
+                    if(val == 0) continue;
                     int divisor2 = gcd(k, num);
                     ndp[j][k] = (ndp[j][k] + val) % MOD;
                     ndp[divisor1][k] = (ndp[divisor1][k] + val) % MOD;
@@ -58,12 +93,12 @@ class Solution {
             dp = ndp;
         }
         int ans = 0;
-        for (int j = 1; j <= m; j++) {
+        for(int j=1; j<=m; j++){
             ans = (ans + dp[j][j]) % MOD;
         }
         return ans;
     }
-    private int gcd(int a, int b) {
+    private int gcd(int a, int b){
         if(a==0) return b;
         return gcd(b%a, a);
     }
