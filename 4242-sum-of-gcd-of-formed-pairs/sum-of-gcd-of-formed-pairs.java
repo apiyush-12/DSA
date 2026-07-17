7222b01c929a6748edd025acf163e537fb76a839
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-17 22:54:59 +0530

    Time: 58 ms (41.50%) | Memory: 108 MB (63.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,57 +1,27 @@
-// class Solution {
-//     public long gcdSum(int[] nums) {
-//         int max = -1, n = nums.length;
-//         int[] prefixGCD = new int[n];
-//         for (int i = 0; i < n; i++) {
-//             max = Math.max(nums[i], max);
-//             prefixGCD[i] = gcd(nums[i], max);
-//         }
-//         long ans = 0;
-//         Arrays.sort(prefixGCD);
-//         for (int i = 0, j = n - 1; i < j; i++, j--) {
-//             ans += gcd(prefixGCD[i], prefixGCD[j]);
-//         }
-//         return ans;
-//     }
-
-//     private int gcd(int a, int b) {
-//         if (b == 0)
-//             return a;
-//         return gcd(b, a % b);
-//     }
-// }
-
-
 class Solution {
     public long gcdSum(int[] nums) {
         int n = nums.length;
-        int mx = 0;
-        int[] prefixGcd = new int[n];
-        for(int i=0;i<n;i++) {
-            int num = nums[i];
-            mx = Math.max(mx, num);
-            prefixGcd[i] = gcd(mx, num);
+        int max = 0;
+        int[] prefixgcd = new int[n];
+        int k=0;
+        for(int num : nums){
+            max = Math.max(num, max);
+            prefixgcd[k] = gcd(num, max);
+            k++;
         }
 
-        Arrays.sort(prefixGcd);
-
+        Arrays.sort(prefixgcd);
         int i=0, j=n-1;
         long sum = 0;
-        while(i < j) {
-            sum += gcd(prefixGcd[i], prefixGcd[j]);
+        while(i<j){
+            sum += gcd(prefixgcd[i], prefixgcd[j]);
             i++;
             j--;
         }
-
         return sum;
     }
-
-    private int gcd(int a, int b) {
-        while(b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
-        return a;
+    private int gcd(int a, int b){
+        if(a==0) return b;
+        return gcd(b%a, a);
     }
-}
+} 
