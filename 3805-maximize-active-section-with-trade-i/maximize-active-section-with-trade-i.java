2dc684483305849e0d1f8363e93a0f86ff5eafa2
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-21 22:27:37 +0530

    Time: 55 ms (88.73%) | Memory: 47.8 MB (83.10%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int n = s.length();
+        int ones = 0, maxSum = 0, prevSum = -1;
+        int i=0;
+        while(i<n){
+            if(s.charAt(i) == '1'){
+                ones++;
+                i++;
+            }else{
+                int curr = 0;
+                while(i<n && s.charAt(i) == '0'){
+                    curr++;
+                    i++;
+                }
+                if(prevSum > 0) maxSum = Math.max(prevSum + curr, maxSum);
+                prevSum = curr;
+            }
+        }
+        return maxSum + ones;
+    }
+}
