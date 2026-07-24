26566c8ac333fcd5ac50f75131eb2bb38bfc30bb
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-24 23:41:40 +0530

    Time: 176 ms (98.18%) | Memory: 47 MB (63.64%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
@@ -1,32 +1,66 @@
+// class Solution {
+//     public int uniqueXorTriplets(int[] nums) {
+//         int maxVal = 0;
+//         for(int num : nums){
+//             maxVal = Math.max(maxVal, num);
+//         }   
+//         int xorLimit = 1;
+//         while(xorLimit <= maxVal) xorLimit <<= 1;
+//         boolean[] oneN = new boolean[xorLimit];
+//         boolean[] twoN = new boolean[xorLimit];
+//         boolean[] threeN = new boolean[xorLimit];
+
+//         for(int num : nums) oneN[num] = true;
+
+//         for(int num : nums){
+//             for(int xorV = 0; xorV < xorLimit; xorV++){
+//                 if(oneN[xorV]) twoN[xorV^num] = true;
+//             }
+//         }
+
+//         for(int num : nums){
+//             for(int xorV=0; xorV < xorLimit; xorV++){
+//                 if(twoN[xorV]) threeN[xorV ^ num] = true;
+//             }
+//         }
+//         int answer = 0;
+//         for(boolean poss : threeN){
+//             if(poss) answer++;
+//         }
+//         return answer;
+//     }
+// }
+
+
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        int maxVal = 0;
-        for(int num : nums){
-            maxVal = Math.max(maxVal, num);
-        }   
-        int xorLimit = 1;
-        while(xorLimit <= maxVal) xorLimit <<= 1;
-        boolean[] oneN = new boolean[xorLimit];
-        boolean[] twoN = new boolean[xorLimit];
-        boolean[] threeN = new boolean[xorLimit];
+        boolean[] pairsPossible = new boolean[2048];
+        boolean[] ans = new boolean[2048];
 
-        for(int num : nums) oneN[num] = true;
-
-        for(int num : nums){
-            for(int xorV = 0; xorV < xorLimit; xorV++){
-                if(oneN[xorV]) twoN[xorV^num] = true;
+        int n = nums.length;
+        for (int i=0;i<n;i++) {
+            for (int j=i+1;j<n;j++) {
+                pairsPossible[nums[i]^nums[j]] = true;
             }
         }
 
-        for(int num : nums){
-            for(int xorV=0; xorV < xorLimit; xorV++){
-                if(twoN[xorV]) threeN[xorV ^ num] = true;
+        // for triplets
+        for (int e: nums) {
+            ans[e] = true;
+        }
+
+        for (int i=0;i<2048;i++) {
+            if (pairsPossible[i]) {
+                for (int e: nums) {
+                    ans[i^e] = true;
+                }
             }
         }
-        int answer = 0;
-        for(boolean poss : threeN){
-            if(poss) answer++;
+
+        int ret = 0;
+        for (boolean e: ans) {
+            if (e) ret++;
         }
-        return answer;
+        return ret;
     }
 }
