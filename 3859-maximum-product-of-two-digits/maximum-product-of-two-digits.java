3c5343f6a27807bc9dd913e3389817b7a7a9911b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-26 21:01:14 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (39.22%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,14 +1,32 @@
-class Solution {
-    public int maxProduct(int n) {
+// class Solution {
+//     public int maxProduct(int n) {
+//         int f = 0, s = 0;
+//         while (n != 0) {
+//             int x = n % 10;
+//             if (x > f) {
+//                 s = f;
+//                 f = x;
+//             } else if (x > s) s = x;
+//             n /= 10;
+//         }
+//         return f * s;
+//     }
+// }
+
+
+class Solution{
+    public int maxProduct(int n){
         int f = 0, s = 0;
-        while (n != 0) {
+        while(n != 0){
             int x = n % 10;
-            if (x > f) {
+            if(x > f){
                 s = f;
                 f = x;
-            } else if (x > s) s = x;
-            n /= 10;
+            }else if(x > s){
+                s = x;
+            }
+            n = n / 10;
         }
-        return f * s;
+        return s * f;
     }
 }
