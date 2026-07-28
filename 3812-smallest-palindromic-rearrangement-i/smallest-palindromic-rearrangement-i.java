1141f8236180d15b10ba33891e713e4f8e7f86cb
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-28 22:33:02 +0530

    Time: 34 ms (36.95%) | Memory: 48.1 MB (57.43%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] letterCount = new int[26];
+        for(char ch : s.toCharArray()) letterCount[ch-'a']++;
+        StringBuilder leftSide = new StringBuilder();
+        StringBuilder middle = new StringBuilder();
+
+        for(int i=0; i<26; i++){
+            int count = letterCount[i];
+            if(count % 2 != 0){
+                middle.append((char)(i+'a'));
+            }
+            for(int j=0; j<count/2; j++) leftSide.append((char)(i + 'a'));
+        }
+        StringBuilder result = new StringBuilder();
+        result.append(leftSide);
+        result.append(middle);
+        result.append(leftSide.reverse());
+
+        return result.toString();
+    }
+}
