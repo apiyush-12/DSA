21b2325a2778988a042a6e40d01fa095f331c8e1
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-28 22:35:28 +0530

    Time: 22 ms (91.16%) | Memory: 48.3 MB (38.15%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,24 +1,58 @@
+// class Solution {
+//     public String smallestPalindrome(String s) {
+//         int[] letterCount = new int[26];
+//         for (char ch : s.toCharArray())
+//             letterCount[ch - 'a']++;
+//         StringBuilder leftSide = new StringBuilder();
+//         StringBuilder middle = new StringBuilder();
+
+//         for (int i = 0; i < 26; i++) {
+//             int count = letterCount[i];
+//             if (count % 2 != 0) {
+//                 middle.append((char) (i + 'a'));
+//             }
+//             for (int j = 0; j < count / 2; j++)
+//                 leftSide.append((char) (i + 'a'));
+//         }
+//         StringBuilder result = new StringBuilder();
+//         result.append(leftSide);
+//         result.append(middle);
+//         result.append(leftSide.reverse());
+
+//         return result.toString();
+//     }
+// }
+
+
 class Solution {
     public String smallestPalindrome(String s) {
-        int[] letterCount = new int[26];
-        for (char ch : s.toCharArray())
-            letterCount[ch - 'a']++;
-        StringBuilder leftSide = new StringBuilder();
-        StringBuilder middle = new StringBuilder();
+        int n = s.length();
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        char[] ans = new char[n];
+        int left = 0;
+        int right = n - 1;
 
         for (int i = 0; i < 26; i++) {
-            int count = letterCount[i];
-            if (count % 2 != 0) {
-                middle.append((char) (i + 'a'));
+            while (freq[i] >= 2) {
+                char c = (char) ('a' + i);
+                ans[left++] = c;
+                ans[right--] = c;
+                freq[i] -= 2;
             }
-            for (int j = 0; j < count / 2; j++)
-                leftSide.append((char) (i + 'a'));
         }
-        StringBuilder result = new StringBuilder();
-        result.append(leftSide);
-        result.append(middle);
-        result.append(leftSide.reverse());
 
-        return result.toString();
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 1) {
+                ans[left] = (char) ('a' + i);
+                break;
+            }
+        }
+
+        return new String(ans);
     }
 }
