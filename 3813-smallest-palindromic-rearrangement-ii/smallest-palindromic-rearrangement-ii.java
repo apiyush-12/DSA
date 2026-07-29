d069d72011b5465f563c48c03a551c44208f04dd
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-30 00:48:05 +0530

    Time: 32 ms (51.61%) | Memory: 47.2 MB (74.19%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
@@ -1,75 +1,87 @@
 class Solution {
     long[] fact;
     static long maxK = 1000001;
+
     public String smallestPalindrome(String s, int k) {
         int[] freq = new int[26];
-        for(char ch : s.toCharArray()) freq[ch-'a']++;
+        for (char ch : s.toCharArray())
+            freq[ch - 'a']++;
 
         String mid = "";
-        for(int i=0; i<26; i++){
-            if(freq[i]%2 != 0){
-                mid = String.valueOf((char)(i+'a'));
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 != 0) {
+                mid = String.valueOf((char) (i + 'a'));
                 break;
             }
         }
         int[] half = new int[26];
         int len = 0;
-        for(int i=0; i<26; i++){
-            half[i] = freq[i]/2;
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
             len += half[i];
         }
         computeFactorials(len);
         StringBuilder halfStr = new StringBuilder();
-        if(!buildKthPalindrome(half, k, halfStr, len)) return "";
+        if (!buildKthPalindrome(half, k, halfStr, len))
+            return "";
 
         StringBuilder rev = new StringBuilder(halfStr).reverse();
         return halfStr.toString() + mid + rev.toString();
     }
-    private boolean buildKthPalindrome(int[] freq, long k, StringBuilder sb, int len){
-        if(len == 0) return true;
-        for(int i=0; i<26; i++){
-            if(freq[i] == 0) continue;
+
+    private boolean buildKthPalindrome(int[] freq, long k, StringBuilder sb, int len) {
+        if (len == 0)
+            return true;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
+                continue;
             freq[i]--;
             long perms = multinomial(freq);
-            if(k <= perms){
-                sb.append((char)(i+'a'));
-                return buildKthPalindrome(freq, k, sb, len-1);
-            }else{
+            if (k <= perms) {
+                sb.append((char) (i + 'a'));
+                return buildKthPalindrome(freq, k, sb, len - 1);
+            } else {
                 k -= perms;
                 freq[i]++;
             }
         }
         return false;
     }
-    private long multinomial(int[] counts){
+
+    private long multinomial(int[] counts) {
         int tot = 0;
-        for(int x : counts) tot += x;
+        for (int x : counts)
+            tot += x;
         long res = 1;
-        for(int i=0; i<26; i++){
+        for (int i = 0; i < 26; i++) {
             int cnt = counts[i];
-            res = res*binom(tot, cnt);
-            if(res >= maxK) return maxK;
+            res = res * binom(tot, cnt);
+            if (res >= maxK)
+                return maxK;
             tot -= cnt;
         }
         return res;
     }
 
-    private long binom(int n, int k){
-        if(k>n) return 0;
-        if(k > n-k) k =n-k;
-        long res= 1;
-        for(int i=1; i<=k; i++){
-            res = res * (n-i+1)/i;
-            if(res >= maxK) return maxK;
+    private long binom(int n, int k) {
+        if (k > n)
+            return 0;
+        if (k > n - k)
+            k = n - k;
+        long res = 1;
+        for (int i = 1; i <= k; i++) {
+            res = res * (n - i + 1) / i;
+            if (res >= maxK)
+                return maxK;
         }
         return res;
     }
 
-    private void computeFactorials(int n){
-        fact = new long[n+1];
+    private void computeFactorials(int n) {
+        fact = new long[n + 1];
         fact[0] = 1;
-        for(int i=1; i<=n; i++){
-            fact[i] = fact[i-1]*i;
+        for (int i = 1; i <= n; i++) {
+            fact[i] = fact[i - 1] * i;
         }
     }
 
