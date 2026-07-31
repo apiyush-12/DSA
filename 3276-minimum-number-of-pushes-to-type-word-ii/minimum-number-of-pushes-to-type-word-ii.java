f0ac1665760b6c246e427cda7d5c9b9f384c1f8b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-31 23:39:42 +0530

    Time: 9 ms (94.22%) | Memory: 48.3 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int n = word.length();
+//         Map<Character, Integer> map = new HashMap<>();
+//         for(char c : word.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
+//         List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+//         list.sort((entry1, entry2)->entry2.getValue().compareTo(entry1.getValue()));
+//         int result = 0;
+//         int count = 0;
+//         int freq = 1;
+//         for(int i=0; i<list.size(); i++){
+//             result = result + freq*list.get(i).getValue();
+//             count++;
+//             if(count % 8 == 0) freq++;
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int minimumPushes(String word) {
-        int n = word.length();
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : word.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((entry1, entry2)->entry2.getValue().compareTo(entry1.getValue()));
-        int result = 0;
-        int count = 0;
-        int freq = 1;
-        for(int i=0; i<list.size(); i++){
-            result = result + freq*list.get(i).getValue();
-            count++;
-            if(count % 8 == 0) freq++;
+        int ans=0;
+        int[] fre=new int[26];
+        int n=word.length();
+        for(int i=0;i<n;i++){
+            char ch=word.charAt(i);
+            fre[ch-'a']++;
         }
-        return result;
+        Arrays.sort(fre);
+        for(int i=25;i>=0;i--){
+            ans+=fre[i]*((25-i)/8+1);
+        }
+        return ans;
     }
 }
