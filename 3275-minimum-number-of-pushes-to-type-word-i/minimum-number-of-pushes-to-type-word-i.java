2df16592f945290e304b0a16ae03f4b1fc805e49
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-07-31 23:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.02%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -24,11 +24,26 @@
 // }
 
 
+// class Solution{
+//     public int minimumPushes(String word){
+//         int n = word.length();
+//         int sum = 0;
+//         for(int i=0; i<n; i++) sum += i / 8 + 1;
+//         return sum;
+//     }
+// }
+
+
 class Solution{
     public int minimumPushes(String word){
         int n = word.length();
         int sum = 0;
-        for(int i=0; i<n; i++) sum += i / 8 + 1;
+        for(int i=0; i<n; i++){
+            if(i < 8) sum += 1;
+            else if(i<16) sum += 2;
+            else if(i<24) sum += 3;
+            else sum += 4;
+        }
         return sum;
     }
 }
