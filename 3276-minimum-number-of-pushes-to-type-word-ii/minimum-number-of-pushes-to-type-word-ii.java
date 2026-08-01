14cb20ebff3030f44450cedc0c8d51ca76475def
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-01 23:09:18 +0530

    Time: 10 ms (63.47%) | Memory: 48.3 MB (18.47%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -34,11 +34,11 @@
 
 class Solution {
     public int minimumPushes(String word) {
+        int ans = 0;
         int[] freq = new int[26];
         for (int ch : word.toCharArray())
             freq[ch - 'a']++;
         Arrays.sort(freq);
-        int ans = 0;
         for (int i = 25; i >= 0; i--)
             ans += freq[i] * ((25 - i) / 8 + 1);
         return ans;
