cdf86a9a09bc2eb35d8ef7b10bebe0edd2ef9a67
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-02 00:04:04 +0530

    Time: 73 ms (14.54%) | Memory: 42.7 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,15 +1,38 @@
-class Solution {
-    Integer[][] dp;
-    public boolean predictTheWinner(int[] nums) {
+// class Solution {
+//     Integer[][] dp;
+//     public boolean predictTheWinner(int[] nums) {
+//         int n = nums.length;
+//         dp = new Integer[n][n];
+//         return solve(nums, 0, n - 1) >= 0;
+//     }
+//     private int solve(int[] nums, int i, int j) {
+//         if (i == j)
+//             return nums[i];
+//         if (dp[i][j] != null)
+//             return dp[i][j];
+//         int left = nums[i] - solve(nums, i + 1, j);
+//         int right = nums[j] - solve(nums, i, j - 1);
+//         return Math.max(left, right);
+//     }
+// }
+
+class Solution{
+    Integer dp[][];
+    public boolean predictTheWinner(int[] nums){
         int n = nums.length;
         dp = new Integer[n][n];
-        return solve(nums, 0, n-1) >= 0;        
+        int total = 0;
+        for(int x : nums) total += x;
+        int p1 = solve(nums, 0, n-1);
+        int p2 = total - p1;
+        return p1 >= p2;
     }
     private int solve(int[] nums, int i, int j){
+        if(i>j) return 0;
         if(i==j) return nums[i];
         if(dp[i][j] != null) return dp[i][j];
-        int left = nums[i] - solve(nums, i+1, j);
-        int right = nums[j] - solve(nums, i, j-1);
+        int left = nums[i] + Math.min(solve(nums, i+1, j-1), solve(nums, i+2, j));
+        int right = nums[j] + Math.min(solve(nums, i+1, j-1), solve(nums, i, j-2));
         return Math.max(left, right);
     }
 }
