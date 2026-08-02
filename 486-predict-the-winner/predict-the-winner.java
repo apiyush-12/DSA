f91d6c2893da57f4d249e968a179e7795604f341
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-02 16:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.02%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -38,20 +38,35 @@
 // }
 
 
-class Solution {
+// class Solution {
 
-    public boolean predictTheWinner(int[] nums) {
+//     public boolean predictTheWinner(int[] nums) {
         
+//         int n = nums.length;
+//         int[] dp = nums.clone(); 
+        
+//         for (int diff = 1; diff < n; ++diff) {
+//             for (int j = n - 1; j - diff >= 0; --j) {
+//                 int i = j - diff;
+//                 dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+//             }
+//         }
+        
+//         return dp[n - 1] >= 0;
+//     }
+// }
+
+
+class Solution{
+    public boolean predictTheWinner(int[] nums){
         int n = nums.length;
-        int[] dp = nums.clone(); 
-        
-        for (int diff = 1; diff < n; ++diff) {
-            for (int j = n - 1; j - diff >= 0; --j) {
-                int i = j - diff;
-                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+        int[] dp = nums.clone();
+        for(int i=1; i<n; i++){
+            for(int j=n-1; j-i >= 0; --j){
+                int k = j-i;
+                dp[j] = Math.max(nums[k]-dp[j], nums[j]-dp[j-1]);
             }
         }
-        
-        return dp[n - 1] >= 0;
+        return dp[n-1] >=0;
     }
 }
