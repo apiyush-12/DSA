87e1c95a5c19317faccfe04b6cc5700adb381077
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-03 22:00:53 +0530

    Time: 5 ms (24.96%) | Memory: 43.1 MB (62.60%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -20,8 +20,23 @@
 // }
 
 
+// class Solution{
+//     public boolean stoneGame(int[] piles){
+//         return true;
+//     }
+// }
+
+
 class Solution{
     public boolean stoneGame(int[] piles){
-        return true;
+        int n = piles.length;
+        int[] dp = piles.clone();
+        for(int i=1; i<n; i++){
+            for(int j=n-1; j-i >= 0; --j){
+                int k = j-i;
+                dp[j] = Math.max(piles[k]-dp[j], piles[j]-dp[j-1]);
+            }
+        }
+        return dp[n-1] >=0;
     }
 }
