e60f019e3fa8903f3602b9cf33be27fa29439667
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-05 17:49:43 +0530

    Time: 128 ms (19.23%) | Memory: 291 MB (38.46%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer> result = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+
+        for(int[] edge : invocations){
+            int a = edge[0];
+            int b = edge[1];
+            adj.get(a).add(b);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        dfs(adj, set, k);
+        boolean remove = true;
+        for(int[] edge : invocations){
+            if(!set.contains(edge[0]) && set.contains(edge[1])){
+                remove = false;
+                break;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(!set.contains(i) || !remove) result.add(i);
+        }
+        return result;
+    }
+    void dfs(List<List<Integer>> adj, Set<Integer> set, int i){
+        if(set.contains(i)) return;
+        set.add(i);
+        for(int node : adj.get(i)) dfs(adj, set, node);
+    }
+}
