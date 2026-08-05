c5bee535ab656a5e9cc7439a1d3a3bdf58451240
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-05 17:50:15 +0530

    Time: 125 ms (19.23%) | Memory: 290.9 MB (38.46%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -2,9 +2,10 @@ class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
         List<Integer> result = new ArrayList<>();
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
 
-        for(int[] edge : invocations){
+        for (int[] edge : invocations) {
             int a = edge[0];
             int b = edge[1];
             adj.get(a).add(b);
@@ -13,20 +14,24 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         dfs(adj, set, k);
         boolean remove = true;
-        for(int[] edge : invocations){
-            if(!set.contains(edge[0]) && set.contains(edge[1])){
+        for (int[] edge : invocations) {
+            if (!set.contains(edge[0]) && set.contains(edge[1])) {
                 remove = false;
                 break;
             }
         }
-        for(int i=0; i<n; i++){
-            if(!set.contains(i) || !remove) result.add(i);
+        for (int i = 0; i < n; i++) {
+            if (!set.contains(i) || !remove)
+                result.add(i);
         }
         return result;
     }
-    void dfs(List<List<Integer>> adj, Set<Integer> set, int i){
-        if(set.contains(i)) return;
+
+    void dfs(List<List<Integer>> adj, Set<Integer> set, int i) {
+        if (set.contains(i))
+            return;
         set.add(i);
-        for(int node : adj.get(i)) dfs(adj, set, node);
+        for (int node : adj.get(i))
+            dfs(adj, set, node);
     }
 }
