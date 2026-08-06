689794f5b49607fbb2785e9596cece9722ac21a8
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-06 14:53:09 +0530

    Time: 3 ms (82.74%) | Memory: 46.7 MB (73.65%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,17 +1,36 @@
-class Solution {
-    public List<Integer> findMissingElements(int[] nums) {
-        int max = Integer.MIN_VALUE;
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+//         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+//         Set<Integer> seen = new HashSet<>();
+//         for (int num : nums) {
+//             seen.add(num);
+//             min = Math.min(min, num);
+//             max = Math.max(max, num);
+//         }
+//         List<Integer> missing = new ArrayList<>();
+//         for (int i = min; i <= max; i++) {
+//             if (!seen.contains(i))
+//                 missing.add(i);
+//         
+//         return missing;
+//     }
+// }
+
+class Solution{
+    public List<Integer> findMissingElements(int[] nums){
         int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         Set<Integer> seen = new HashSet<>();
+
         for(int num : nums){
             seen.add(num);
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+            min = Math.min(num, min);
+            max = Math.max(num, max);
         }
-        List<Integer> missing =new ArrayList<>();
-        for(int i = min; i<=max; i++){
-            if(!seen.contains(i)) missing.add(i);
+        List<Integer> ans = new ArrayList<>();
+        for(int i=min; i<=max; i++){
+            if(!seen.contains(i)) ans.add(i);
         }
-        return missing;
+        return ans;
     }
 }
