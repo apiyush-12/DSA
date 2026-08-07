4c771b13a0de909fda48768d0b16b1e7b6a5657a
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-07 20:42:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (31.03%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -1,13 +1,31 @@
-class Solution {
+// class Solution {
+//     private int pro(int n) {
+//         int p = 1;
+//         while (n != 0) {
+//             p = p * (n % 10);
+//             n = n / 10;
+//         }
+//         return p;
+//     }
+
+//     public int smallestNumber(int n, int t) {
+//         while (pro(n) % t != 0)
+//             n++;
+//         return n;
+//     }
+// }
+
+
+class Solution{
     private int pro(int n){
         int p = 1;
         while(n != 0){
-            p = p * (n % 10);
-            n = n / 10;
+            p = p*(n%10);
+            n = n/10;
         }
         return p;
     }
-    public int smallestNumber(int n, int t) {
+    public int smallestNumber(int n, int t){
         while(pro(n) % t != 0) n++;
         return n;
     }
