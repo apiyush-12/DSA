a6e9b96aa3c0abae0cd96eedc1c815238faa55f3
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-11 18:56:41 +0530

    Time: 14 ms (91.08%) | Memory: 42.4 MB (83.73%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,5 +1,21 @@
-class Solution {
-    public boolean winnerSquareGame(int n) {
+// class Solution {
+//     public boolean winnerSquareGame(int n) {
+//         boolean[] dp = new boolean[n+1];
+//         for(int i=0; i<=n; i++){
+//             for(int j=1; j*j <= i; j++){
+//                 if(!dp[i-j*j]){
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[n];
+//     }
+// }
+
+
+class Solution{
+    public boolean winnerSquareGame(int n){
         boolean[] dp = new boolean[n+1];
         for(int i=0; i<=n; i++){
             for(int j=1; j*j <= i; j++){
