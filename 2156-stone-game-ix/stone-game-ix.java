81080e8e1403a06dd7b4f265903b74f71c10a11f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-16 21:37:33 +0530

    Time: 4 ms (75.68%) | Memory: 114.6 MB (82.43%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -1,11 +1,21 @@
-class Solution {
-    public boolean stoneGameIX(int[] stones) {
+// class Solution {
+//     public boolean stoneGameIX(int[] stones) {
+//         int[] cnt = new int[3];
+//         for(int x : stones) cnt[x % 3]++;
+//         int a = cnt[1];
+//         int b = cnt[2];
+//         int c = cnt[0];
+//         if(c % 2 == 0) return a > 0 && b > 0;
+//         return Math.abs(a-b) > 2;
+//     }
+// }
+
+
+class Solution{
+    public boolean stoneGameIX(int[] stones){
         int[] cnt = new int[3];
         for(int x : stones) cnt[x % 3]++;
-        int a = cnt[1];
-        int b = cnt[2];
-        int c = cnt[0];
-        if(c % 2 == 0) return a > 0 && b > 0;
-        return Math.abs(a-b) > 2;
+        if(cnt[0] % 2 == 0) return cnt[1] > 0 && cnt[2] > 0;
+        return Math.abs(cnt[1]-cnt[2]) > 2;
     }
 }
