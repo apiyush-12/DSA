59e00453bd93224e85abb3546c441dc17486ec65
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-18 20:26:34 +0530

    Time: 1 ms (99.30%) | Memory: 44.7 MB (74.74%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,24 +1,56 @@
+// class Solution {
+//     public int largestInteger(int[] nums, int k) {
+//         int[] count = new int[51];
+//         if(nums.length == k){
+//             int res = nums[0];
+//             for(int x : nums){
+//                 res = Math.max(res, x);
+//             }
+//             return res;
+//         }
+//         for(int i=0; i<=nums.length-k; i++){
+//             boolean[] seen = new boolean[51];
+//             for(int j=i; j<i+k; j++) seen[nums[j]] = true;
+//             for(int num=0; num<=50; num++){
+//                 if(seen[num]) count[num]++;
+//             }
+//         }
+
+//         for(int i=50; i>=0; i--){
+//             if(count[i] == 1) return i;
+//         }
+//         return -1;
+//     }
+// }
+
+
 class Solution {
+
     public int largestInteger(int[] nums, int k) {
-        int[] count = new int[51];
-        if(nums.length == k){
+        int n = nums.length;
+        if (n == k) {
             int res = nums[0];
-            for(int x : nums){
-                res = Math.max(res, x);
-            }
+            for (int x : nums) res = Math.max(res, x);
             return res;
         }
-        for(int i=0; i<=nums.length-k; i++){
-            boolean[] seen = new boolean[51];
-            for(int j=i; j<i+k; j++) seen[nums[j]] = true;
-            for(int num=0; num<=50; num++){
-                if(seen[num]) count[num]++;
+        int[] count = new int[51];
+        for (int x : nums) count[x]++;
+        if (k == 1) {
+            for (int i = 50; i >= 0; --i) {
+                if (count[i] == 1) {
+                    return i;
+                }
             }
+            return -1;
         }
-
-        for(int i=50; i>=0; i--){
-            if(count[i] == 1) return i;
+        
+        int res = -1;
+        if (count[nums[0]] == 1) {
+            res = Math.max(res, nums[0]);
         }
-        return -1;
+        if (count[nums[n - 1]] == 1) {
+            res = Math.max(res, nums[n - 1]);
+        }
+        return res;
     }
 }
