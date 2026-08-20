8d2c56ca65ceacaeefc01d1d62e721d4243e1f81
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-20 22:08:17 +0530

    Time: 1 ms (98.08%) | Memory: 46.3 MB (95.43%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,18 +1,43 @@
+// class Solution {
+//     public int[] resultArray(int[] nums) {
+//         int n = nums.length;
+//         List<Integer> list1 = new ArrayList<>();
+//         List<Integer> list2 = new ArrayList<>();
+//         list1.add(nums[0]);
+//         list2.add(nums[1]);
+//         for(int i=2; i<n; i++){
+//             if(list1.get(list1.size()-1) > list2.get(list2.size()-1)) list1.add(nums[i]);
+//             else list2.add(nums[i]);
+//         }
+//         int[] res = new int[n];
+//         int index = 0;
+//         for(int num : list1) res[index++] = num;
+//         for(int num : list2) res[index++] = num;
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public int[] resultArray(int[] nums) {
-        int n = nums.length;
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        list1.add(nums[0]);
-        list2.add(nums[1]);
-        for(int i=2; i<n; i++){
-            if(list1.get(list1.size()-1) > list2.get(list2.size()-1)) list1.add(nums[i]);
-            else list2.add(nums[i]);
+        ArrayList<Integer> a=new ArrayList<>();
+        ArrayList<Integer> b=new ArrayList<>();
+        a.add(nums[0]);
+        b.add(nums[1]);
+        for(int i=2;i<nums.length;i++){
+            if(a.get(a.size()-1)>b.get(b.size()-1)) a.add(nums[i]);
+            else b.add(nums[i]);
         }
-        int[] res = new int[n];
-        int index = 0;
-        for(int num : list1) res[index++] = num;
-        for(int num : list2) res[index++] = num;
-        return res;
+        int i=0;
+        for(int k=0;k<a.size();k++) {
+            nums[i]=a.get(k);
+            i++;
+        }
+        for(int k=0;k<b.size();k++){
+            nums[i]=b.get(k);
+            i++;
+        }
+        return nums;
+
     }
 }
