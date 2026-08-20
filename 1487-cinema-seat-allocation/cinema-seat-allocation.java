6409f61775234499948f6e138cded6f69b8d6c4c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-20 21:46:14 +0530

    Time: 15 ms (98.69%) | Memory: 53.7 MB (29.84%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -22,50 +22,91 @@
 // }
 
 
-class Solution {
-    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+// class Solution {
+//     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+//         Map<Integer, Integer> reserved = new HashMap<>();
+//         for (int[] seat : reservedSeats) {
+//             int row = seat[0];
+//             int col = seat[1];
+//             if (col >= 2 && col <= 9) {
+//                 reserved.put(row, reserved.getOrDefault(row, 0) | (1 << col));
+//             }
+//         }
+//         int answer = (n - reserved.size()) * 2;
+//         for (int mask : reserved.values()) {
+//             boolean left = true;   // seats 2-5
+//             boolean middle = true; // seats 4-7
+//             boolean right = true;  // seats 6-9
+//             // Check seats 2,3,4,5
+//             for (int seat = 2; seat <= 5; seat++) {
+//                 if ((mask & (1 << seat)) != 0) {
+//                     left = false;
+//                     break;
+//                 }
+//             }
+//             // Check seats 4,5,6,7
+//             for (int seat = 4; seat <= 7; seat++) {
+//                 if ((mask & (1 << seat)) != 0) {
+//                     middle = false;
+//                     break;
+//                 }
+//             }
+//             // Check seats 6,7,8,9
+//             for (int seat = 6; seat <= 9; seat++) {
+//                 if ((mask & (1 << seat)) != 0) {
+//                     right = false;
+//                     break;
+//                 }
+//             }
+
+//             if (left && right) {
+//                 answer += 2;
+//             } else if (left || middle || right) {
+//                 answer += 1;
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
+
+
+class Solution{
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats){
         Map<Integer, Integer> reserved = new HashMap<>();
-        for (int[] seat : reservedSeats) {
+        for(int[] seat : reservedSeats){
             int row = seat[0];
             int col = seat[1];
-            if (col >= 2 && col <= 9) {
+            if(col >=2 && col <=9){
                 reserved.put(row, reserved.getOrDefault(row, 0) | (1 << col));
             }
         }
         int answer = (n - reserved.size()) * 2;
-        for (int mask : reserved.values()) {
-            boolean left = true;   // seats 2-5
-            boolean middle = true; // seats 4-7
-            boolean right = true;  // seats 6-9
-            // Check seats 2,3,4,5
-            for (int seat = 2; seat <= 5; seat++) {
-                if ((mask & (1 << seat)) != 0) {
+        for(int mask : reserved.values()){
+            boolean left = true;
+            boolean right = true;
+            boolean middle = true;
+            for(int seat = 2; seat <= 5; seat++){
+                if((mask & (1 << seat)) != 0){
                     left = false;
                     break;
-                }
+                } 
             }
-            // Check seats 4,5,6,7
-            for (int seat = 4; seat <= 7; seat++) {
-                if ((mask & (1 << seat)) != 0) {
+            for(int seat = 4; seat <= 7; seat++){
+                if((mask & (1 << seat)) != 0){
                     middle = false;
                     break;
                 }
             }
-            // Check seats 6,7,8,9
-            for (int seat = 6; seat <= 9; seat++) {
-                if ((mask & (1 << seat)) != 0) {
+            for(int seat = 6; seat <= 9; seat++){
+                if((mask & (1<<seat)) != 0){
                     right = false;
                     break;
                 }
             }
-
-            if (left && right) {
-                answer += 2;
-            } else if (left || middle || right) {
-                answer += 1;
-            }
+            if(left && right) answer += 2;
+            else if(left || middle || right) answer += 1;
         }
-
         return answer;
     }
 }
