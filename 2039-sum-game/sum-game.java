b821836049e5786c58a9c9ef18ecf1dc4f561142
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-23 18:50:53 +0530

    Time: 8 ms (53.28%) | Memory: 47.2 MB (10.95%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int diff = 0;   //Sl - Sr
+        int qDiff = 0;   //qR-qL
+        for(int i=0; i<n/2; i++){
+            if(num.charAt(i) == '?') qDiff++;
+            else diff += num.charAt(i)-'0';
+        }
+        for(int i=n/2; i<n; i++){
+            if(num.charAt(i) == '?') qDiff--;
+            else diff -= num.charAt(i)-'0';
+        }
+
+        if(qDiff == 0) return diff !=0;
+
+        return diff * 2 != -9 * qDiff;
+    }
+}
