6f64e33e6837d45af3c5afb4da658beadb0d011b
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-23 18:54:07 +0530

    Time: 6 ms (97.08%) | Memory: 46.8 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,19 +1,56 @@
+// class Solution {
+//     public boolean sumGame(String num) {
+//         int n = num.length();
+//         int diff = 0;   //Sl - Sr
+//         int qDiff = 0;   //qR-qL
+//         for(int i=0; i<n/2; i++){
+//             if(num.charAt(i) == '?') qDiff++;
+//             else diff += num.charAt(i)-'0';
+//         }
+//         for(int i=n/2; i<n; i++){
+//             if(num.charAt(i) == '?') qDiff--;
+//             else diff -= num.charAt(i)-'0';
+//         }
+
+//         if(qDiff == 0) return diff !=0;
+
+//         return diff * 2 != -9 * qDiff;
+//     }
+// }
+
+
 class Solution {
     public boolean sumGame(String num) {
         int n = num.length();
-        int diff = 0;   //Sl - Sr
-        int qDiff = 0;   //qR-qL
-        for(int i=0; i<n/2; i++){
-            if(num.charAt(i) == '?') qDiff++;
-            else diff += num.charAt(i)-'0';
-        }
-        for(int i=n/2; i<n; i++){
-            if(num.charAt(i) == '?') qDiff--;
-            else diff -= num.charAt(i)-'0';
+        int half = n / 2;
+        int qDiff = 0;
+        int sumDiff = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                qDiff++;
+            } else {
+                sumDiff += c - '0';
+            }
         }
 
-        if(qDiff == 0) return diff !=0;
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
 
-        return diff * 2 != -9 * qDiff;
+            if (c == '?') {
+                qDiff--;
+            } else {
+                sumDiff -= c - '0';
+            }
+        }
+        // Odd difference in number of '?' -> Alice wins
+        if (qDiff % 2 != 0) {
+            return true;
+        }
+
+        // Bob can force equality only in this case
+        return sumDiff + 9 * (qDiff / 2) != 0;
     }
 }
