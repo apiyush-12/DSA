24127d0c02a4bdb5b9df30804bea4cd78dac605a
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-24 20:55:42 +0530

    Time: 7 ms (79.86%) | Memory: 47.2 MB (26.62%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -19,26 +19,45 @@
 // }
 
 
-class Solution {
-    public boolean sumGame(String num) {
+// class Solution {
+//     public boolean sumGame(String num) {
+//         int n = num.length();
+//         int half = n / 2;
+//         int qDiff = 0;
+//         int sumDiff = 0;
+
+//         for (int i = 0; i < half; i++) {
+//             char c = num.charAt(i);
+//             if (c == '?') qDiff++;
+//             else sumDiff += c - '0';
+//         }
+
+//         for (int i = half; i < n; i++) {
+//             char c = num.charAt(i);
+//             if (c == '?') qDiff--;
+//             else sumDiff -= c - '0';
+//         }
+//         if (qDiff % 2 != 0) return true;
+//         return sumDiff + 9 * (qDiff / 2) != 0;
+//     }
+// }
+
+class Solution{
+    public boolean sumGame(String num){
         int n = num.length();
-        int half = n / 2;
         int qDiff = 0;
-        int sumDiff = 0;
-
-        for (int i = 0; i < half; i++) {
+        int sumDiff= 0;
+        for(int i=0; i<n/2; i++){
             char c = num.charAt(i);
-            if (c == '?') qDiff++;
-            else sumDiff += c - '0';
+            if(c == '?') qDiff++;
+            else sumDiff += c-'0';
         }
-
-        for (int i = half; i < n; i++) {
+        for(int i=n/2; i<n; i++){
             char c = num.charAt(i);
-            if (c == '?') qDiff--;else {
-                sumDiff -= c - '0';
-            }
+            if(c == '?') qDiff--;
+            else sumDiff -= c-'0';
         }
-        if (qDiff % 2 != 0) return true;
-        return sumDiff + 9 * (qDiff / 2) != 0;
+        if(qDiff % 2 != 0) return true;
+        return sumDiff * 2 != -9*qDiff;
     }
 }
