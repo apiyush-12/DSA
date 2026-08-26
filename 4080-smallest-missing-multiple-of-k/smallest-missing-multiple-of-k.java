d6fad11a9f7c79c3f738d4dcc0f6f3cc896e50f8
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-26 19:43:51 +0530

    Time: 2 ms (68.16%) | Memory: 44.9 MB (83.22%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -10,18 +10,33 @@
 // }
 
 
-class Solution {
-    public int missingMultiple(int[] nums, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]%k==0){
-                set.add(nums[i]);
-            }
+// class Solution {
+//     public int missingMultiple(int[] nums, int k) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i =0;i<nums.length;i++){
+//             if(nums[i]%k==0){
+//                 set.add(nums[i]);
+//             }
+//         }
+//         for(int j=1;j<=set.size()+1;j++){
+//             if(!set.contains(k*j)){
+//                 return k*j;
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+
+class Solution{
+    public int missingMultiple(int[] nums, int k){
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            if(nums[i]%k == 0) set.add(nums[i]);
         }
-        for(int j=1;j<=set.size()+1;j++){
-            if(!set.contains(k*j)){
-                return k*j;
-            }
+        for(int i=1; i<=set.size()+1; i++){
+            if(!set.contains(i*k)) return k*i;
         }
         return 0;
     }
