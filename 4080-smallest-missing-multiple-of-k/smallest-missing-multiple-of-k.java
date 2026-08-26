5ae49377217498e65f8e4e7d1f97d0ac6a0c0b02
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-26 19:45:15 +0530

    Time: 1 ms (81.90%) | Memory: 44.9 MB (83.22%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -9,7 +9,6 @@
 //     }
 // }
 
-
 // class Solution {
 //     public int missingMultiple(int[] nums, int k) {
 //         HashSet<Integer> set = new HashSet<>();
@@ -27,16 +26,17 @@
 //     }
 // }
 
-
-class Solution{
-    public int missingMultiple(int[] nums, int k){
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
         int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
-            if(nums[i]%k == 0) set.add(nums[i]);
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % k == 0)
+                set.add(nums[i]);
         }
-        for(int i=1; i<=set.size()+1; i++){
-            if(!set.contains(i*k)) return k*i;
+        for (int i = 1; i <= set.size() + 1; i++) {
+            if (!set.contains(i * k))
+                return k * i;
         }
         return 0;
     }
