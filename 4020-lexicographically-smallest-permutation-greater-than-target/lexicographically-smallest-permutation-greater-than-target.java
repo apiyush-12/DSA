2bbaa0168140b07fda265f2f9673747ef7a74f73
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-28 23:52:05 +0530

    Time: 2 ms (92.54%) | Memory: 44.7 MB (77.61%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -49,7 +49,6 @@
 //     }
 // }
 
-
 // class Solution {
 //     public String lexGreaterPermutation(String s, String target) {
 //         int n = s.length();
@@ -91,36 +90,39 @@
 //     }
 // }
 
-
-class Solution{
-    public String lexGreaterPermutation(String s, String target){
+// piyush
+class Solution {
+    public String lexGreaterPermutation(String s, String target) {
         int n = s.length();
         int[] count = new int[26];
-        for(char ch : s.toCharArray()) count[ch-'a']++;
+        for (char ch : s.toCharArray())
+            count[ch - 'a']++;
         int matched = 0;
-        while(matched < n){
-            int c = target.charAt(matched)-'a';
-            if(count[c] == 0) break;
+        while (matched < n) {
+            int c = target.charAt(matched) - 'a';
+            if (count[c] == 0)
+                break;
             count[c]--;
             matched++;
         }
-        for(int pos = Math.min(matched, n-1); pos >= 0; pos--){
-                if(pos < matched) count[target.charAt(pos)-'a']++;
-            int targetChar = target.charAt(pos)-'a';
-            for(int c = targetChar+1; c<26; c++){
-                if(count[c] > 0){ 
+        for (int pos = Math.min(matched, n - 1); pos >= 0; pos--) {
+            if (pos < matched)
+                count[target.charAt(pos) - 'a']++;
+            int targetChar = target.charAt(pos) - 'a';
+            for (int c = targetChar + 1; c < 26; c++) {
+                if (count[c] > 0) {
                     count[c]--;
                     StringBuilder result = new StringBuilder();
                     result.append(target, 0, pos);
-                    result.append((char)('a'+c));
-                    for(int x=0; x<26; x++){
-                        while(count[x] > 0){
-                            result.append((char)('a'+x));
+                    result.append((char) ('a' + c));
+                    for (int x = 0; x < 26; x++) {
+                        while (count[x] > 0) {
+                            result.append((char) ('a' + x));
                             count[x]--;
                         }
                     }
                     return result.toString();
-                }           
+                }
             }
         }
         return "";
