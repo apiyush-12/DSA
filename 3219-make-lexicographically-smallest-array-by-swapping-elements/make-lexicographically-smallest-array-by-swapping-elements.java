9df11231f16ddfecdd8fcdab1dc722f08cb94c84
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-08-30 22:01:50 +0530

    Time: 68 ms (95.42%) | Memory: 136.2 MB (83.21%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -28,31 +28,61 @@
 // }
 
 
-class Solution {
-    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+// class Solution {
+//     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+//         int[][] values = new int[nums.length][2];
+//         for(int i=0;i<nums.length;i++){
+//             values[i][0] = nums[i];
+//             values[i][1] = i;
+//         }
+//         Arrays.sort(values,(a,b) -> a[0]-b[0]);
+
+//         int start = 0;
+//         while(start<nums.length){
+//             int end = start;
+//             while(end+1<nums.length && values[end+1][0] - values[end][0] <= limit){
+//                 end++;
+//             }
+//             int  groupSize = end-start+1;
+//             int[] indexArr = new int[groupSize];
+//             for(int i=start;i<=end;i++){
+//                 indexArr[i-start] = values[i][1];
+//             }
+//             Arrays.sort(indexArr);
+//             for(int i=0;i<groupSize;i++){
+//                 nums[indexArr[i]] = values[start+i][0];
+//             }
+//             start = end+1;
+//         }
+//         return nums;
+//     }
+// }
+
+
+class Solution{
+    public int[] lexicographicallySmallestArray(int[] nums, int limit){
         int[][] values = new int[nums.length][2];
-        for(int i=0;i<nums.length;i++){
+        for(int i=0; i<nums.length; i++){
             values[i][0] = nums[i];
             values[i][1] = i;
         }
-        Arrays.sort(values,(a,b) -> a[0]-b[0]);
-
+        Arrays.sort(values, (a,b) -> a[0]-b[0]);
         int start = 0;
-        while(start<nums.length){
+        while(start < nums.length){
             int end = start;
-            while(end+1<nums.length && values[end+1][0] - values[end][0] <= limit){
+            while(end+1 < nums.length && values[end+1][0]-values[end][0] <= limit){
                 end++;
             }
-            int  groupSize = end-start+1;
+            int groupSize = end - start + 1;
             int[] indexArr = new int[groupSize];
-            for(int i=start;i<=end;i++){
+            for(int i= start; i<=end; i++){
                 indexArr[i-start] = values[i][1];
             }
             Arrays.sort(indexArr);
-            for(int i=0;i<groupSize;i++){
+            for(int i=0; i<groupSize; i++){
                 nums[indexArr[i]] = values[start+i][0];
             }
-            start = end+1;
+            start = end + 1;
         }
         return nums;
     }
