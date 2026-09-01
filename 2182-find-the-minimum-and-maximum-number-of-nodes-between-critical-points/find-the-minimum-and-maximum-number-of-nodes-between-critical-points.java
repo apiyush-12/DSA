f3a7b311743236958cd8aaa4136b2c473b573076
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-01 19:34:13 +0530

    Time: 4 ms (99.87%) | Memory: 105.9 MB (94.27%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -44,41 +44,81 @@
 
 
 
-class Solution {
-    public int[] nodesBetweenCriticalPoints(ListNode head) {
+// class Solution {
+//     public int[] nodesBetweenCriticalPoints(ListNode head) {
+//         int[] ans = new int[2];
+//         int idx = 1;
+//         int f_idx = -1;
+//         int l_idx = -1;
+//         int min_dist = Integer.MAX_VALUE;
+
+//         ListNode a = head;
+//         ListNode b = a.next;
+//         ListNode c = b.next;
+//         if(c==null){     // 2 nodes only
+//             ans[0] = -1;
+//             ans[1] = -1;
+//             return ans;
+//         }
+//         while(c!=null){
+//             if(b.val<a.val && b.val<c.val || b.val>a.val && b.val>c.val){
+//                 if(f_idx==-1) f_idx = idx;
+//                 if(l_idx!=-1) {
+//                     int dist = idx - l_idx;
+//                     min_dist = Math.min(dist,min_dist);
+//                 }
+//                 l_idx = idx;
+//             }
+//             idx++;
+//             a = a.next;
+//             b = b.next;
+//             c = c.next;      
+//         }
+//         int max_dist = l_idx - f_idx; 
+//         if(max_dist == 0)
+//             max_dist = -1;
+//         if(min_dist == Integer.MAX_VALUE)
+//             min_dist = -1;
+//         ans[0] = min_dist;
+//         ans[1] = max_dist;
+//         return ans;
+//     }
+// }
+
+
+
+class Solution{
+    public int[] nodesBetweenCriticalPoints(ListNode head){
         int[] ans = new int[2];
         int idx = 1;
         int f_idx = -1;
         int l_idx = -1;
         int min_dist = Integer.MAX_VALUE;
-
         ListNode a = head;
         ListNode b = a.next;
         ListNode c = b.next;
-        if(c==null){     // 2 nodes only
+        if(c==null){
             ans[0] = -1;
             ans[1] = -1;
             return ans;
         }
-        while(c!=null){
-            if(b.val<a.val && b.val<c.val || b.val>a.val && b.val>c.val){
-                if(f_idx==-1) f_idx = idx;
-                if(l_idx!=-1) {
+        while(c != null){
+            if(b.val < a.val && b.val < c.val || b.val > a.val && b.val > c.val){
+                if(f_idx == -1) f_idx = idx;
+                if(l_idx != -1){
                     int dist = idx - l_idx;
-                    min_dist = Math.min(dist,min_dist);
+                    min_dist = Math.min(dist, min_dist);
                 }
                 l_idx = idx;
             }
             idx++;
             a = a.next;
             b = b.next;
-            c = c.next;      
+            c = c.next;
         }
-        int max_dist = l_idx - f_idx; 
-        if(max_dist == 0)
-            max_dist = -1;
-        if(min_dist == Integer.MAX_VALUE)
-            min_dist = -1;
+        int max_dist = l_idx -f_idx;
+        if(max_dist == 0) max_dist = -1;
+        if(min_dist == Integer.MAX_VALUE) min_dist = -1;
         ans[0] = min_dist;
         ans[1] = max_dist;
         return ans;
