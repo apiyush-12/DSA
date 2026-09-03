2399dcce46152eeedf0a2da76127613ad990268d
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-03 15:12:58 +0530

    Time: 1 ms (32.15%) | Memory: 45.4 MB (16.17%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,20 @@
-class Solution {
-    public boolean uniformArray(int[] nums1) {
+// class Solution {
+//     public boolean uniformArray(int[] nums1) {
+//         return true;
+//     }
+// }
+
+
+
+class Solution{
+    public boolean uniformArray(int[] nums1){
+        int odd = 0;
+        int even = 0;
+        for(int a : nums1){
+            if(a % 2 == 0) even++ ;
+            else odd++;
+        }
+        if(even == nums1.length || odd == nums1.length) return true;
         return true;
     }
 }
