91e2b1e9d1053449f0320df1b74c2e31a4608730
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-03 23:27:39 +0530

    Time: 1283 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -9,17 +9,25 @@
 -- order by confirmation_rate
 
 
-SELECT 
-    s.user_id,
-    ROUND(
-        IFNULL(
-            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) 
-            / COUNT(c.action),
-            0
-        ),
-        2
-    ) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c
-ON s.user_id = c.user_id
-GROUP BY s.user_id;
+-- SELECT 
+--     s.user_id,
+--     ROUND(
+--         IFNULL(
+--             SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) 
+--             / COUNT(c.action),
+--             0
+--         ),
+--         2
+--     ) AS confirmation_rate
+-- FROM Signups AS s
+-- LEFT JOIN Confirmations AS c
+-- ON s.user_id = c.user_id
+-- GROUP BY s.user_id;
+
+
+
+select s.user_id , Round(avg(if(c.action = 'confirmed', 1 , 0)),2) as confirmation_rate 
+from Signups s
+left join Confirmations c 
+on s.user_id = c.user_id
+group by s.user_id
