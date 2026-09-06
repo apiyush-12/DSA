cdc5955e290443e3bc871c7ef26c52da3311660e
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-06 16:38:43 +0530

    Time: 14 ms (92.72%) | Memory: 42.9 MB (90.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -21,23 +21,43 @@
 // }
 
 
+// class Solution{
+//     public int numDistinct(String s, String t){
+//         int m = s.length();
+//         int n = t.length();
+
+//         long[][] dp = new long[m+1][n+1];
+//         for(int i=0; i<=m; i++) dp[i][n] = 1;
+
+//         for(int i=m-1; i>=0; i--){
+//             for(int j=n-1; j>=0; j--){
+//                 if(s.charAt(i) == t.charAt(j)){
+//                     dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+//                 }else{
+//                     dp[i][j] = dp[i+1][j];
+//                 }
+//             }
+//         }
+//         return (int) dp[0][0];
+//     }
+// }
+
+
 class Solution{
-    public int numDistinct(String s, String t){
-        int m = s.length();
-        int n = t.length();
-
-        long[][] dp = new long[m+1][n+1];
-        for(int i=0; i<=m; i++) dp[i][n] = 1;
-
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
-                if(s.charAt(i) == t.charAt(j)){
-                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+    public int numDistinct(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        int[] curr=new int[n+1];
+        curr[n]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(s.charAt(i)==t.charAt(j)){
+                    curr[j]=curr[j+1]+curr[j];
                 }else{
-                    dp[i][j] = dp[i+1][j];
+                    curr[j]=curr[j];
                 }
             }
         }
-        return (int) dp[0][0];
+        return curr[0];
     }
 }
