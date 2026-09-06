24164b825185709989a734bd7a8132548f33026c
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-06 16:36:21 +0530

    Time: 21 ms (33.78%) | Memory: 66.5 MB (6.38%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,21 +1,43 @@
-class Solution {
-    int[][] memo;
-    public int numDistinct(String s, String t) {
+// class Solution {
+//     int[][] memo;
+//     public int numDistinct(String s, String t) {
+//         int m = s.length();
+//         int n = t.length();
+//         memo = new int[m][n];
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 memo[i][j] = -1;
+//             }
+//         }
+//         return solve(0, 0, s, t);
+//     }
+//     private int solve(int i, int j, String s, String t){
+//         if(j == t.length()) return 1;
+//         if(i == s.length()) return 0;
+//         if(memo[i][j] != -1) return memo[i][j];
+//         if(s.charAt(i) != t.charAt(j)) return memo[i][j] = solve(i+1, j, s, t);
+//         return memo[i][j] = solve(i+1, j+1, s, t) + solve(i+1, j, s, t);
+//     }
+// }
+
+
+class Solution{
+    public int numDistinct(String s, String t){
         int m = s.length();
         int n = t.length();
-        memo = new int[m][n];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                memo[i][j] = -1;
+
+        long[][] dp = new long[m+1][n+1];
+        for(int i=0; i<=m; i++) dp[i][n] = 1;
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(s.charAt(i) == t.charAt(j)){
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+                }else{
+                    dp[i][j] = dp[i+1][j];
+                }
             }
         }
-        return solve(0, 0, s, t);
-    }
-    private int solve(int i, int j, String s, String t){
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(s.charAt(i) != t.charAt(j)) return memo[i][j] = solve(i+1, j, s, t);
-        return memo[i][j] = solve(i+1, j+1, s, t) + solve(i+1, j, s, t);
+        return (int) dp[0][0];
     }
 }
