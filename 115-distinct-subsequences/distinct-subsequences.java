e3caa76b7eb1497b092d7187d7e39c027019a05f
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-06 16:18:14 +0530

    Time: 28 ms (5.20%) | Memory: 54.4 MB (38.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,59 +1,21 @@
-// class Solution {
-//     public int numDistinct(String s, String t) {
-//         int m = s.length();
-//         int n = t.length();
-//         int[][] dp =new int[m+1][n+1];
-//         for(int i = 0; i<=m; i++){
-//             dp[i][0] = 1;
-//         }
-//         for(int i=1; i<=m; i++){
-//             for(int j =1; j<=n; j++){
-//                 if(s.charAt(i-1) == t.charAt(j-1)){
-//                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-//                 }else{
-//                     dp[i][j] = dp[i-1][j];
-//                 }
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
-
-// class Solution {
-//     public int numDistinct(String s, String t) {
-//         int m = s.length();
-//         int n = t.length();
-//         int[][] dp =new int[m+1][n+1];
-//         for(int i = 0; i<=m; i++) dp[i][0] = 1;
-//         for(int i=1; i<=m; i++){
-//             for(int j =1; j<=n; j++){
-//                 if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-//                 else dp[i][j] = dp[i-1][j];
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
-
-
-// piyush
-class Solution{
-    public int numDistinct(String s, String t){
+class Solution {
+    int[][] memo;
+    public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        int[][] dp = new int[m+1][n+1];
-        for(int i = 0; i<=m; i++){
-            dp[i][0] = 1;
-        }
-        for(int i = 1; i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
+        memo = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                memo[i][j] = -1;
             }
         }
-        return dp[m][n];
+        return solve(0, 0, s, t);
+    }
+    private int solve(int i, int j, String s, String t){
+        if(j == t.length()) return 1;
+        if(i == s.length()) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        if(s.charAt(i) != t.charAt(j)) return memo[i][j] = solve(i+1, j, s, t);
+        return memo[i][j] = solve(i+1, j+1, s, t) + solve(i+1, j, s, t);
     }
 }
