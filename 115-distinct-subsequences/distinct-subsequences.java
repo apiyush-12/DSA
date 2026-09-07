117c46bb12da41e092a4ebe9ba4ee8ae32af9a80
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-07 16:17:38 +0530

    Time: 14 ms (92.80%) | Memory: 42.9 MB (92.91%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -43,19 +43,36 @@
 // }
 
 
+// class Solution{
+//     public int numDistinct(String s, String t) {
+//         int m=s.length();
+//         int n=t.length();
+//         int[] curr=new int[n+1];
+//         curr[n]=1;
+//         for(int i=m-1;i>=0;i--){
+//             for(int j=0;j<n;j++){
+//                 if(s.charAt(i)==t.charAt(j)){
+//                     curr[j]=curr[j+1]+curr[j];
+//                 }else{
+//                     curr[j]=curr[j];
+//                 }
+//             }
+//         }
+//         return curr[0];
+//     }
+// }
+
+
 class Solution{
-    public int numDistinct(String s, String t) {
-        int m=s.length();
-        int n=t.length();
-        int[] curr=new int[n+1];
-        curr[n]=1;
-        for(int i=m-1;i>=0;i--){
-            for(int j=0;j<n;j++){
-                if(s.charAt(i)==t.charAt(j)){
-                    curr[j]=curr[j+1]+curr[j];
-                }else{
-                    curr[j]=curr[j];
-                }
+    public int numDistinct(String s, String t){
+        int m = s.length();
+        int n = t.length();
+        int[] curr = new int[n+1];
+        curr[n] = 1;
+        for(int i=m-1; i>=0; i--){
+            for(int j=0; j<n; j++){
+                if(s.charAt(i) == t.charAt(j)) curr[j] = curr[j+1] + curr[j];
+                else curr[j] = curr[j];
             }
         }
         return curr[0];
