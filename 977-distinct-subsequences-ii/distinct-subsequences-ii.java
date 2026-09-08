f69618037debccee305dc559ac81d9865512b10d
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-08 20:52:43 +0530

    Time: 2 ms (100.00%) | Memory: 43.4 MB (41.92%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -1,10 +1,26 @@
-class Solution {
-    public int distinctSubseqII(String s) {
+// class Solution {
+//     public int distinctSubseqII(String s) {
+//         int MOD = 1_000_000_007;
+//         long dp = 1;
+//         long[] last = new long[26];
+//         for (char c : s.toCharArray()) {
+//             int idx = c - 'a';
+//             long newDp = (2 * dp % MOD - last[idx] + MOD) % MOD;
+//             last[idx] = dp;
+//             dp = newDp;
+//         }
+//         return (int) ((dp - 1 + MOD) % MOD);
+//     }
+// }
+
+
+class Solution{
+    public int distinctSubseqII(String s){
         int MOD = 1_000_000_007;
         long dp = 1;
         long[] last = new long[26];
-        for (char c : s.toCharArray()) {
-            int idx = c - 'a';
+        for(char c : s.toCharArray()){
+            int idx = c-'a';
             long newDp = (2 * dp % MOD - last[idx] + MOD) % MOD;
             last[idx] = dp;
             dp = newDp;
