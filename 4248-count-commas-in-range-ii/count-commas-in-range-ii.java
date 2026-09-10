f36314bda532ffcb778de998b07828b5b9228a32
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-10 22:07:53 +0530

    Time: 1 ms (99.24%) | Memory: 42.9 MB (22.81%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,5 +1,18 @@
-class Solution {
-    public long countCommas(long n) {
+// class Solution {
+//     public long countCommas(long n) {
+//         long commas = 0;
+//         long threshold = 1000;
+//         while(threshold <= n){
+//             commas += n - threshold + 1;
+//             threshold *= 1000;
+//         }
+//         return commas;
+//     }
+// }
+
+
+class Solution{
+    public long countCommas(long n){
         long commas = 0;
         long threshold = 1000;
         while(threshold <= n){
