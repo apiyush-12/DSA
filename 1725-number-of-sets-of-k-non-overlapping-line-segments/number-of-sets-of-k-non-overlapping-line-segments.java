1b3d72d1478ede432dc1f9b5400df45aaae14664
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-16 20:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (82.58%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,19 +1,46 @@
+// class Solution {
+//     public static final int MOD = 1_000_000_007;
+//     public int numberOfSets(int n, int k) {
+//         int[][] dp = new int[k+1][n+1];
+//         for(int pos = 0; pos <= n; pos++) dp[0][pos] = 1;
+//         for(int segments = 1; segments <= k; segments++){
+//             int[] prevRowSum = new int[n+1];
+//             for(int pos = n-1; pos >= 0; pos--){
+//                 prevRowSum[pos] = (int)((prevRowSum[pos + 1] + dp[segments-1][pos]) % MOD);
+//             }
+//             for(int pos = n-1; pos>=0; pos--){
+//                 long ways = dp[segments][pos + 1];
+//                 ways += prevRowSum[pos + 1];
+//                 dp[segments][pos] = (int) (ways % MOD);
+//             }
+//         }
+//         return dp[k][0];
+//     }
+// }
+
+
+
 class Solution {
-    public static final int MOD = 1_000_000_007;
-    public int numberOfSets(int n, int k) {
-        int[][] dp = new int[k+1][n+1];
-        for(int pos = 0; pos <= n; pos++) dp[0][pos] = 1;
-        for(int segments = 1; segments <= k; segments++){
-            int[] prevRowSum = new int[n+1];
-            for(int pos = n-1; pos >= 0; pos--){
-                prevRowSum[pos] = (int)((prevRowSum[pos + 1] + dp[segments-1][pos]) % MOD);
-            }
-            for(int pos = n-1; pos>=0; pos--){
-                long ways = dp[segments][pos + 1];
-                ways += prevRowSum[pos + 1];
-                dp[segments][pos] = (int) (ways % MOD);
-            }
+
+    private static final long MOD = 1000000007L;
+
+    private long quickPow(long a, long e) {
+        long result = 1;
+        while (e > 0) {
+            if ((e & 1) != 0) result = (result * a) % MOD;
+            a = (a * a) % MOD;
+            e >>= 1;
         }
-        return dp[k][0];
+        return result;
+    }
+
+    public int numberOfSets(int n, int k) {
+        int m = 2 * k;
+        long numerator = 1, denominator = 1;
+        for (int i = 1; i <= m; i++) {
+            numerator = (numerator * (n + k - i)) % MOD;
+            denominator = (denominator * i) % MOD;
+        }
+        return (int) ((numerator * quickPow(denominator, MOD - 2)) % MOD);
     }
 }
