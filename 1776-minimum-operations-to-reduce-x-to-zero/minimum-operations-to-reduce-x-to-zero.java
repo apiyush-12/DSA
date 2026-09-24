1a7793242f120ab9141b867135a20d019551a2d4
Piyush kumar <142339554+apiyush-12@users.noreply.github.com>
2026-09-24 20:32:03 +0530

    Time: 3 ms (100.00%) | Memory: 102.1 MB (44.91%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -1,22 +1,48 @@
-class Solution {
-    public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        long totalSum = 0;
+// class Solution {
+//     public int minOperations(int[] nums, int x) {
+//         int n = nums.length;
+//         long totalSum = 0;
+//         for(int num : nums) totalSum += num;
+//         long target = totalSum - x;
+//         if(target < 0) return -1;
+//         int left = 0;
+//         long windowSum = 0;
+//         int maxLength = -1;
+//         for(int right=0; right < n; right++){
+//             windowSum += nums[right];
+//             while(left <= right && windowSum > target){
+//                 windowSum -= nums[left];
+//                 left++;
+//             }
+//             if(windowSum == target) maxLength = Math.max(maxLength, right-left+1);
+//         }
+//         if(maxLength == -1) return -1;
+//         return n - maxLength;
+//     }
+// }
+
+
+
+class Solution{
+    public int minOperations(int[] nums, int x){
+        int totalSum = 0;
         for(int num : nums) totalSum += num;
-        long target = totalSum - x;
-        if(target < 0) return -1;
-        int left = 0;
-        long windowSum = 0;
-        int maxLength = -1;
-        for(int right=0; right < n; right++){
-            windowSum += nums[right];
-            while(left <= right && windowSum > target){
-                windowSum -= nums[left];
-                left++;
+        x = totalSum - x;
+        if(x < 0) return -1;
+        else if(x==0) return nums.length;
+        int start = 0;
+        int end = 0;
+        int maxLength=0;
+        int sum = 0;
+        while(end < nums.length){
+            sum += nums[end];
+            while(sum >= x){
+                if(sum==x) maxLength = Math.max(maxLength, end - start +1);
+                sum -= nums[start];
+                start++;
             }
-            if(windowSum == target) maxLength = Math.max(maxLength, right-left+1);
+            end++;
         }
-        if(maxLength == -1) return -1;
-        return n - maxLength;
+        return maxLength == 0 ? -1 : nums.length - maxLength;
     }
 }
